045c9be915b30917ba8fe2c127d760e297fdf5de
Andrew Carpenter <acarpen@wested.org>
2009-07-22 08:06:22 -0700

    add topic associations to entries

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,6 +2,9 @@ class Entry < ActiveRecord::Base
   has_many :agency_assignments
   has_many :agencies, :through => :agency_assignments
   
+  has_many :topic_assignments
+  has_many :topics, :through => :topic_assignments
+  
   has_many :url_references
   has_many :urls, :through => :url_references
 end
