7298d4daf920a16052f13f622a90aeb50687f6fc
Andrew Carpenter <acarpen@wested.org>
2009-07-25 08:47:45 -0700

    eliminate most false positives in date extraction

--- test/unit/potential_date_extractor_test.rb
+++ test/unit/potential_date_extractor_test.rb
@@ -8,18 +8,17 @@ class PotentialDateExtractorTest < ActiveSupport::TestCase
   def test_text_dates
     assert_equal(["January 3"], PotentialDateExtractor.extract("On January 3 foo bar 17 as 12"))
     assert_equal(["January 3rd"], PotentialDateExtractor.extract("On January 3rd foo bar 17 as 12"))
-    assert_equal(["January the 3rd"], PotentialDateExtractor.extract("On January the 3rd foo bar 17 as 12"))
     assert_equal(["January 3rd, 2009"], PotentialDateExtractor.extract("On January 3rd, 2009 foo bar 17 as 12"))
-    assert_equal(["January the 3rd, 2009"], PotentialDateExtractor.extract("On January the 3rd, 2009 foo bar 17 as 12"))
     assert_equal(["January 3 2009"], PotentialDateExtractor.extract("On January 3 2009 foo bar 17 as 12"))
     assert_equal(["January 3, 2009"], PotentialDateExtractor.extract("On January 3, 2009 foo bar 17 as 12"))
     assert_equal(["Jan 3, 2009"], PotentialDateExtractor.extract("On Jan 3, 2009 foo bar 17 as 12"))
     assert_equal(["Jan. 3, 2009"], PotentialDateExtractor.extract("On Jan. 3, 2009 foo bar 17 as 12"))
+    assert_equal(["Jan 3 2009"], PotentialDateExtractor.extract("On Jan 3 2009 foo bar 17 as 12"))
+    assert_equal(["Jan. 3 2009"], PotentialDateExtractor.extract("On Jan. 3 2009 foo bar 17 as 12"))
   end
   
   def test_us_dates
     assert_equal(["3/6/2009"], PotentialDateExtractor.extract("On 3/6/2009 foo bar 17 as 12"))
-    assert_equal(["3/06/2009"], PotentialDateExtractor.extract("On 3/06/2009 foo bar 17 as 12"))
     assert_equal(["03/6/2009"], PotentialDateExtractor.extract("On 03/6/2009 foo bar 17 as 12"))
     assert_equal(["03/06/2009"], PotentialDateExtractor.extract("On 03/06/2009 foo bar 17 as 12"))
     assert_equal(["3/6/09"], PotentialDateExtractor.extract("On 3/6/09 foo bar 17 as 12"))
@@ -28,21 +27,24 @@ class PotentialDateExtractorTest < ActiveSupport::TestCase
     assert_equal(["03/06/09"], PotentialDateExtractor.extract("On 03/06/09 foo bar 17 as 12"))
   end
   
-  def test_iso_dates
-    assert_equal(["2009-01-01"], PotentialDateExtractor.extract("On 2009-01-01 foo bar 17 as 12"))
-    assert_equal(["2009-1-1"], PotentialDateExtractor.extract("On 2009-1-1 foo bar 17 as 12"))
-    assert_equal(["09-01-01"], PotentialDateExtractor.extract("On 09-01-01 foo bar 17 as 12"))
-    assert_equal(["09-1-1"], PotentialDateExtractor.extract("On 09-1-1 foo bar 17 as 12"))
-  end
-  
   def test_multiple_matches
-    assert_equal(["January the 3rd, 2009", "2009-01-01"], PotentialDateExtractor.extract("On January the 3rd, 2009 foo bar 17 as 12 and 2009-01-01 was great"))
+    assert_equal(["January 3rd, 2009", "01/01/2009"], PotentialDateExtractor.extract("On January 3rd, 2009 foo bar 17 as 12 and 01/01/2009 was great"))
   end
   
   def test_not_too_aggressive
-    assert_equal([], PotentialDateExtractor.extract("12:01"))
-    assert_equal([], PotentialDateExtractor.extract("May 2009"))
-    assert_equal([], PotentialDateExtractor.extract("2003, Pub. L.108-159 (2003)"))
-    assert_equal([], PotentialDateExtractor.extract("36 CFR 242.3 and 50 CFR 100.3 of the subsistence"))
+    [
+      "On 2009-01-01 foo bar 17 as 12",
+      "On 09-01-01 foo bar 17 as 12",
+      "On 09-1-1 foo bar 17 as 12",
+      'May 2009',
+      '2003, Pub. L.108-159 (2003)',
+      '12:01',
+      '36 CFR 242.3 and 50 CFR 100.3 of the subsistence',
+      'at coordinates 44-22-48 NL, and 108- 02-18 WL',
+      'at coordinates 44-22-48 NL, and 08- 02-18 WL',
+      'Case no. R-S/07-10)'
+    ].each do |str|
+      assert_equal([], PotentialDateExtractor.extract(str))
+    end
   end
 end
