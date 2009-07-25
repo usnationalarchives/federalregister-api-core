632c90ba85484957a83dfeb900477bd111ff231c
Bob Burbach <bburbac@wested.org>
2009-07-25 10:03:36 -0700

    Remove prototype, add jrails and jquery sparkline

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){ 
+  $('.composite_sparkline').each( function() {
+    span = $(this)
+    values = span.html();
+  });
+
+  $('.composite_sparkline').sparkline(values[$(this)], {type: 'bar', barSpacing: 0, barWidth: 3, barColor: '#ddd'} );
+  $('.composite_sparkline').sparkline(values, {type: 'line', lineColor: 'black', fillColor:false, composite:true} );
+});
