28354e36bb592122157544baae379020b71137d7
Bob Burbach <bburbac@wested.org>
2009-07-25 13:48:25 -0700

    Begin work on entry show

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,12 +1,15 @@
 class EntriesController < ApplicationController
   
   def index
-    @year = params[:year]
-    @month = params[:month]
-    @entries = Entry.find(:all, :conditions => ['publication_date >= ?', "#{@year}-#{@month}-01"], :order => 'publication_date DESC')
+    @year = params[:year]   || Time.now.strftime("%Y")
+    @month = params[:month] || Time.now.strftime("%m")
+    @day   = params[:day]   || Time.now.strftime("%d")
+    
+    @entries = Entry.find(:all, :conditions => ['publication_date >= ?', "#{@year}-#{@month}-#{@days}"], :order => 'publication_date DESC')
   end
   
   def show
-    @entry = Entry.find_by_id(params[:id])
+    @entry = Entry.find_by_document_number(params[:document_number])
+    raise "Entry doesn't exist" if @entry.nil?
   end
 end
