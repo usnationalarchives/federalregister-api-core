dc1374d995cdccef53d0bf1a5df03b677456bde0
Bob Burbach <bburbac@wested.org>
2009-07-28 09:47:41 -0700

    Add agency views

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -3,9 +3,11 @@ class AgenciesController < ApplicationController
   def index
     @agencies  = Agency.find(:all, :order => 'name ASC')
     @chart_max = Agency.max_entry_count
+    @featured_agencies = Agency.featured
+    @week = params[:week].to_i || Time.now.strftime("%W").to_i
   end
   
   def show
-    @agency = Agency.find_by_id(params[:id], :include => :entries, :order => 'entries.publication_date DESC')
+    @agency = Agency.find_by_slug(params[:id], :include => :entries, :order => 'entries.publication_date DESC')
   end
 end
--- app/models/agency.rb
+++ app/models/agency.rb
@@ -19,27 +19,42 @@ class Agency < ActiveRecord::Base
     slug
   end
   
-  def entry_count_by_week
+  def parent
+    Agency.find_by_id(parent_id) unless parent_id.nil?
+  end
+  
+  def entry_count_by_week(week = 0)
+    if week == 0
+      where_clause = "WHERE agencies.id = #{self.id}"
+    else
+      where_clause = "WHERE agencies.id = #{self.id} && WEEK(entries.publication_date) = #{week}"
+    end
+    
     entry_counts = Entry.connection.select_all("
       SELECT WEEK(entries.publication_date) AS pub_week, COUNT(entries.id) AS entry_count
       FROM agencies
       LEFT JOIN entries ON agencies.id = entries.agency_id
-      WHERE agencies.id = #{self.id}
+      #{where_clause}
       GROUP BY WEEK(entries.publication_date)
     ")
+    
     counts = []
-    week = 1
-    weeks_to_date = Time.now.strftime('%U').to_i
-    entry_counts.each do |entry_count|
-      pub_week = entry_count['pub_week'].to_i
-      while week <= pub_week
-        counts << (week == pub_week ? entry_count['entry_count'].to_i : 0)
-        week = week + 1
+    if week == 0
+      week_count = 1
+      weeks_to_date = Time.now.strftime('%U').to_i
+      entry_counts.each do |entry_count|
+        pub_week = entry_count['pub_week'].to_i
+        while week_count <= pub_week
+          counts << (week_count == pub_week ? entry_count['entry_count'].to_i : 0)
+          week_count = week_count + 1
+        end
       end
-    end
-    while week < weeks_to_date
-      counts << 0
-      week = week + 1
+      while week_count < weeks_to_date
+        counts << 0
+        week_count = week_count + 1
+      end
+    else
+      counts = entry_counts.empty? ? 0 : entry_counts.first['entry_count'].to_i
     end
     counts
   end
@@ -52,6 +67,11 @@ class Agency < ActiveRecord::Base
       ORDER BY agency_id DESC
       LIMIT 1
     ")
+  end
+  
+  def self.featured
+    # grab cabinet level agencies (departments) as these are top producing
+    agencies = Agency.all(:conditions => ['name LIKE ?', 'Department%'])
   end
   
   private
