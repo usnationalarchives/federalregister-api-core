37547244eaa72f9e09f5bd08267d6dcc07754c32
Andrew Carpenter <acarpen@wested.org>
2009-07-29 20:48:40 -0700

    add SASS plugin

--- config/environment.rb
+++ config/environment.rb
@@ -18,6 +18,7 @@ Rails::Initializer.run do |config|
   config.gem "nokogiri", :version => '1.3.2'
   config.gem "zilkey-active_api", :lib => 'active_api', :version => '0.2.0'
   config.gem "curb", :version => '0.4.4.0'
+  config.gem "haml", :version => '2.0.9'
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
