debe53a2dced0b842d0d81a092ab2d9ff93aeeba
dave augustine <daugust@wested.org>
2009-07-30 21:35:04 -0700

    Style updates. Topics now show their name rather than a #

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :topics
   
   map.connect 'agencies/:year/weekly/:week', :controller => 'agencies',
                                              :action     => 'index',
