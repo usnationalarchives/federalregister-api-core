1c16c8451b56072f7f2ea84d4b65c61738f0d3fb
Bob Burbach <rburbach@bob-burbachs-macbook.local>
2009-07-30 20:37:40 -0700

    Add basic geokit functionality for entry and place

--- app/models/place.rb
+++ app/models/place.rb
@@ -13,6 +13,27 @@
 =end Schema Information
 
 class Place < ActiveRecord::Base
+  attr_accessor :distance
+  
   has_many :place_determinations
   has_many :entrys, :through => :place_determinations
+  
+  acts_as_mappable :lat_column_name => :latitude,
+                   :lng_column_name => :longitude
+                   
+  def location 
+    @location = [latitude, longitude]
+  end
+  
+  def find_places_within(dist)
+    Place.find(:all, :origin => location, :within => dist)
+  end
+  
+  # def find_entries_within(dist)
+  #   places = find_places_within(dist)
+  #   entries = []
+  #   places.each do |place|
+  #     entries << place.entry
+  #   end
+  # end
 end
