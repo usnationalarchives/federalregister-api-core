5e8d03486126ccae309db768bb87e9200145b7f3
Andrew Carpenter <acarpen@wested.org>
2009-08-02 11:34:33 -0700

    geocoding remote ip library added

---
+++ lib/locator.rb
@@ -0,0 +1,30 @@
+module Locator
+  def remote_location(ip = request.remote_ip)
+    begin
+      require 'ostruct'
+      require 'geoip_city'
+      
+      db = GeoIPCity::Database.new('/opt/GeoIP/share/GeoIP/GeoLiteCity.dat')
+      
+      result = db.look_up(ip)
+      
+      if result
+        OpenStruct.new(result)
+      else
+        nil
+      end
+    rescue MissingSourceFile => e
+      OpenStruct.new({
+        :longitude=>-122.073196411133,
+        :country_code3=>"USA",
+        :country_name=>"United States",
+        :area_code=>650,
+        :city=>"Mountain View",
+        :region=>"CA",
+        :latitude=>37.3973999023438,
+        :country_code=>"US",
+        :dma_code=>807
+      })
+    end
+  end
+end
