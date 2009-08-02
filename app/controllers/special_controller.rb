3c804a1f3e37763e5045f15ec8acdd2078ad5558
dave augustine <daugust@wested.org>
2009-08-01 20:10:09 -0700

    stylee changes

--- app/controllers/special_controller.rb
+++ app/controllers/special_controller.rb
@@ -3,5 +3,6 @@ class SpecialController < ApplicationController
   def home
     # stuff here
     @entries = Entry.all(:limit => 30)
+    @featured_agencies = Agency.featured
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,13 +2,36 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
   
-  if($(".daily")){
-    $(".daily").easySlider({
-      auto: true,
-      continuous: true,
-      pause: 4000,
-      controlsShow: false,
-      vertical: true 
-  	});
-  }
+  // if($(".daily")){
+  //   $(".daily").easySlider({
+  //     auto: true,
+  //     continuous: true,
+  //     pause: 4000,
+  //     controlsShow: false,
+  //     vertical: true 
+  //  });
+  // }
+  
+    // initialize scrollable  
+    $("div.daily").scrollable({ 
+             
+        // items are auto-scrolled in 2 secnod interval 
+        interval: 4000, 
+         
+        // when last item is encountered go back to first item 
+        loop: true,  
+         
+        // make animation a little slower than the default 
+        speed: 600, 
+         
+        // when seek starts make items little transparent 
+        onBeforeSeek: function() { 
+            this.getItems().fadeTo(300, 0.2);         
+        }, 
+         
+        // when seek ends resume items to full transparency 
+        onSeek: function() { 
+            this.getItems().fadeTo(300, 1); 
+        } 
+    }); 
 });
