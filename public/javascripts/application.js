ba08b436a14b48501fee6e2525fc4220c10b191e
dave augustine <daugust@wested.org>
2009-08-02 14:46:11 -0700

    struggling

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,16 +2,6 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
   
-  // if($(".daily")){
-  //   $(".daily").easySlider({
-  //     auto: true,
-  //     continuous: true,
-  //     pause: 4000,
-  //     controlsShow: false,
-  //     vertical: true 
-  //  });
-  // }
-  
     // initialize scrollable  
     $("div.daily").scrollable({ 
              
@@ -24,19 +14,8 @@ $(document).ready(function() {
         loop: true,  
          
         // make animation a little slower than the default 
-        speed: 600//, 
-         
-        // // when seek starts make items little transparent 
-        // onBeforeSeek: function() { 
-        //     this.getItems().fadeTo(300, 0.2);         
-        // }, 
-        //  
-        // // when seek ends resume items to full transparency 
-        // onSeek: function() { 
-        //     this.getItems().fadeTo(300, 1); 
-        // } 
+        speed: 600
     });
-
 
   /*                                   */
   /* Tooltips for featured agency list */
