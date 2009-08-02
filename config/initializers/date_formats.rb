e0864134f2e8a888813fd248f9963af6c2fd7792
Bob Burbach <rburbach@bob-burbachs-macbook.local>
2009-08-02 12:10:09 -0700

    Add count for entries by featured agencies for 30, 90, 1 year. Javascript doesn't quite work.

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -3,5 +3,6 @@ ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
   :day_date   => "%A %d",
   :pretty     => "%A, %B %d, %Y",
   :ymd        => "%Y/%m/%d",
-  :deafult    => "%m/%d/%Y"
+  :deafult    => "%m/%d/%Y",
+  :db_year    => "%Y-%m-%d"
 )
