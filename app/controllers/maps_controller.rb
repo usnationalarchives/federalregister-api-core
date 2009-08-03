d4f6db59b9e91989bd590412f5da92d6cb739c9c
Bob Burbach <rburbach@bob-burbachs-macbook.local>
2009-08-03 01:06:53 -0700

    Add map view of entries near a place using cloudkicker

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,18 +1,24 @@
 class MapsController < ApplicationController
   include Locator
+  include Cloudkicker
+  
   def index
     @user_location = remote_location
     lat  = @user_location.latitude
     long = @user_location.longitude
     @places = Place.find_near([lat,long])
-    @map = GoogleMap::Map.new
-    #@map.bounds =  [GoogleMap::Point.new(47.6597, -121.318), GoogleMap::Point.new(48.6597, -123.318)] #SEATTLE WASHINGTON 50KM
+    
+    @map = Cloudkicker::Map.new( :lat      => lat, 
+                                 :long     => long,
+                                 :zoom     => 8,
+                                 :style_id => 1714
+                                )
     @places.each do |place|
-      @map.markers << GoogleMap::Marker.new( :map => @map, 
-                                             :lat => place.latitude,
-                                             :lng => place.longitude,
-                                             :html => place.entry_list
-                                           )
+      Cloudkicker::Marker.new( :map   => @map, 
+                               :lat   => place.latitude,
+                               :long  => place.longitude, 
+                               :title => place.entry_list
+                             )
     end
   end
 end
