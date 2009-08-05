a7ac19a2a9e8498bd148437639336cecf4a46a32
Andrew Carpenter <acarpen@wested.org>
2009-08-05 00:23:44 -0700

    advanced entry search

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,11 +1,45 @@
 class EntriesController < ApplicationController
+  include Geokit::Geocoders
   def search
+    @topics = Topic.all
+    @agencies = Agency.all
+    
+    with = {}
+    
     @search_term = params[:q]
     
-    @entries = Entry.search(
-      @search_term,
-      :page => params[:page],
-      :order => "@relevance DESC, publication_date DESC"
+    @near = params[:near]
+    unless @near.blank?
+      within = params[:within].to_i
+      if within <= 0 || within >= 500
+        within = 500
+      end
+      
+      location = Rails.cache.fetch("location_of: '#{@near}'") { Geokit::Geocoders::GoogleGeocoder.geocode(@near) }
+      
+      # TODO: send error message to user on invalid location
+      
+      place_ids = Place.find(:all, :select => "id", :origin => location, :within => within).map &:id
+      with[:place_ids] = place_ids
+    end
+    
+    [:agency_id, :topic_ids].each do |attribute|
+      unless params[attribute].blank?
+        with[attribute] = params[attribute]
+      end
+    end
+    
+    if !params[:publication_date_greater_than].blank? || !params[:publication_date_less_than].blank?
+      # TODO: send error message to user on invalid dates
+      start_date = Chronic.parse(params[:publication_date_greater_than], :context => :past) || DateTime.parse('1994-01-01')
+      end_date = Chronic.parse(params[:publication_date_less_than], :context => :past) || DateTime.parse('2100-01-01')
+      with[:publication_date] = Range.new(start_date.midnight.to_f.to_i,end_date.midnight.to_f.to_i)
+    end
+    
+    @entries = Entry.search(@search_term, 
+      :page => params[:page] || 1,
+      :order => "@relevance DESC, publication_date DESC",
+      :with => with
     )
   end
   
