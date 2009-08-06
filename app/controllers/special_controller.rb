016bf36fd0083c2c917b35f58f1e78efd2588f4d
Bob Burbach <rburbach@bob-burbachs-macbook.local>
2009-08-06 12:52:02 -0700

    Add closing soon and recently opened sections. Scope entries to current day

--- app/controllers/special_controller.rb
+++ app/controllers/special_controller.rb
@@ -2,7 +2,26 @@ class SpecialController < ApplicationController
   
   def home
     # stuff here
-    @entries = Entry.all(:limit => 30)
+    @entries = Entry.all(:conditions => ['publication_date = ?', Date.today])
     @featured_agencies = Agency.featured
-  end
+    
+    date_range = [Date.today, Date.today + 1]
+    @closing_soon = ReferencedDate.find(:all, 
+                                        :include => :entry, 
+                                        :conditions => {:date_type => 'CommentDate', :date => date_range[0]..date_range[1]},
+                                        :order => 'date ASC',
+                                        :limit => 10
+                                       )
+
+    date_range = [Date.today - 7, Date.today]                                   
+    @recently_opened = ReferencedDate.find(:all, 
+                                           :include => :entry, 
+                                           :conditions => {:date_type => 'CommentDate', 
+                                                           :date => date_range[0]..date_range[1],
+                                                           :entries => {:publication_date => date_range[0]..date_range[1]}
+                                                          },
+                                           :order => 'date ASC',
+                                           :limit => 10
+                                          )                                   
+  end                 
 end
