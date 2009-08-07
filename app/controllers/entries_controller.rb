e92a5ba1c23e7bb901642d9c561bc212f6afa9a7
Bob Burbach <rburbach@bob-burbachs-macbook.local>
2009-08-07 12:58:37 -0700

    Update map styles and add ability to use small control

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -127,9 +127,9 @@ class EntriesController < ApplicationController
     end
     
     if !@places.blank?
-      @map = Cloudkicker::Map.new( :style_id => 1714,
-                                   :bounds   => true,
-                                   :points   => @places
+      @map = Cloudkicker::Map.new( :style_id          => 1714,
+                                   :bounds            => true,
+                                   :points            => @places
                                  )
       @places.each do |place|
         Cloudkicker::Marker.new( :map   => @map, 
