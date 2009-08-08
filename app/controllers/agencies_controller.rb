c09216815e0704d9f441392e7131a703cd30531d
dave augustine <daugust@wested.org>
2009-08-07 20:37:01 -0700

    link colors

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -48,7 +48,7 @@ class AgenciesController < ApplicationController
       wants.html
       
       wants.rss do
-        @feed_name = "GovPulse: #{@agency.name}"
+        @feed_name = "govpulse: #{@agency.name}"
         @feed_description = "Recent Federal Register entries from #{@agency.name}."
         @entries = @agency.entries.all(:include => [:topics, :agency], :order => "publication_date DESC", :limit => 20)
         render :template => 'entries/index.rss.builder'
