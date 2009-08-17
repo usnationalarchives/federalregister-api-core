6e376bc9cbab6677a1cf3b80552f651f6175614a
Bob Burbach <rburbach@bob-burbachs-macbook.local>
2009-08-17 01:15:41 -0700

    Add calendars to the entry view pages. Toggles via icon.

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -21,13 +21,55 @@ module CalendarHelper
     
     html = ''
     months.each do |month|
-      html << calendar_for(year, month, &entry_events_proc(entry, date_range))
+      html << calendar_for(year.to_i, month.to_i, &entry_events_proc(entry, date_range))
     end
     html 
   end
   
   def dates_calendar_view(dates, year, month)
-    calendar_for(year, month, :calendar_class => 'group calendar', &dates_events_proc(dates))
+    calendar_for(year.to_i, month.to_i, :calendar_class => 'group calendar', &dates_events_proc(dates))
+  end
+  
+  def entries_calendar_view(year, month)
+    year = year.to_i
+    month = month.to_i
+    if Entry.count(:conditions => ['publication_date >= ?', "#{year}-#{month + 1}-01"]) > 0
+      month_1     = month - 1
+      bump_year_1 = month_1 > 0 ? (month_1 < 13 ? 0 : 1) : -1
+      year_1      = year + bump_year_1
+      month_1     = month_1 > 0 ? (month_1 < 13 ? month_1 : 1) : 12
+      
+      month_2     = month + 1
+      bump_year_2 = month_2 > 0 ? (month_2 < 13 ? 0 : 1) : -1
+      year_2      = year + bump_year_2
+      month_2     = month_2 > 0 ? (month_2 < 13 ? month_2 : 1) : 12
+      
+      dates = [[year_1,month_1], [year, month], [year_2, month_2]]
+    else
+      #TODO Need to only show days in the next month that we have entries for. i.e. looking at July you should not see links for all of Aug
+      month_1     = month - 1
+      bump_year_1 = month_1 > 0 ? (month_1 < 13 ? 0 : 1) : -1
+      year_1      = year + bump_year_1
+      month_1     = month_1 > 0 ? (month_1 < 13 ? month_1 : 1) : 12
+      
+      month_2 = month_1 - 1
+      year_2  = year_1
+      
+      dates = [[year_2, month_2], [year_1, month_1], [year, month]]
+    end
+    html = ''
+    dates.each do |year, month|
+      end_of_month = Date.parse("#{year}-#{month}-01").end_of_month
+      last_pub = Entry.find(:first, 
+                            :conditions => ['publication_date >= ? && publication_date <= ?', 
+                                            "#{year}-#{month}-01",
+                                            end_of_month
+                                           ],
+                            :order => 'publication_date DESC'
+                           )
+      html << calendar_for(year, month, :current_month => "%B %Y", :calendar_class => 'group calendar', &entries_proc(end_of_month, last_pub))
+    end
+    html
   end
   
   def entry_events_proc(entry, date_range)
@@ -49,6 +91,18 @@ module CalendarHelper
         [link_to(day.day, "#event_#{day.to_s(:db)}"), { :class => "dayWithEvents" }]
       else
         day.day
+      end
+    end
+  end
+  
+  def entries_proc(end_of_month, last_pub)
+    # TODO: This doesn't yet handle holidays. We should show a link to a holiday date on the calendar.
+    last_day = last_pub.nil? ? end_of_month : last_pub.publication_date
+    lambda do |day|
+      if [0,6].include?(day.wday) || day.day > last_day.day
+        day.day
+      else
+        [link_to(day.day, "#entry_#{day.to_s(:ymd)}"), { :class => "dayWithEntries" }]
       end
     end
   end
