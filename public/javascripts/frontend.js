36ac8e2842742d56b8082cbe87d9741f7ae7ffff
dave augustine <daugust@wested.org>
2009-08-16 18:46:29 -0700

    standardized header in sidebar, split advanced search into two columns, fixed search bar gfx

--- public/javascripts/frontend.js
+++ public/javascripts/frontend.js
@@ -4,22 +4,22 @@ $(document).ready(function() {
   * SEARCH FILTER
   **********************************/
  //hide and disable the form on entry
- $("form li:not('.simple')").toggle().find("li").children(":not(label)").disable();
- 
- //when the button is clicked - change the button text, toggle the open class, and show/hide enable/disable
-  $("a.options").bind("click", function(e){
-    e.preventDefault();
-    
-    if($(this).hasClass("open"))
-      $(this).html($(this).html().replace('Hide', "Show"));
-    else
-      $(this).html($(this).html().replace('Show', "Hide"));
-      
-    $(this).toggleClass("open");
-    
-    $("form li:not('.simple')").toggle().find("li").children(":not(label)").toggleDisabled();
-  });
-  
+ // $("form li:not('.simple')").toggle().find("li").children(":not(label)").disable();
+ // 
+ // //when the button is clicked - change the button text, toggle the open class, and show/hide enable/disable
+ //  $("a.options").bind("click", function(e){
+ //    e.preventDefault();
+ //    
+ //    if($(this).hasClass("open"))
+ //      $(this).html($(this).html().replace('Hide', "Show"));
+ //    else
+ //      $(this).html($(this).html().replace('Show', "Hide"));
+ //      
+ //    $(this).toggleClass("open");
+ //    
+ //    $("form li:not('.simple')").toggle().find("li").children(":not(label)").toggleDisabled();
+ //  });
+ //  
   
   
   // This needs to be fixed to use a proper slug - may require url rewriting.
