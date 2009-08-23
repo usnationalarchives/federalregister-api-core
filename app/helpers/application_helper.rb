b6ffb389689e4bf76c91b3a2ddaf38915d92b3a7
Andrew Carpenter <acarpen@wested.org>
2009-08-23 12:58:17 -0700

    add page titles, cleanup title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  
-  def title(text)
+  def title(text, options = {})
+    options.symbolize_keys!
+    
     content_for :title, text
+    unless options[:body] == false
+      content_for :precolumn, content_tag(:h1, text)
+    end
   end
   
   def feed_autodiscovery(feed_url, title = 'RSS')
