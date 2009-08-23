a994abf92b40cf8c47a6e04f7126f4375effa033
Andrew Carpenter <acarpen@wested.org>
2009-08-23 10:04:24 -0700

    topic RSS feed

--- app/controllers/topic_groups_controller.rb
+++ app/controllers/topic_groups_controller.rb
@@ -8,39 +8,53 @@ class TopicGroupsController < ApplicationController
     group_name = params[:id]
     group_name.gsub!(/_|-/, ' ')
     @topic_group = TopicGroup.find_by_group_name!(params[:id])
-    @entries = Entry.find(:all,
-        :conditions => {:topics => {:group_name => @topic_group.group_name}},
-        :include => :topics,
-        :order => "entries.publication_date DESC",
-        :limit => 100)
-    
-    agencies_and_entry_counts = []
-    @entries.group_by(&:agency_id).each do |agency_id, entries|
-      next if agency_id.blank?
-      agencies_and_entry_counts << [Agency.find(agency_id), entries.size]
-    end
-    
-    @agency_labels = []
-    @agency_values = []
-    agencies_and_entry_counts.sort_by{|a| a[1]}.reverse[0,10].each do |agency, count|
-      @agency_labels << "#{agency.sidebar_name}"
-      @agency_values << count
-    end
 
-    if @agency_values.sum < @entries.size
-      count = (@entries.size - @agency_values.sum)
-      @agency_labels << "Other"
-      @agency_values << count
+    respond_to do |wants|
+      wants.html do
+        @entries = Entry.find(:all,
+            :conditions => {:topics => {:group_name => @topic_group.group_name}},
+            :include => :topics,
+            :order => "entries.publication_date DESC",
+            :limit => 100)
+        
+        agencies_and_entry_counts = []
+        @entries.group_by(&:agency_id).each do |agency_id, entries|
+          next if agency_id.blank?
+          agencies_and_entry_counts << [Agency.find(agency_id), entries.size]
+        end
+    
+        @agency_labels = []
+        @agency_values = []
+        agencies_and_entry_counts.sort_by{|a| a[1]}.reverse[0,10].each do |agency, count|
+          @agency_labels << "#{agency.sidebar_name}"
+          @agency_values << count
+        end
+
+        if @agency_values.sum < @entries.size
+          count = (@entries.size - @agency_values.sum)
+          @agency_labels << "Other"
+          @agency_values << count
+        end
+    
+        @granule_labels = []
+        @granule_values = []
+        @entries.group_by(&:granule_class).each do |granule_class, entries|
+          @granule_labels << granule_class
+          @granule_values << entries.size
+        end
+      end
+      wants.rss do
+        @feed_name = "govpulse: #{@topic_group.name}"
+        @feed_description = "Recent Federal Register entries about #{@topic_group.name}."
+        @entries = Entry.find(:all,
+            :conditions => {:topics => {:group_name => @topic_group.group_name}},
+            :joins => [:topics],
+            :include => :agency,
+            :order => "entries.publication_date DESC",
+            :limit => 20)
+        render :template => 'entries/index.rss.builder'
+      end
     end
-    
-    @granule_labels = []
-    @granule_values = []
-    @entries.group_by(&:granule_class).each do |granule_class, entries|
-      @granule_labels << granule_class
-      @granule_values << entries.size
-    end
-    
-    
   end
   
   def by_letter
