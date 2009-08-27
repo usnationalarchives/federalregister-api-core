7d1e2c467c95f45dd502980f919c73bcd00d8668
Andrew Carpenter <acarpen@wested.org>
2009-08-26 18:40:43 -0700

    make custom paths available in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,19 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+  include PathHelper
+  
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   
   include Locator
   
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  
+  private
+  
+  # this way helpers included in the controller can reference the controller in the same way
+  def controller
+    self
+  end
 end
