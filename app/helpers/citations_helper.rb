324278f2098f21e685bd0bd5769b8293e293daa9
Andrew Carpenter <acarpen@wested.org>
2009-08-26 18:41:06 -0700

    only link internally if we have the FR entry

--- app/helpers/citations_helper.rb
+++ app/helpers/citations_helper.rb
@@ -2,9 +2,18 @@ module CitationsHelper
   def add_citation_links(text)
     text.gsub!(/((\d+)\s+U\.?S\.?C\.?\s+(\d+))/, '<a href="http://frwebgate.access.gpo.gov/cgi-bin/getdoc.cgi?dbname=browse_usc&docid=Cite:+\2USC\3" class="usc external" target="_blank">\1</a>')
     text.gsub!(/((\d+)\s+CFR\s+(\d+)(?:\.(\d+))?)/, '<a href="http://frwebgate.access.gpo.gov/cgi-bin/get-cfr.cgi?YEAR=current&TITLE=\2&PART=\3&SECTION=\4&SUBPART=&TYPE=TEXT" class="cfr external" target="_blank">\1</a>')
-    text.gsub!(/((\d+)\s+FR\s+(\d+))/, '<a href="/citation/\2/\3" class="fr">\1</a>')
-    text.gsub!(/((\d+)\s+FR\s+(\d+))/, '<a href="/citation/\2/\3" class="fr">\1</a>')
-    text.gsub!(/(Pub\.\s+L\.\s+(\d+)-(\d+))/) do
+    text.gsub!(/((\d+)\s+FR\s+(\d+))/) do
+      full = $1
+      issue = $2
+      page = $3
+      if issue.to_i >= 59
+        link_to $1, "/citation/#{issue}/#{page}"
+      else
+        $1
+      end
+    end
+    
+    text.gsub!(/(Pub(?:lic|\.)\s+L(?:aw|\.)\.\s+(\d+)-(\d+))/) do
       full = $1
       congress = $2
       law = $3
