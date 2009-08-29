7077b36a429729df5f0ed8a197256ff65fd3d54b
Andrew Carpenter <acarpen@wested.org>
2009-08-29 09:30:54 -0700

    allow user to change location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,18 @@
 class LocationsController < ApplicationController
   def edit
-    @location = Location.new
+    @location = current_location
+  end
+  
+  def update
+    @location = Geokit::Geocoders::GoogleGeocoder.geocode(params[:geokit_geo_loc][:name])
+    
+    if @location.success
+      session[:location] = @location.to_hash
+      redirect_to params[:redirect_to] || root_url
+    else
+      @message = 'We could not understand that location.'
+      @location = current_location
+      render :action => 'edit'
+    end
   end
 end
