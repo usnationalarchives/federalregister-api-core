7253aee47c6cadc5c6a77f247e77814bcca8626c
dave augustine <daugust@wested.org>
2009-08-31 21:38:48 -0700

    added >> to lists. fixed tooltips.

--- lib/locator.rb
+++ lib/locator.rb
@@ -1,22 +1,9 @@
 module Locator
   require 'geoip'
   
-  def current_location(ip = request.remote_ip)
-    unless @current_location
-      if session[:location]
-        @current_location = Geokit::GeoLoc.new session[:location]
-      else
-        @current_location = Geokit::Geocoders::GeoIp::do_geocode(ip)
-        if ! @current_location.success?
-          @current_location = default_location
-        end
-        
-        session[:location] = @current_location.to_hash
-      end
-    end
-    
-    @current_location
-  end
+def current_location(ip = request.remote_ip)
+  default_location
+end
   
   private
   
