a7c8df6d84ebe6837936df83ce0664e40e61fa5c
dave augustine <daugust@wested.org>
2009-08-31 21:42:38 -0700

    changed locator.rb back to what it was

--- lib/locator.rb
+++ lib/locator.rb
@@ -1,9 +1,22 @@
 module Locator
   require 'geoip'
   
-def current_location(ip = request.remote_ip)
-  default_location
-end
+  def current_location(ip = request.remote_ip)
+    unless @current_location
+      if session[:location]
+        @current_location = Geokit::GeoLoc.new session[:location]
+      else
+        @current_location = Geokit::Geocoders::GeoIp::do_geocode(ip)
+        if ! @current_location.success?
+          @current_location = default_location
+        end
+        
+        session[:location] = @current_location.to_hash
+      end
+    end
+    
+    @current_location
+  end
   
   private
   
