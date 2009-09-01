1f36dc6768f1cdab01019fe5003c1d3057e42d31
Andrew Carpenter <acarpen@wested.org>
2009-08-31 17:16:07 -0700

    first pass at AJAXified congress

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -250,9 +250,6 @@ class EntriesController < ApplicationController
                                )
       end
     end
-    members_of_congress = Sunlight::Legislator.all_in_zipcode(94110)
-    @senators = members_of_congress.reject{|l| l.title != 'Sen'}
-    @reps     = members_of_congress.reject{|l| l.title != 'Rep'}
   end
   
   def tiny_pulse
