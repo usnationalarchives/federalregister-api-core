216dc802dbc72648e8ac8ffe94f3966ae91ef005
Andrew Carpenter <acarpen@wested.org>
2009-09-01 06:42:43 -0700

    cache legislator lookups

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -18,7 +18,10 @@ class LocationsController < ApplicationController
   
   def congress
     if current_location.is_us?
-      members_of_congress = Sunlight::Legislator.all_for(:latitude => current_location.lat, :longitude => current_location.lng)
+      members_of_congress = Rails.cache.fetch("legislators_for #{current_location.lat}, #{current_location.lng}") do
+        Sunlight::Legislator.all_for(:latitude => current_location.lat, :longitude => current_location.lng)
+      end
+      
       @senators = members_of_congress.values_at(:senior_senator, :junior_senator)
       @reps     = members_of_congress.values_at(:representative)
     
