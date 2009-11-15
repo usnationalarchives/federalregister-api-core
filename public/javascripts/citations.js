9d1142f2e8459d1181bbcbbeca97e6dd8a647b9b
dave augustine <daugust@wested.org>
2009-11-15 15:24:32 -0800

    updated citation live search to match more than the anchors but not inside tool tips

--- public/javascripts/citations.js
+++ public/javascripts/citations.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+
+  $("ul.citation_controls li.livesearch input").bind("keyup", function(e){
+    $("ul.citation-right_tip li").hide().find(":not('div.tip *'):regex('\\b" + $(this).attr("value") + "')").parent().show();
+  });
+    
+  $("ul.filter li.livesearch input").bind("focus", function(e){
+    show_section("#all");    
+    $("ul.filter li").removeClass("on");
+    $(this).parent().addClass("on");
+  });  
+  
   var anchor_value = window.location.hash;
   show_section(anchor_value);
   
@@ -22,6 +33,9 @@ function show_section(anchor_value){
       $("#citations").hide();
       $("ul.citation_controls li.referencing_entries").addClass("on");      
       break;
+    case '#all':
+      $("#citations").add("#referencing_entries").show();
+      $("ul.citation_controls li.all").addClass("on");      
     default:
       break;
   }  
