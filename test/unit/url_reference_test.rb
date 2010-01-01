8e03c89108cb66e9c0f7c71fd2a6a921406f6a7b
Andrew Carpenter <acarpen@wested.org>
2010-01-01 15:19:45 -0800

    should_have_index is now should_have_db_index

--- test/unit/url_reference_test.rb
+++ test/unit/url_reference_test.rb
@@ -4,5 +4,5 @@ class UrlReferenceTest < ActiveSupport::TestCase
   should_belong_to :entry
   should_belong_to :url
   
-  should_have_index :entry_id, :url_id
+  should_have_db_index :entry_id, :url_id
 end
