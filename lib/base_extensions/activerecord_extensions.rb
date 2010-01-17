32a4852a86dc6f19824f305faa239c78dd617f8c
Andrew Carpenter <acarpen@wested.org>
2010-01-17 14:29:08 -0800

    look in filesystem for full text of entries

--- lib/base_extensions/activerecord_extensions.rb
+++ lib/base_extensions/activerecord_extensions.rb
@@ -27,4 +27,36 @@ class ActiveRecord::Base
       
     end
   end
+  
+  def self.file_attribute(attribute, &filename_generator)
+    require 'ftools'
+    
+    path_method = "#{attribute}_file_path"
+    define_method path_method do
+      instance_eval(&filename_generator)
+    end
+    
+    define_method "#{attribute}=" do |val|
+      path = self.send(path_method)
+      File.makedirs(File.dirname(path))
+      self.class.transaction do 
+        if self.class.columns_hash["#{attribute}_created_at"] && self["#{attribute}_created_at"].nil?
+          self["#{attribute}_created_at"] = Time.now
+        end
+        
+        if self.class.columns_hash["#{attribute}_updated_at"]
+          self["#{attribute}_updated_at"] = Time.now
+        end
+        
+        File.open(path, 'w') {|f| f.write(val) }
+        save
+      end
+    end
+
+    define_method attribute do
+      path = self.send(path_method)
+      File.read(path)
+    end
+  end
+  
 end
