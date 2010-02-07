846fa7563be5e489f1ea9aaf610ea19b67eb813b
Andrew Carpenter <acarpen@wested.org>
2010-02-07 09:25:30 -0800

    put homepage map in iframe

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,6 +3,30 @@ class LocationsController < ApplicationController
     @location = current_location
   end
   
+  def places
+    @dist = 20
+    @location = current_location
+    @lat  = @location.lat
+    @long = @location.lng
+    @places = Place.usable.find_near([@lat,@long], :within => @dist, :limit => 20)
+    
+    @map = Cloudkicker::Map.new( :style_id => 1714,
+                                 :zoom     => 8,
+                                 :lat      => @places.map(&:latitude).average,
+                                 :long     => @places.map(&:longitude).average
+                               )
+    @places.each do |place|
+      Cloudkicker::Marker.new( :map   => @map, 
+                               :lat   => place.latitude,
+                               :long  => place.longitude, 
+                               :title => 'Click to view location info',
+                               :info  => render_to_string(:partial => 'maps/place_marker_tooltip', :locals => {:place => place} ),
+                               :info_max_width => 200
+                             )
+    end
+    render :layout => 'minimal'
+  end
+  
   def update
     @location = Geokit::Geocoders::GoogleGeocoder.geocode(params[:geokit_geo_loc][:name])
     
