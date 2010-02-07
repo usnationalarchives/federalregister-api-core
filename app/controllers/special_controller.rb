291a59931bd9c66887a7f37bd3a7c84e612eceaf
Andrew Carpenter <acarpen@wested.org>
2010-02-07 08:28:21 -0800

    speed improvements for homepage; also add totals

--- app/controllers/special_controller.rb
+++ app/controllers/special_controller.rb
@@ -8,22 +8,22 @@ class SpecialController < ApplicationController
     
     next_week = Date.today .. Date.today + 7.days
     this_week = Date.today - 7.days .. Date.today
-    @closing_soon = Agency.find(:all,
-                                :include => {:entries => :referenced_dates},
-                                :conditions => {:referenced_dates => {:date_type => 'CommentDate', :date => next_week}},
-                                :order => 'referenced_dates.date ASC')
-    @recently_opened = Agency.find(:all,
-                                :include => {:entries => :referenced_dates},
-                                :conditions => {:referenced_dates => {:date_type => 'CommentDate', :date => this_week}},
-                                :order => 'referenced_dates.date ASC')
-    @effective_this_week = Agency.find(:all,
-                                :include => {:entries => :referenced_dates},
-                                :conditions => {:referenced_dates => {:date_type => 'EffectiveDate', :date => this_week}},
-                                :order => 'referenced_dates.date ASC')
-    @recent_proposed_rules_by_agency = Agency.find(:all,
-                                :include => :entries,
+    @closing_soon = Entry.find(:all,
+                                :include => [:agency, :comments_close_date],
+                                :conditions => {:referenced_dates => {:date => next_week}},
+                                :order => 'referenced_dates.date ASC').group_by(&:agency).reject{|a,e| a.nil?}
+    @recently_opened = Entry.find(:all,
+                                :include => [:agency, :comments_close_date],
+                                :conditions => {:referenced_dates => {:date => this_week}},
+                                :order => 'referenced_dates.date ASC').group_by(&:agency).reject{|a,e| a.nil?}
+    @effective_this_week = Entry.find(:all,
+                                :include => [:agency, :effective_date],
+                                :conditions => {:referenced_dates => {:date => this_week}},
+                                :order => 'referenced_dates.date ASC').group_by(&:agency).reject{|a,e| a.nil?}
+    @recent_proposed_rules_by_agency = Entry.find(:all,
+                                :include => :agency,
                                 :conditions => {:entries => {:publication_date => this_week, :granule_class => 'PRORULE' }},
-                                :order => 'entries.publication_date DESC')
+                                :order => 'entries.publication_date DESC').group_by(&:agency).reject{|a,e| a.nil?}
     @dist = 20
     @location = current_location
     @lat  = @location.lat
