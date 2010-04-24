fdc8972f5c4b63758731aaaaae95429b7bcf72cf
Andrew Carpenter <aemain@Andrew-Carpenters-MacBook-Air.local>
2010-04-23 17:02:28 -0700

    fix regulatory_plan associations, add significant named scope

--- app/models/regulatory_plan.rb
+++ app/models/regulatory_plan.rb
@@ -1,5 +1,17 @@
 class RegulatoryPlan < ActiveRecord::Base
+  SIGNIFICANT_PRIORITY_CATEGORIES = ['Economically Significant', 'Other Significant']
+  
   file_attribute(:full_xml)  {"#{RAILS_ROOT}/data/regulatory_plans/#{issue}/#{regulation_id_number}.xml"}
   
   has_many :events, :class_name => "RegulatoryPlanEvent"
+  has_many :events,
+           :primary_key => :regulation_id_number,
+           :foreign_key => :regulation_id_number
+  has_many :entries,
+           :primary_key => :regulation_id_number,
+           :foreign_key => :regulation_id_number
+  
+  def self.current_issue
+    RegulatoryPlan.first(:select => :issue, :order => "issue DESC").issue
+  end
 end
