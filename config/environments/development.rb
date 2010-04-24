e578f392a3225768419265e51420e022cd7d3ce9
Andrew Carpenter <acarpen@wested.org>
2010-04-24 08:59:20 -0700

    don't load thunder punch, just require it

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,4 +16,4 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-config.gem "thunder_punch"
+config.gem "thunder_punch", :lib => false
