2b0891f55a7cafa2b30cb534429e739ab417eb01
Andrew Carpenter <acarpen@wested.org>
2010-04-25 09:05:17 -0700

    refactor and add page number support to entry importer

--- lib/content/entry_importer/utils.rb
+++ lib/content/entry_importer/utils.rb
@@ -1,17 +1,15 @@
-class EntryImporter
-  module Utils
-    def included(klass)
-      klass.instance_eval do
-        cattr_accessor :provided
-        self.provided = [] unless self.provided.present?
-      end
-      
-      klass.provided = klass.provided + @attributes
+module Content::EntryImporter::Utils
+  def included(klass)
+    klass.instance_eval do
+      cattr_accessor :provided
+      self.provided = [] unless self.provided.present?
     end
     
-    def provides(*attributes)
-      @attributes ||= []
-      @attributes += attributes
-    end
+    klass.provided = klass.provided + @attributes
+  end
+  
+  def provides(*attributes)
+    @attributes ||= []
+    @attributes += attributes
   end
 end
