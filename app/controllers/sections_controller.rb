947d1402d2765471d738b70b70bba494e0c84b03
Andrew Carpenter <acarpen@wested.org>
2010-04-26 12:57:38 -0700

    quick first pass at section page

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,5 +1,6 @@
 class SectionsController < ApplicationController
   def show
     @section = Section.find_by_slug(params[:slug]) or raise ActiveRecord::RecordNotFound
+    @publication_date = Entry.latest_publication_date
   end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -2,4 +2,8 @@ class Section < ActiveYaml::Base
   set_root_path "#{Rails.root}/data"
   field :name
   field :slug
+  
+  def entries
+    Entry.scoped(:conditions => {:section_assignments => {:section_id => id}}, :joins => :section_assignments)
+  end
 end
