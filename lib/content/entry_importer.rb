5f0f3da58fc6d12aabd58c1be0f94805fb37acfc
Andrew Carpenter <acarpen@wested.org>
2010-05-01 11:04:32 -0700

    more importing work

--- lib/content/entry_importer.rb
+++ lib/content/entry_importer.rb
@@ -3,6 +3,7 @@ module Content
     include Content::EntryImporter::CFR
     include Content::EntryImporter::PageNumber
     include Content::EntryImporter::Sections
+    include Content::EntryImporter::Agencies
   
     def self.process_all_by_date(date, *attributes)
       if date =~ /^\d{4}$/
@@ -18,36 +19,44 @@ module Content
       dates.each do |date|
         puts "handling #{date}"
         ModsFile.new(date).document_numbers.each do |document_number|
-          importer = EntryImporter.new(date, document_number)
-          importer.update_attributes(attributes)
+          importer = EntryImporter.new(:date => date, :document_number => document_number)
+          importer.update_attributes(*attributes)
         end
       end
     end
   
-    attr_accessor :date, :document_number, :mods_node, :bulkdata_node
-    def initialize(date, document_number, options = {})
-      @date = date.is_a?(String) ? Date.parse(date) : date
-      @document_number = document_number
-    
+    attr_accessor :date, :document_number, :mods_node, :bulkdata_node, :entry
+    def initialize(options = {})
       options.symbolize_keys!
+      if options[:entry]
+        @entry = options[:entry]
+        @date = @entry.publication_date
+        @document_number = @entry.document_number
+      else
+        @date = options[:date].is_a?(String) ? Date.parse(options[:date]) : options[:date]
+        raise "must provide a date if no entry" if @date.nil?
+        @document_number = options[:document_number] or raise "must provide a document number if no entry"
+        @entry = Entry.find_by_document_number(@document_number) || Entry.new(:document_number => @document_number, :publication_date => @date)
+      end
+      
       if options[:mods_node]
         @mods_node = options[:mods_node]
-      else
-        @mods_node = ModsFile.new(@date).find_entry_node_by_document_number(@document_number)
       end
     
       if options[:bulkdata_node]
         @bulkdata_node = options[:bulkdata_node]
       end
+    end
     
-      @entry = Entry.find_by_document_number(@document_number) || Entry.new(:document_number => @document_number, :publication_date => @date)
+    def mods_node
+      @mods_node ||= ModsFile.new(@date).find_entry_node_by_document_number(@document_number)
     end
-  
+    
     def update_all_provided_attributes
-      set_attributes(self.provided)
+      set_attributes(*self.provided)
     end
   
-    def update_attributes(attribute_names)
+    def update_attributes(*attribute_names)
       attribute_names.each do |attr|
         @entry.send("#{attr}=", self.send(attr))
       end
