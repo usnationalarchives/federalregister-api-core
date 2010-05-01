1928d772e4284f69a778c556075a1f1ae5701c69
Andrew Carpenter <acarpen@wested.org>
2010-05-01 11:04:22 -0700

    first pass at highlighting entries

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,6 +3,10 @@ class Section < ActiveYaml::Base
   field :name
   field :slug
   
+  def highlighted_entries(publication_date)
+    Entry.scoped(:conditions => {:section_highlights => {:publication_date => publication_date, :section_id => id}}, :joins => :section_highlights, :order => "section_highlights.position")
+  end
+  
   def entries
     Entry.scoped(:conditions => {:section_assignments => {:section_id => id}}, :joins => :section_assignments)
   end
