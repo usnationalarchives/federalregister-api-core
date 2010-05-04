11439d7f204bdc80d6be7dfe866f406b8db52a98
Andrew Carpenter <acarpen@wested.org>
2010-05-03 17:20:12 -0700

    section view!

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,5 +2,6 @@ class SectionsController < ApplicationController
   def show
     @section = Section.find_by_slug(params[:slug]) or raise ActiveRecord::RecordNotFound
     @publication_date = Entry.latest_publication_date
+    @highlighted_entries = @section.highlighted_entries(@publication_date)
   end
 end
