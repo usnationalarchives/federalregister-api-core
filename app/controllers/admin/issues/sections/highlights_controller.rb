3c4e8b0fd0e28976d3d46bc6970fa2a0a4762206
Andrew Carpenter <acarpen@wested.org>
2010-05-03 17:41:04 -0700

    admin editing of title, abstract; etc

--- app/controllers/admin/issues/sections/highlights_controller.rb
+++ app/controllers/admin/issues/sections/highlights_controller.rb
@@ -9,13 +9,29 @@ class Admin::Issues::Sections::HighlightsController < AdminController
     @section_highlight.save!
   end
   
+  def edit
+    @publication_date = Date.parse(params[:issue_id])
+    @section = Section.find_by_slug(params[:section_id])
+    @section_highlight = SectionHighlight.find_by_publication_date_and_section_id_and_entry_id!(@publication_date, @section, params[:id])
+  end
+  
   def update
     @publication_date = Date.parse(params[:issue_id])
     @section = Section.find_by_slug(params[:section_id])
     @section_highlight = SectionHighlight.find_by_publication_date_and_section_id_and_entry_id!(@publication_date, @section, params[:id])
     
-    @section_highlight.update_attributes!(params[:section_highlight])
-    render :nothing => true
+    if @section_highlight.update_attributes(params[:section_highlight])
+      respond_to do |wants|
+        wants.html do
+          redirect_to admin_issue_section_path(@publication_date.to_s(:db), @section)
+        end
+        wants.js do
+          render :nothing => true
+        end
+      end
+    else
+      render :action => :edit
+    end
   end
   
   def destroy
