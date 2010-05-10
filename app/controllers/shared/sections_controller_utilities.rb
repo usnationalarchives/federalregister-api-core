015c7db6da4938d51812ff275c128803f42d6156
Andrew Carpenter <acarpen@wested.org>
2010-05-10 15:44:42 -0700

    section preview

---
+++ app/controllers/shared/sections_controller_utilities.rb
@@ -0,0 +1,9 @@
+module Shared
+  module SectionsControllerUtilities
+    def prepare_for_show(section_slug, date)
+      @section = Section.find_by_slug(section_slug) or raise ActiveRecord::RecordNotFound
+      @publication_date = date
+      @highlighted_entries = @section.highlighted_entries(@publication_date)
+    end
+  end
+end
