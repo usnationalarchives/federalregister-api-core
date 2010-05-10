bc83a1d5512eb15a4178e5cb3aee5ee025f6b652
Andrew Carpenter <acarpen@wested.org>
2010-05-10 08:21:42 -0700

    first complete pass at lede photo suggestions

--- app/models/lede_photo.rb
+++ app/models/lede_photo.rb
@@ -1,4 +1,34 @@
 class LedePhoto < ApplicationModel
   has_many :entries
-  has_attached_file :photo
+  has_attached_file :photo,
+                    :styles => { :small => ["150", :jpg], :medium => ["245", :jpg], :large => ["580", :jpg], :full_size => ["", :jpg] },
+                    :processors => [:thumbnail]
+  
+  before_save :download_and_crop_file
+  
+  private
+  
+  def download_and_crop_file
+    if url.present?
+      file_path = Tempfile.new('flickr.jpg').path
+      Rails.logger.warn "FILE NAME => #{file_path}"
+      Curl::Easy.download(url, file_path)
+      
+      if crop_x
+        dst = Tempfile.new('flickr.jpg')
+        dst.binmode
+
+        command = <<-end_command
+          "#{ File.expand_path(file_path) }[0]"
+          -crop "#{crop_width}x#{crop_height}+#{crop_x}+#{crop_y}"
+          "#{ File.expand_path(dst.path) }"
+        end_command
+        Rails.logger.warn(command)
+        Paperclip.run("convert", command.gsub(/\s+/, " "))
+        file_path = dst.path
+      end
+      
+      self.photo = File.open(file_path)
+    end
+  end
 end
---
+++ lib/content/entry_importer/lede_photo_candidates.rb
@@ -0,0 +1,25 @@
+module Content::EntryImporter::LedePhotoCandidates
+  extend ActiveSupport::Memoizable
+  # extend Content::EntryImporter::Utils
+  # provides :lede_photo_suggestions
+  
+  def lede_photo_candidates
+    if false && entry.lede_photo_candidates
+      entry.lede_photo_candidates
+    else
+      tags = SocialTagExtractor.new.extract(entry.abstract)
+    
+      suggestions = []
+      tags.each do |tag|
+        suggestions << [tag, flickr_info_for_tag(tag)]
+      end
+    
+      YAML::dump(suggestions)
+    end
+  end
+  
+  def flickr_info_for_tag(tag_name)
+    Flickr.new.search(tag_name)
+  end
+  memoize :flickr_info_for_tag
+end
