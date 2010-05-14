7bf88dcafcfcf0e08d1f2e26e185eb4bb0cbbb5c
Andrew Carpenter <acarpen@wested.org>
2010-05-14 10:14:52 -0700

    fix topic name assignments

--- lib/content/entry_importer/topic_names.rb
+++ lib/content/entry_importer/topic_names.rb
@@ -3,15 +3,15 @@ module Content::EntryImporter::TopicNames
   provides :topic_names
   
   def topic_names
-    topics = []
+    topic_names = []
     mods_node.css('subject topic').each do |topic_node|
       
       # clean up their mess
-      topic_name = topic_node.content
-      topic_name.sub!(/^and /, '') # remove 'and' at beginning
-      topic_names = topic_name.split(/\s*;\s*/).map(&:capitalize) # split on semicolons
+      name = topic_node.content
+      name.sub!(/^and /, '') # remove 'and' at beginning
+      names = name.split(/\s*;\s*/).map(&:capitalize) # split on semicolons
       
-      topic_names.each do |name|
+      names.each do |name|
         next if name.length == 1 # one character topic names help no one
         topic_names << TopicName.find_or_create_by_name(name)
       end
