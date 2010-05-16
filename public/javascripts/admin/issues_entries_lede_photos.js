886b6efa150d4553b6fee07e21da0b34e7565a8f
Bob Burbach <bob.burbach@gmail.com>
2010-05-15 18:53:26 -0700

    Getting content flow and topics to sit on the page appropriately.

--- public/javascripts/admin/issues_entries_lede_photos.js
+++ public/javascripts/admin/issues_entries_lede_photos.js
@@ -34,7 +34,76 @@ function prepare_to_crop_image(item) {
   });
 }
 
+/* hiding dom elements that are using ContentFlow (like photo flow) 
+   before ContentFlow initializes breaks the auto resizing of images
+   so these methods provide a hide that uses CSS positioning instead
+*/
+function css_hide(object) {
+  $(object).css('position', 'absolute');
+  $(object).css('left', '-9999px');
+}
+
+function css_show(object) {
+  $(object).css('position', 'relative');
+  $(object).css('left', '0px');
+}
+
 $(document).ready(function(){
+  
+  // get photo flow off the screen initially so tabs function properly
+  css_hide( $('#lede_photo_flow') );
+  // get tags off screen as well
+  $('#lede_photo_candidate_topics').hide();
+  
+  $("#issue_entry_edit ul li a#content").bind('click', function(e) {
+    e.preventDefault();
+    if( !$(this).hasClass('active') ) {
+      // change the styles on our buttons so they look pressed
+      $("#issue_entry_edit ul li a#photo").removeClass('active');
+      $(this).addClass('active');
+      
+      // hide photos and show edit fields
+      $('#lede_photo_flow').hide();
+      $('#lede_photo_candidate_topics').hide();
+      $('#entry_content').show();
+    }
+  });
+  
+  $("#issue_entry_edit ul li a#photo").bind('click', function(e) {
+    e.preventDefault();
+    if( !$(this).hasClass('active') ) {
+      
+      // bring photo flow back into position
+      css_show( $('#lede_photo_flow') );
+      
+      // change the styles on our buttons so they look pressed
+      $("#issue_entry_edit ul li a#content").removeClass('active');
+      $(this).addClass('active');
+      
+      // hide edit fields and show photos
+      $('#entry_content').hide();
+      $('#lede_photo_flow').show();
+      $('#lede_photo_candidate_topics').show();
+    }
+  });
+  
+  // hide each topics photo flow initially
+  $('.topic_photo_flow').each(function() {
+    css_hide(this);
+  });
+  
+  // show first topic
+  css_show( $('.topic_photo_flow:first') );
+  
+  $(".topic_link").bind('click', function(e){
+    e.preventDefault();
+    var topic_flow_id = '#' + $(this).attr('id') + '_flow'
+    $('.topic_photo_flow').each(function() {
+      css_hide(this);
+    });
+    css_show($(topic_flow_id));
+  });
+  
   $("#custom_tag").change(function() {
     var tag = $(this).val()
     
