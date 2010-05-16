aedef23bebe162934079e54722c7375bf375e608
Andrew Carpenter <acarpen@wested.org>
2010-05-16 10:50:17 -0700

    hide duplicate agencies

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -183,7 +183,7 @@ class Entry < ApplicationModel
   
   def agencies_exluding_parents
     parent_agency_ids = agencies.map(&:parent_id).compact
-    agencies.reject{|a| parent_agency_ids.include?(a.id) }
+    agencies.reject{|a| parent_agency_ids.include?(a.id) }.sort.uniq
   end
   
   def curated_title
