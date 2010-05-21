daa0f8a41dd578872fb6c0b141d5d65eb20054dc
Andrew Carpenter <acarpen@wested.org>
2010-05-21 14:19:35 -0700

    semi-generic autocomplete functionality

---
+++ public/javascripts/application_form_builder.js
@@ -0,0 +1,28 @@
+$(function() {
+  $('.formtastic .autocomplete').each(function(){
+    var parent_li = $(this);
+    var name_field = $(parent_li.find('input[type=text]').first());
+    var hidden_field = $(parent_li.find('input[type=hidden]').first());
+    
+    $.ajax({
+      url: name_field.attr('data-source-url'),
+      dataType: 'json',
+      success: function(json_data){
+        var data = $.makeArray($(json_data).map(function(i,obj) { return {id : obj.id, value : obj.name } }));
+        name_field.autocomplete({
+          source: data,
+          minLength: 0,
+          select: function(event, ui) {
+            name_field.val('');
+            parent_li.find("ul.selected").append('<li>' + ui.item.value + '<input type="hidden" name="' + hidden_field.attr('name') + '" value="' + ui.item.id + '" /></li>')
+            return false;
+          }
+        });
+      }
+    });
+  
+    parent_li.find(".selected li").live("click", function() {
+      $(this).remove();
+    })
+  });
+});
