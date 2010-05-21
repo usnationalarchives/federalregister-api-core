ce6ab83745453edd08d1385a96e6a1041c3aeead
Andrew Carpenter <acarpen@wested.org>
2010-05-21 12:11:39 -0700

    compass & 960

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,6 @@
-require 'compass'
+Compass.add_project_configuration(File.join(Rails.root, "config", "compass.config"))
 # If you have any compass plugins, require them here.
-Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
-Compass.configuration.environment = RAILS_ENV.to_sym
+require 'ninesixty'
+
 Compass.configure_sass_plugin!
+Compass.handle_configuration_change!
