35d3f1a37164ba15b405c3ac2862df56d3d272d6
Dave Augustine <dave@criticaljuncture.org>
2010-05-23 16:36:22 -0700

    Small updates and fixes to the last commit.

--- public/javascripts/sectionpage.js
+++ public/javascripts/sectionpage.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   
-  $("#comments-closing-opening").tabs({
-    trackState: true,
-    srcPath: '/blank.html'
-  });
+  if( $("#comments-closing-opening").size() > 0){
+    $("#comments-closing-opening").tabs({
+      trackState: true,
+      srcPath: '/blank.html'
+    });
+  }
   
 });
