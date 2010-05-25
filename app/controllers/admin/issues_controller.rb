537940164548fb1ebc9ecdf379bc266e5355c0df
Andrew Carpenter <acarpen@wested.org>
2010-05-25 08:19:14 -0700

    correctly handle missing sections on unassigned page

--- app/controllers/admin/issues_controller.rb
+++ app/controllers/admin/issues_controller.rb
@@ -1,7 +1,7 @@
 class Admin::IssuesController < AdminController
   def show
     @publication_date = Date.parse(params[:id])
-    @entries_without_sections = Entry.published_on(@publication_date).all(:include => :sections, :conditions => "sections.id IS NULL")
+    @entries_without_sections = Entry.published_on(@publication_date).all(:include => :section_assignments, :conditions => "section_assignments.id IS NULL")
     @sections = Section.all
   end
 end
