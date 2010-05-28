00581ebe3c1b0807f32e009a53f6d966312a3b0b
Dave Augustine <dave@criticaljuncture.org>
2010-05-28 14:49:59 -0700

    Agency page first pass, static content included. Tweaks to comment list.

--- public/javascripts/articlepage.js
+++ public/javascripts/articlepage.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-  $("ul.table_of_graphics").before('<div id="gallery"><div id="controls"></div><div class="slideshow-container"><div id="loading"></div><div id="slideshow"></div><div id="caption"></div></div></div>');
-  $("ul.table_of_graphics").wrap("<div id='thumbs'></div>");
-	$('div.navigation').css({'width' : '200px', 'float' : 'left'});
-   var gallery = $('#thumbs').galleriffic({	
-     imageContainerSel:      '#slideshow',
-		 controlsContainerSel:   '#controls'
-		});
+  if( $("#entries").size() > 0 ){
+    $("ul.table_of_graphics").before('<div id="gallery"><div id="controls"></div><div class="slideshow-container"><div id="loading"></div><div id="slideshow"></div><div id="caption"></div></div></div>');
+    $("ul.table_of_graphics").wrap("<div id='thumbs'></div>");
+  	$('div.navigation').css({'width' : '200px', 'float' : 'left'});
+     var gallery = $('#thumbs').galleriffic({	
+       imageContainerSel:      '#slideshow',
+  		 controlsContainerSel:   '#controls'
+  		});
+	}
 });
