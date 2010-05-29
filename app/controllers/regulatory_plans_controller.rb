08ef678c07b582641db72c8edd652cdf577a9623
Andrew Carpenter <acarpen@wested.org>
2010-05-29 13:54:15 -0700

    reg search placeholder

--- app/controllers/regulatory_plans_controller.rb
+++ app/controllers/regulatory_plans_controller.rb
@@ -4,6 +4,10 @@ class RegulatoryPlansController < ApplicationController
     @entries = @regulatory_plan.entries.paginate(:per_page => 10, :page => params[:page])
   end
   
+  def search
+    @search = RegulatoryPlanSearch.new(params)
+  end
+  
   def tiny_url
     regulatory_plan = RegulatoryPlan.find_by_regulation_id_number!(params[:regulation_id_number], :order => "issue DESC")
     redirect_to regulatory_plan_path(regulatory_plan), :status=>:moved_permanently
