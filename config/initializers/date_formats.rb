2c797e6af806770976ebe825209669ed30baa6a5
Andrew Carpenter <acarpen@wested.org>
2010-05-28 17:11:09 -0700

    sparkline of distribution of dates

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,5 +1,6 @@
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
   :month_year => "%B %Y", 
+  :year_month => "%Y%m", 
   :day_date   => "%A %d",
   :wday_and_pretty_date => lambda { |time| time.strftime("%A, %B #{time.day.ordinalize}") },
   :pretty     => "%A, %B %d, %Y",
