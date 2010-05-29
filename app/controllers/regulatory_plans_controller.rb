4c041b8c8830fd20480a31fa6a4241ae0aeda40e
Andrew Carpenter <acarpen@wested.org>
2010-05-29 13:44:44 -0700

    tiny urls for regulations

--- app/controllers/regulatory_plans_controller.rb
+++ app/controllers/regulatory_plans_controller.rb
@@ -3,4 +3,9 @@ class RegulatoryPlansController < ApplicationController
     @regulatory_plan = RegulatoryPlan.find_by_regulation_id_number(params[:regulation_id_number], :order => "issue DESC")
     @entries = @regulatory_plan.entries.paginate(:per_page => 10, :page => params[:page])
   end
+  
+  def tiny_url
+    regulatory_plan = RegulatoryPlan.find_by_regulation_id_number!(params[:regulation_id_number], :order => "issue DESC")
+    redirect_to regulatory_plan_path(regulatory_plan), :status=>:moved_permanently
+  end
 end
