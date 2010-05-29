83ed9cd326c696e96a189822e2c3e1a0d57eaefb
Bob Burbach <bob.burbach@gmail.com>
2010-05-29 16:44:22 -0700

    Add mysql, sphinx, git, rails, capistrano, and imagemagick to our server configs. Modify groups/users to have proper system ids and update ubuntu recipe to load the neccessary libraries for gems that compile natively. Add ebs attachment and spawn.

--- config/poolparty/clouds.rb
+++ config/poolparty/clouds.rb
@@ -11,20 +11,35 @@ pool :fr2 do
     instance_type 'm1.small'
     
     # attach the ebs volumes
-    # ebs_volumes do
-    #   size 40
-    #   device "/dev/sdh"
-    #   snapshot_id "snap-24b1654c" #TODO find a way to automate this as it's new everyday...!
-    # end
+    ebs_volumes do
+      size 40
+      device "/dev/sdh"
+      snapshot_id "snap-5aa8c532" #TODO find a way to automate this as it's new everyday...!
+    end
     
     chef :solo do
       repo File.join(File.dirname(__FILE__) , "chef_cloud")
       
       recipe "apt"
       recipe "ubuntu"
+      recipe "ec2"
+      recipe "openssl"
+      recipe "imagemagick"
+      
+      #recipe "mysql"
+      #recipe "mysql::client"
+      recipe "mysql::server"
+      recipe "mysql::server_ec2"
+      recipe "sphinx"
+      
       recipe "apache2"
       recipe "passenger_enterprise"
       recipe "passenger_enterprise::apache2"
+      
+      recipe "git"
+      recipe "rails"
+      recipe "capistrano"
+      
       
       attributes  :lsb    => {
                                   :code_name  => 'karmic',
@@ -34,9 +49,23 @@ pool :fr2 do
                                 :listen_ports   => ["80"], 
                                 :server_name    => 'test.fr2.criticaljuncture.org',
                                 #:server_aliases => 'www.something',
+                                :web_dir        => '/var/www',
                                 :docroot        => '/var/www/apps/fr2/current/public',
                                 :name           => 'fr2',
                                 :enable_mods    => ["rewrite", "deflate", "expires"]
+                             },
+                  :ec2    => true,
+                  :mysql  => {
+                              :server_root_password   => 'abcd1234',
+                              :server_repl_password   => '1234abcd',
+                              :server_debian_password => '12abcd34',
+                              :ec2_path               => "/vol/lib/mysql",
+                              :ebs_vol_dev            => "/dev/sdh",
+                              :ebs_vol_size           => 40
+                             },
+                  :rails  => {
+                              :version     => "2.3.5",
+                              :environment => "production"
                              }
                              
     end
