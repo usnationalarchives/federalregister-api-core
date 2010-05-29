769d6f9a4c9b74c836ac3b7dbceadb27ec3333cf
Andrew Carpenter <acarpen@wested.org>
2010-05-29 14:54:23 -0700

    add agency admin for logo & description

--- app/controllers/admin/agencies_controller.rb
+++ app/controllers/admin/agencies_controller.rb
@@ -1,10 +1,31 @@
 class Admin::AgenciesController < AdminController
   def index
     respond_to do |wants|
-      wants.json do
-        @agencies = Agency.all(:order => "agencies.name")
-        render :json => @agencies.to_json(:only => [ :id, :name ])
+      wants.html do
+        @search = Agency.scoped(:order => "agencies.name").search(params[:search])
+        @agencies = @search.all.paginate(:per_page => 20, :page => params[:page])
       end
+      
+      wants.json do
+        agencies = Agency.all(:order => "agencies.name")
+        render :json => agencies.to_json(:only => [ :id, :name ])
+      end
+    end
+  end
+  
+  def edit
+    @agency = Agency.find_by_slug!(params[:id])
+  end
+  
+  def update
+    @agency = Agency.find_by_slug!(params[:id])
+    
+    if @agency.update_attributes(params[:agency])
+      flash[:notice] = "Successfully saved."
+      redirect_to admin_agencies_url
+    else
+      flash.now[:error] = "There was a problem."
+      render :action => :edit
     end
   end
 end
