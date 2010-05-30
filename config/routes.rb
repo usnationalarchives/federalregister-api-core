f01720f1e203f36b9a52d12ae392db6ba48b08cc
Andrew Carpenter <acarpen@wested.org>
2010-05-30 10:25:49 -0700

    fix copy/paste error

--- config/routes.rb
+++ config/routes.rb
@@ -105,7 +105,7 @@ ActionController::Routing::Routes.draw do |map|
   map.regulatory_plan_search 'regulations/search',
                       :controller => 'regulatory_plans',
                       :action     => 'search'
-  map.short_entry 'r/:regulation_id_number', :controller => 'regulatory_plans',
+  map.short_regulation 'r/:regulation_id_number', :controller => 'regulatory_plans',
                                              :action     => 'tiny_url'
 
   # SECTIONS
