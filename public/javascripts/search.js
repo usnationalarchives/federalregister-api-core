268ce3e4c8890168617969ad1fa5ce1912976237
Dave Augustine <dave@criticaljuncture.org>
2010-05-31 13:52:45 -0700

    Small polish to the search page. Rearranging agency view.

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('a.load_facet').live('click', function() {
     var anchor = $(this);
-    $(this).after('<img src="/images/ui/ui-anim_basic_16x16.gif" />')
+    $(this).after('<img src="/images/ui/ui-anim_basic_16x16.gif" />');
     var facet_name = anchor.attr('data-facet-name');
     var url = $(location).attr('href').replace('/search?', '/search/facet?facet=' + facet_name + '&');
     $.ajax({
