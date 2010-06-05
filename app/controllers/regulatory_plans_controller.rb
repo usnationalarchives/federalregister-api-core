78d77404bd6d433e1ad87bac024dab46e4f04733
Andrew Carpenter <acarpen@wested.org>
2010-06-05 08:32:07 -0700

    add support for AJAX-loading facets on regulation search

--- app/controllers/regulatory_plans_controller.rb
+++ app/controllers/regulatory_plans_controller.rb
@@ -7,6 +7,12 @@ class RegulatoryPlansController < ApplicationController
     @search = RegulatoryPlanSearch.new(params)
   end
   
+  def search_facet
+    @search = RegulatoryPlanSearch.new(params)
+    facets = @search.send(params[:facet] + "_facets")
+    render :partial => "search/facet", :collection => facets, :layout => nil
+  end
+  
   def tiny_url
     regulatory_plan = RegulatoryPlan.find_by_regulation_id_number!(params[:regulation_id_number], :order => "issue DESC")
     redirect_to regulatory_plan_path(regulatory_plan), :status=>:moved_permanently
