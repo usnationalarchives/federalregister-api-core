76e0efedea04b34cb5ee1cf409e687e237a3fbca
Andrew Carpenter <acarpen@wested.org>
2010-06-08 18:14:47 -0700

    faux most popular entries on section page

--- app/controllers/shared/sections_controller_utilities.rb
+++ app/controllers/shared/sections_controller_utilities.rb
@@ -4,6 +4,7 @@ module Shared
       @section = Section.find_by_slug(section_slug) or raise ActiveRecord::RecordNotFound
       @publication_date = date
       @highlighted_entries = @section.highlighted_entries(@publication_date)
+      @popular_entries = @section.entries.popular(5)
     end
   end
 end
