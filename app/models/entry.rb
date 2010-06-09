3279478c5f98e0ce528a17966bfb4ada3c86145e
Andrew Carpenter <acarpen@wested.org>
2010-06-08 17:55:06 -0700

    limit comments closing/opening to 7 days

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -143,7 +143,7 @@ class Entry < ApplicationModel
     scoped(:conditions => {:entries => {:publication_date => publication_date}})
   end
   
-  def self.comments_closing(range = (Date.today .. Date.today + 14.days))
+  def self.comments_closing(range = (Date.today .. Date.today + 7.days))
     scoped(
       :joins => :comments_close_date,
       :conditions => {:referenced_dates => {:date => range}},
@@ -151,7 +151,7 @@ class Entry < ApplicationModel
     )
   end
   
-  def self.comments_opening(range = (Date.today - 14.days .. Date.today))
+  def self.comments_opening(range = (Date.today - 7.days .. Date.today))
     scoped(
       :joins => :comments_close_date,
       :conditions => {:entries => {:publication_date => range}},
