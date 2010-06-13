6a8836b155162992b3c7f336a00d165a636c4703
Dave Augustine <dave@criticaljuncture.org>
2010-06-13 14:51:59 -0700

    Homepage promo working as intended.  Tweaked to look more like the comp.  Implemented some type changes on article view.  A few changes to address Mikes comments.

--- app/models/lede_photo.rb
+++ app/models/lede_photo.rb
@@ -21,7 +21,7 @@ class LedePhoto < ApplicationModel
   attr_reader :flickr_photo_id
   has_many :entries
   has_attached_file :photo,
-                    :styles => { :small => ["140", :jpg], :medium => ["245", :jpg], :large => ["580", :jpg], :full_size => ["", :jpg] },
+                    :styles => { :homepage => ["100", :jpg], :small => ["140", :jpg], :medium => ["245", :jpg], :large => ["580", :jpg], :full_size => ["", :jpg] },
                     :processors => [:thumbnail]
   
   before_save :download_and_crop_file
