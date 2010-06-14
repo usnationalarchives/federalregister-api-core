8cf81e48b4f9aa17fcbe1101564fde37f6eec187
Dave Augustine <dave@criticaljuncture.org>
2010-06-14 09:23:50 -0700

    Homepage updates.

--- public/javascripts/jquery.utilities.js
+++ public/javascripts/jquery.utilities.js
@@ -11,3 +11,20 @@ jQuery.extend(
   jQuery.expr[':'], {
     Contains: "jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase())>=0"
 });
+
+
+// http://blog.mastykarz.nl/jquery-random-filter/
+jQuery.jQueryRandom = 0;
+jQuery.extend(
+  jQuery.expr[":"], {
+    random: function(a, i, m, r) {
+        if (i == 0) {
+            jQuery.jQueryRandom = Math.floor(Math.random() * r.length);
+        };
+        console.log(a);
+        console.log(i);
+        console.log(m);
+        console.log(r);
+        return i == jQuery.jQueryRandom;
+    }
+});
