13308eeff08f1fb198a8e6d18004078109e64922
Andrew Carpenter <acarpen@wested.org>
2010-06-15 19:03:19 -0700

    final touches to bundler...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,4 +31,4 @@ config.action_mailer.default_url_options = {:host => 'fr2.criticaljuncture.org'}
 # Commented out in as password protection is really annoying
 # config.action_controller.asset_host = "http://assets%d.fr2.criticaljuncture.org"
 
-# Put gems in Gemfile
+# Put gems in Gemfile...
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,4 +27,4 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 
-# Put gems in Gemfile
+# Put gems in Gemfile...
