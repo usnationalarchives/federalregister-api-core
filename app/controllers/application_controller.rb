4f34c45a6b044e8f40b05c04e0aca3eec313f5b5
Andrew Carpenter <acarpen@wested.org>
2010-06-17 16:30:14 -0400

    disable sessions on non-admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  session :disabled => true
   helper :all # include all helpers, all the time
   include RouteBuilder
   include ViewHelper
