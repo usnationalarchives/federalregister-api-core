6e65680a44f395539b6c35596e29996f04e0d1a5
Andrew Carpenter <acarpen@wested.org>
2010-06-18 14:27:22 -0400

    beginning of cache headers

--- app/controllers/special_controller.rb
+++ app/controllers/special_controller.rb
@@ -1,10 +1,11 @@
 class SpecialController < ApplicationController
   def home
-    expires_in 10.minutes, :public => true
+    expires_in 4.hours, :public => true
     @sections = Section.all
   end
   
   def agency_highlight
+    expires_in 10, :public => true
     @agency_highlight = AgencyHighlight.random_choice
     render :layout => false
   end
