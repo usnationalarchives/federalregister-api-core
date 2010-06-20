1e8334c673b0bdd5c57fadcfb8bf73cd4f2cf5de
Andrew Carpenter <acarpen@wested.org>
2010-06-20 12:27:43 -0700

    early first pass at admin for events

--- app/helpers/admin/eventful_entries_helper.rb
+++ app/helpers/admin/eventful_entries_helper.rb
@@ -1,11 +1,29 @@
 module Admin::EventfulEntriesHelper
-  def link_events(text, events)
+  def link_events(text, dates)
     result = text.dup
     
-    events.each do |event|
-      result.gsub!(event, "<strong>#{event}</strong>")
+    dates.uniq.each do |date|
+      result.gsub!(date) do |date_str|
+        date = Date.parse(date_str)
+        if date
+          link_to date_str, '#', :class => "date", 'data-date' => date.to_s
+        else
+          date_str
+        end
+      end
     end
     
     result
   end
+  
+  def link_places(text, places)
+    result = text.dup
+    places.each do |place|
+      result.gsub!(/#{Regexp.escape(place.string)}/) do |place_str|
+        link_to place.string, '#', :class => "place", 'data-id' => place.id, 'data-name' => place.name
+      end
+    end
+    result
+  end
+  
 end
