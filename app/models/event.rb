34dfb81c05a94197c4a79aa89120be0820177a6e
Andrew Carpenter <acarpen@wested.org>
2010-06-22 11:49:50 -0700

    merge referenced dates into events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,22 +1,48 @@
 class Event < ApplicationModel
+  EVENT_TYPES_SINGULAR = {
+    'PublicMeeting' => 'Public Meeting',
+    'ClosedMeeting' => 'Closed Meeting',
+    'CommentsOpen'  => 'Comment Period Opening',
+    'CommentsClose' => 'Comment Period Closing',
+    'EffectiveDate' => 'Effective Date'
+  }
+  EVENT_TYPES_PLURAL = {
+    'PublicMeeting' => 'Public Meetings',
+    'ClosedMeeting' => 'Closed Meetings',
+    'CommentsOpen'  => 'Comment Periods Opening',
+    'CommentsClose' => 'Comment Periods Closing',
+    'EffectiveDate' => 'Effective Dates'
+  }
+  
   belongs_to :entry
   belongs_to :place
   has_many :agency_assignments, :through => :entry, :foreign_key => "entries.id"
-  validates_presence_of :entry, :place, :date, :title
+  validates_presence_of :entry, :date, :event_type
+  validates_presence_of :title, :place, :if => Proc.new{|e| e.event_type == 'PublicMeeting' || e.event_type == 'ClosedMeeting'}
+  validates_inclusion_of :event_type, :in => EVENT_TYPES_SINGULAR.keys
   
   def agencies
     agency_assignments.map(:agency)
   end
   
+  def type
+    Event::EVENT_TYPES_SINGULAR[event_type]
+  end
+  
+  def title
+    self['title'] || entry.title
+  end
+  
   define_index do
     # fields
-    indexes title
-    indexes date
+    indexes "IFNULL(events.title, entries.title)", :as => :title
     indexes entry.abstract
-    indexes place.name
+    indexes place.name, :as => :place
+    indexes event_type, :as => :type, :facet => true
     indexes "LOAD_FILE(CONCAT('#{RAILS_ROOT}/data/text/', entries.document_file_path, '.txt'))", :as => :entry_full_text
     
     # attributes
+    has date
     has entry.agency_assignments(:agency_id), :as => :agency_ids
     has place_id
     
