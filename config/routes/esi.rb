5ce9e40bd21c405bb50d95804dcfbfd35bdc4e65
Andrew Carpenter <acarpen@wested.org>
2010-06-22 08:34:01 -0700

    first pass at event searching

--- config/routes/esi.rb
+++ config/routes/esi.rb
@@ -7,6 +7,11 @@ ActionController::Routing::Routes.draw do |map|
   map.entries_search_results 'articles/search/results', :controller => 'entries/search', :action => 'results'
   map.entries_search_facets 'articles/search/facets/:facet', :controller => 'entries/search', :action => 'facets'
   
+  # EVENT SEARCH
+  map.event_search_header 'events/search/header', :controller => 'events/search', :action => 'header'
+  map.event_search_results 'events/search/results', :controller => 'events/search', :action => 'results'
+  map.event_search_facets 'events/search/facets/:facet', :controller => 'events/search', :action => 'facets'
+  
   # REGULATIONS
   map.regulatory_plan_timeline 'regulations/:regulation_id_number/timeline',
                                 :controller => 'regulatory_plans',
