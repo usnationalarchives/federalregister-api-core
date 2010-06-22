17a400b589ff178b7eede6422627ff6cbd907355
Andrew Carpenter <acarpen@wested.org>
2010-06-22 12:32:11 -0700

    full remove referenced_dates

--- app/helpers/entry_helper.rb
+++ app/helpers/entry_helper.rb
@@ -1,10 +1,4 @@
 module EntryHelper
-  def text_with_links(entry, text)
-    text = add_date_links(entry, text)
-    text = add_citation_links(text)
-    text
-  end
-  
   def entry_title(entry)
     truncate(entry.title, :length => 100)
   end
