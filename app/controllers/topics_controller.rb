415f47bad78cd48113356385f76a907d94e434bf
Andrew Carpenter <acarpen@wested.org>
2010-06-23 07:39:04 -0700

    rss for topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -10,21 +10,12 @@ class TopicsController < ApplicationController
 
     respond_to do |wants|
       wants.html do
-        @most_cited_entries = Entry.all(
-          :conditions => ["topics.slug = ? AND entries.citing_entries_count > 0", @topic.slug],
-          :joins => :topics,
-          :order => "citing_entries_count DESC, publication_date DESC",
-          :limit => 50
-        )
-        @entries = Entry.find(:all,
-            :conditions => {:topics => {:slug => @topic.slug}},
-            :include => :topics,
-            :order => "entries.publication_date DESC",
-            :limit => 50)
+        @most_cited_entries = @topic.entries.most_cited(50)
+        @entries = @topic.entries.most_recent(50)
         @entry_types = Entry.all(
           :select => 'granule_class, count(*) AS count',
-          :joins  => :topics,
-          :conditions => {:topics => {:id => @topic.id}},
+          :joins => :topic_assignments,
+          :conditions => {:topic_assignments => {:topic_id => @topic.id}},
           :group => 'granule_class',
           :order => 'count DESC'
         )
@@ -33,12 +24,21 @@ class TopicsController < ApplicationController
       wants.rss do
         @feed_name = "Federal Register: #{@topic.name}"
         @feed_description = "Recent Federal Register entries about #{@topic.name}."
-        @entries = Entry.find(:all,
-            :conditions => {:topics => {:slug => @topic.slug}},
-            :joins => [:topics],
-            :include => :agency,
-            :order => "entries.publication_date DESC",
-            :limit => 20)
+        @entries = @topic.entries.most_recent(20)
+        render :template => 'entries/index.rss.builder'
+      end
+    end
+  end
+  
+  def significant_entries
+    cache_for 1.day
+    @topic = Topic.find_by_slug!(params[:id])
+    
+    respond_to do |wants|
+      wants.rss do
+        @feed_name = "Federal Register: Significant articles from the '#{@topic.name}' topic"
+        @feed_description = "Significant Federal Register articles from the '#{@topic.name}' topic."
+        @entries = @topic.entries.significant.most_recent(20).preload(:topics, :agencies)
         render :template => 'entries/index.rss.builder'
       end
     end
