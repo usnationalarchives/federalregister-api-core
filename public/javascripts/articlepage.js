29ff6f0186a49babfdb9de9651d4efc714e41b55
Andrew Carpenter <acarpen@wested.org>
2010-06-23 06:34:35 -0700

    calculate current page correctly

--- public/javascripts/articlepage.js
+++ public/javascripts/articlepage.js
@@ -14,7 +14,8 @@ var citation_info = {
   create: function( index ){
     var id = "citation_info_" + index;
     var index_el = $("#" + index);
-    var box = '<div id="' + id + '" class="pull_out citation_box"><ul><li class="link"><a href="/a/'+ $(".doc_number").text() + '/' + index +'">Link to this paragraph</a></li><li class="cite_volume"><strong>Citation</strong> ' + $(".metadata_list .volume").text() + ' FR ' + $(".metadata_list .page").text() + '</li><li class="cite_page"><strong>Page</strong> ' + $(".metadata_list .page").text() + '</li><li class="email"><a href="#">Email this</a></li><li class="twitter"><a href="#">Share this on Twitter</a></li><li class="facebook"><a href="#">Share this on Facebook</a></li><li class="digg"><a href="#">Share this on digg</a></li></ul></div>'
+    var page = index_el.attr('data-page');
+    var box = '<div id="' + id + '" class="pull_out citation_box"><ul><li class="link"><a href="/a/'+ $(".doc_number").text() + '/' + index +'">Link to this paragraph</a></li><li class="cite_volume"><strong>Citation</strong> ' + $(".metadata_list .volume").text() + ' FR ' + page + '</li><li class="cite_page"><strong>Page</strong> ' + page + '</li><li class="email"><a href="#">Email this</a></li><li class="twitter"><a href="#">Share this on Twitter</a></li><li class="facebook"><a href="#">Share this on Facebook</a></li><li class="digg"><a href="#">Share this on digg</a></li></ul></div>'
     $("#sidebar").append(box);
     var id_el = $("#" + id); 
     id_el.css({"top": index_el.position().top + 6, "right": 0}).data("id", index).data("sticky", false);
