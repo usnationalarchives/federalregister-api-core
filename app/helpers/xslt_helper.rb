b5122d88a9e8d667c7866db3838a8ec5fc9c23fb
Andrew Carpenter <acarpen@wested.org>
2010-06-23 06:34:35 -0700

    calculate current page correctly

--- app/helpers/xslt_helper.rb
+++ app/helpers/xslt_helper.rb
@@ -1,6 +1,6 @@
 module XsltHelper
-  def transform_xml(xml, stylesheet)
+  def transform_xml(xml, stylesheet, options = {})
     xslt  = Nokogiri::XSLT(File.read("#{RAILS_ROOT}/app/views/#{stylesheet}"))
-    xslt.transform(Nokogiri::XML(xml))
+    xslt.transform(Nokogiri::XML(xml), options.to_a.flatten)
   end
 end
