6087906047c8a1842fcdf12efa651736a978527b
Andrew Carpenter <acarpen@wested.org>
2010-06-23 17:39:15 -0700

    regulatory plan search

--- app/controllers/regulatory_plans_controller.rb
+++ app/controllers/regulatory_plans_controller.rb
@@ -22,16 +22,6 @@ class RegulatoryPlansController < ApplicationController
     render :layout => false
   end
   
-  def search
-    @search = RegulatoryPlanSearch.new(params)
-  end
-  
-  def search_facet
-    @search = RegulatoryPlanSearch.new(params)
-    facets = @search.send(params[:facet] + "_facets")
-    render :partial => "search/facet", :collection => facets, :layout => nil
-  end
-  
   def tiny_url
     regulatory_plan = RegulatoryPlan.find_by_regulation_id_number!(params[:regulation_id_number], :order => "issue DESC")
     redirect_to regulatory_plan_path(regulatory_plan), :status=>:moved_permanently
