0839ad96b9561ce464c48ed1883e61120903a578
Andrew Carpenter <acarpen@wested.org>
2010-06-30 08:37:16 -0700

    refactor excluding_parents to be agency class method

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -237,11 +237,6 @@ class Entry < ApplicationModel
     }
   end
   
-  def agencies_excluding_parents
-    parent_agency_ids = agencies.map(&:parent_id).compact
-    agencies.reject{|a| parent_agency_ids.include?(a.id) }.uniq
-  end
-  
   def curated_title
     self[:curated_title] || title
   end
