e14052f3dbedab01bd9263787e84ed3c5baf0607
Dave Augustine <dave@criticaljuncture.org>
2010-07-02 14:49:46 -0700

    Fixed homepage promo javascript for now

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -10,22 +10,18 @@ $(document).ready(function() {
   var slideshow = new Dragdealer('slideshow',
   {
   	steps: 6,
-  	loose: true,
-  	animationCallback: function(x,y) {
-  	  
-  	}  
-  	});
+  	loose: true
+  });
     
   $(".section_nav a").bind('click', function(event) {
     event.preventDefault();
     $(".section_nav li").removeClass("on");
     $(this).parent().addClass("on");
-    slideshow.setStep( $(".section_nav li").index( $(this).parent() )  + 1);
+    slideshow.setStep( $(".section_nav li").index( $(this).parent() )  + 1 );
   });
     
   var adjustedHigh = (parseFloat( $(".section_nav li").size()));
   var numRand = Math.floor(Math.random()*adjustedHigh);  
-  
   var item = $(".section_nav li a").get(numRand);
   $(item).trigger("click");
 
