f6abbf2a9fb82210c3e57db639e2e66539a7ef2a
Andrew Carpenter <acarpen@wested.org>
2010-07-04 08:48:33 -0700

    annotate models

--- app/models/agency_name_assignment.rb
+++ app/models/agency_name_assignment.rb
@@ -11,10 +11,22 @@
 =end Schema Information
 
 class AgencyNameAssignment < ApplicationModel
+  validates_presence_of :assignable, :agency_name
+
   belongs_to :agency_name
   belongs_to :assignable, :polymorphic => true
   belongs_to :entry, :foreign_key => :assignable_id
   
   has_one :agency_assignment, :foreign_key => :id, :dependent => :destroy
   acts_as_list :scope => 'assignable_id = #{assignable_id} AND assignable_type = \'#{assignable_type}\''
+  
+  after_create :create_agency_assignments
+
+  private
+  def create_agency_assignments
+    if agency_name.agency
+      assignable.agencies << agency_name.agency
+    end
+    true
+  end
 end
