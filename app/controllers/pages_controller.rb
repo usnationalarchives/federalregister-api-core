4487feb1648fb41785bf4edbd2cced57e4d8e6b2
Andrew Carpenter <acarpen@wested.org>
2010-07-13 07:09:53 -0700

    move policy pages under policy; support '-'; 404 on missing pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def show
-    render :action => params[:slug]
+    template = "pages/#{params[:path].gsub(/-/,'_')}.html.erb"
+    if template_exists?(template)
+      render :template => template
+    else
+      raise ActiveRecord::RecordNotFound
+    end
   end
 end
