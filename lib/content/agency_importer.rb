a0335d5befa2ebe595dacf7a65cf079b240ca86e
Andrew Carpenter <acarpen@wested.org>
2010-07-16 07:01:40 -0700

    import agency descriptions

--- lib/content/agency_importer.rb
+++ lib/content/agency_importer.rb
@@ -1,8 +1,8 @@
 module Content
   class AgencyImporter
     def perform
-      Agency.connection.execute("TRUNCATE agencies")
-      Agency.connection.execute("TRUNCATE agency_assignments")
+      # Agency.connection.execute("TRUNCATE agencies")
+      # Agency.connection.execute("TRUNCATE agency_assignments")
       # Agency.connection.execute("TRUNCATE agency_names")
       # Agency.connection.execute("TRUNCATE agency_name_assignments")
       
@@ -10,16 +10,21 @@ module Content
       FasterCSV.foreach("data/gpo_agencies.csv", :headers => :first_row) do |line|
         agency_data = line.to_hash
         
-        agency = Agency.create(:name => line["name"])
+        puts "handing #{line['agency_name']}"
+        agency = Agency.find_or_create_by_name(line["agency_name"])
+        agency.description = line["description"]
         
-        if line["parent_name"].present?
-          agencies_with_parents[agency] = line["parent_name"]
+        agency.save!
+        
+        if line["parent_agency_name"].present?
+          agencies_with_parents[agency] = line["parent_agency_name"]
         end
+        
       end
       
       agencies_with_parents.each_pair do |agency, parent_name|
         agency.parent = Agency.find_by_name(parent_name)
-        agency.save
+        agency.save!
       end
     end
   end
