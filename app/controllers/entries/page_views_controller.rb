ed965467a440f2a660f4b3bf5f92257f199af3ab
Andrew Carpenter <acarpen@wested.org>
2010-07-16 14:31:27 -0700

    try using X-Real-IP for client IP

--- app/controllers/entries/page_views_controller.rb
+++ app/controllers/entries/page_views_controller.rb
@@ -1,6 +1,6 @@
 class Entries::PageViewsController < ApplicationController
   def create
-    EntryPageView.create!(:entry_id => params[:id], :remote_ip => request.env['HTTP_X_FORWARDED_FOR'])
+    EntryPageView.create!(:entry_id => params[:id], :remote_ip => request.env['X-Real-IP'])
     render :nothing => true
   end
 end
