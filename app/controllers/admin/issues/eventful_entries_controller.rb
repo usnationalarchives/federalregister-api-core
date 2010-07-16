8805c4b46d7190b98537152b4cd036da1ceb9334
Andrew Carpenter <acarpen@wested.org>
2010-07-15 21:13:57 -0700

    eventful entries should match any of "public meeting", "public hearing", "town hall meeting", "web dialogue", "webinar"

--- app/controllers/admin/issues/eventful_entries_controller.rb
+++ app/controllers/admin/issues/eventful_entries_controller.rb
@@ -1,20 +1,21 @@
 class Admin::Issues::EventfulEntriesController < AdminController
+  EVENT_PHRASES = ["public meeting", "public hearing", "town hall meeting", "web dialogue", "webinar"]
   def index
     @publication_date = Date.parse(params[:issue_id])
     
     @entries = EntrySearch.new(
       :conditions => {
-        :term => '"public meeting"',
+        :term => "(#{EVENT_PHRASES.map{|phrase| "\"#{phrase}\""}.join('|')})",
         :date => @publication_date.to_s
       },
-      :per_page => 200,
-      :match_mode => :extended
+      :per_page => 200
     ).results
   end
   
   def show
     @publication_date = Date.parse(params[:issue_id])
     @entry = Entry.published_on(@publication_date).find_by_document_number!(params[:id])
+    
     @entry_text = render_to_string( :partial => "entries/abstract", :locals => {:entry => @entry} ) +
       render_to_string( :partial => "entries/full_text", :locals => {:entry => @entry} )
     @dates = PotentialDateExtractor.extract(@entry_text)
