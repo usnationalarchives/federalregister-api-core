9dac07b4d35f2240f933fe5badc5c61414ea1479
Andrew Carpenter <acarpen@wested.org>
2010-07-17 06:50:49 -0700

    fishing for the remote ip

--- app/controllers/entries/page_views_controller.rb
+++ app/controllers/entries/page_views_controller.rb
@@ -1,6 +1,6 @@
 class Entries::PageViewsController < ApplicationController
   def create
-    EntryPageView.create!(:entry_id => params[:id], :remote_ip => request.env['X-Real-IP'])
+    EntryPageView.create!(:entry_id => params[:id], :remote_ip => request.env['HTTP_X_FORWARDED_FOR'])
     render :nothing => true
   end
 end
