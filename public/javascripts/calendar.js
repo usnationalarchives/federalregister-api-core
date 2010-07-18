a4bae4f25971f6ba0a26000fd50831012ff40b52
Andrew Carpenter <acarpen@wested.org>
2010-07-18 10:46:49 -0700

    navigate to arbitrary dates

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -3,4 +3,26 @@ $(document).ready(function(){
     $('table.calendar').load($(this).attr('href'));
     return false;
   });
+  
+  $('#date_selector').submit(function(){
+    var form = $(this);
+    var path = $(this).attr('action');
+    $.ajax({
+      url: path,
+      data: {'search' : $(form).find('#search').val()},
+      complete: function(xmlHttp) {
+        var status = xmlHttp.status;
+        form.find('span.error').remove();
+        console.log(status);
+        if (status == '200') {
+          window.location = xmlHttp.responseText;
+        } else if (status == '422' || status == '404' ) {
+          form.append($("<span class='error'></span>").text(xmlHttp.responseText))
+        } else {
+          form.append("<span class='error'><strong>Unknown error.</strong></span>")
+        }
+      }
+    });
+    return false;
+  })
 });
