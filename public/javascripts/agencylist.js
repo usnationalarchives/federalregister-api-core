f002abb6a3b7b4f17d171e0a5d88d1fbd4e0760f
Dave Augustine <dave@criticaljuncture.org>
2010-07-18 13:31:10 -0700

    refinements to topic browse and agency browse

--- public/javascripts/agencylist.js
+++ public/javascripts/agencylist.js
@@ -2,20 +2,21 @@ $(document).ready(function() {
   
   //clear the live search so it's not confusing
   $("#agencies ul.filter li.livesearch input").removeAttr("value");
-
   
   $("#agencies li.livesearch input").bind("keyup", function(e){
     $("#agency_list > li").hide().find("a:regex('\\b" + $(this).attr("value") + "')").parent().show();
     $("#agencies").trigger('filter', $(this).val());  
   }).bind("focus", function(e){
-    $("#agencies ul.filter li").removeClass("on");
+    $("#agencies .filters ul.actions li").removeClass("on");
     $(this).parent().addClass("on");
   });
 
-  $("#agencies ul.filter li a").bind("click", function(e){
+
+
+  $("#agencies .filters .alpha li a").bind("click", function(e){
     e.preventDefault();
     
-    $("#agencies ul.filter li").removeClass("on");
+    $("#agencies .filters .alpha li").removeClass("on");
     $(this).parent().addClass("on");
         
     if($(this).parent().hasClass("all"))
@@ -34,10 +35,10 @@ $(document).ready(function() {
     $(this).parent().addClass("on");
   });
   
-  $(".agency_list_container .ordering a").bind('click', function(event) {
+  $(".agency_list_container .actions a").bind('click', function(event) {
     event.preventDefault();
     $(this).hasClass("asc") ?  $("#agency_list>li").tsort() :  $("#agency_list>li").tsort({order:"desc"});
-    $(".agency_list_container .ordering li").removeClass("on");
+    $(".agency_list_container .actions li").removeClass("on");
     $(this).parent().addClass("on");
   });
   
