04cb2831b46bcd2882861cd615310db90fb73f81
Andrew Carpenter <acarpen@wested.org>
2010-07-21 20:20:15 -0700

    add reddit & facebook title attributes

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -5,7 +5,7 @@ module LinkHelper
   end
   
   def link_to_facebook(url, title)
-    link_to "Facebook", "http://www.facebook.com/sharer.php?u=#{CGI.escape url}&t=#{CGI.escape title}", :target => :blank, :class => 'button list fb_link social facebook'
+    link_to "Facebook", "http://www.facebook.com/sharer.php?u=#{CGI.escape url}&t=#{CGI.escape title}", :target => :blank, :class => 'button list fb_link social facebook', :title => "Facebook"
   end
   
   def link_to_digg(url, title, description)
@@ -14,6 +14,6 @@ module LinkHelper
   end
   
   def link_to_reddit(url, title)
-    link_to "Reddit", "http://www.reddit.com/submit?url=#{CGI.escape url}&title=#{CGI.escape title}", :target => :blank, :class => 'button list reddit_link social reddit'
+    link_to "Reddit", "http://www.reddit.com/submit?url=#{CGI.escape url}&title=#{CGI.escape title}", :target => :blank, :class => 'button list reddit_link social reddit', :title => "Reddit"
   end
 end
