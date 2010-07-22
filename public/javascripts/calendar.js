ec695cb5735de61cf878f7782ae1d30b29bb4267
Dave Augustine <dave@criticaljuncture.org>
2010-07-21 21:23:19 -0700

    Fix for neseted calendar tags on by date. Multiple general IE6 Layout issues.

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('table.calendar .nav').live('click', function() {
-    $('table.calendar').load($(this).attr('href'));
+    $('#calendar_wrapper').load($(this).attr('href'));
     return false;
   });
   
