707de93a818ed41f063529f108d88924449b6de8
Dave Augustine <dave@criticaljuncture.org>
2010-07-23 21:24:28 -0700

    Ran all our code through JSLint and fixed whatever came up.

--- public/javascripts/sectionpage.js
+++ public/javascripts/sectionpage.js
@@ -1,16 +1,17 @@
-$(document).ready(function() {
-  
-  if( $("#comments-closing-opening").size() > 0){
-    $("#comments-closing-opening").addClass("active");
-    
-    $("#comments-closing-opening").tabs({
-      trackState: true,
-      srcPath: '/blank.html'
-    });
-    
-    $(".TOC a").bind('click', function(event) {
-      $(window).scrollTop( $("#comments-closing-opening").offset().top);
-    });
-  }
-  
+$(document).ready(function () {
+
+    if ($("#comments-closing-opening").size() > 0) {
+        $("#comments-closing-opening").addClass("active");
+
+        $("#comments-closing-opening").tabs({
+            trackState: true,
+            srcPath: '/blank.html'
+        });
+
+        $(".TOC a").bind('click',
+        function (event) {
+            $(window).scrollTop($("#comments-closing-opening").offset().top);
+        });
+    }
+
 });
