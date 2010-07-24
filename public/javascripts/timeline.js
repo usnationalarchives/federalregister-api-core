bf5b9b3098d0af54cfb7c7e701dd16f666730b6b
Dave Augustine <dave@criticaljuncture.org>
2010-07-23 21:24:28 -0700

    Ran all our code through JSLint and fixed whatever came up.

--- public/javascripts/timeline.js
+++ public/javascripts/timeline.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {  
+$(document).ready(function () {  
 
   var li_count = $(".timeline .timeline_list > li").size();
-  if ( li_count > 3 ) {
+  if (li_count > 3) {
     $(".track").show();
     var mask = document.getElementById('scroll-mask');
     var content = document.getElementById('scroll-content');
@@ -12,7 +12,7 @@ $(document).ready(function() {
       x: 1,
       xPrecision: content.offsetWidth,
       steps: li_count - 2,
-    	animationCallback: function(x, y)
+    	animationCallback: function (x, y)
     	{
     		var margin = x * (content.offsetWidth - mask.offsetWidth);
     		content.style.marginLeft = String(-margin) + 'px';
