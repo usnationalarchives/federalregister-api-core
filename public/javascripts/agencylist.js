774847e23474b73ff01811481c7409d63aa3acba
Dave Augustine <dave@criticaljuncture.org>
2010-07-23 20:08:08 -0700

    Agency List passed JSLint.

--- public/javascripts/agencylist.js
+++ public/javascripts/agencylist.js
@@ -1,54 +1,67 @@
-$(document).ready(function() {
-  
-  //clear the live search so it's not confusing
-  $("#agencies ul.filter li.livesearch input").val("");
-  
-  $("#agencies li.livesearch input").bind("keyup", function(e){
-    $("#agency_list > li").hide().find("a:regex('\\b" + $(this).attr("value") + "')").parent().show();
-    $("#agencies").trigger('filter', $(this).val());  
-  }).bind("focus", function(e){
-    $("#agencies .filters .alpha li").removeClass("on");
-    $(this).parent().addClass("on");
-  });
+"use strict";
+$(document).ready(function () {
+    //clear the live search so it's not confusing
+    $("#agencies ul.filter li.livesearch input").val("");
+
+    $("#agencies li.livesearch input").bind("keyup",
+    function (e) {
+        $("#agency_list > li").hide().find("a:regex('\\b" + $(this).attr("value") + "')").parent().show();
+        $("#agencies").trigger('filter', $(this).val());
+    }).bind("focus",
+    function (e) {
+        $("#agencies .filters .alpha li").removeClass("on");
+        $(this).parent().addClass("on");
+    });
 
 
 
-  $("#agencies .filters .alpha li a").bind("click", function(e){
-    e.preventDefault();
-    
-    $("#agencies .filters .alpha li").removeClass("on");
-    $(this).parent().addClass("on");
-        
-    if($(this).parent().hasClass("all"))
-      $("#agency_list > li").show()
-    else
-      $("#agency_list > li").hide().find("a:regex('^[" + $(this).html() + "]')").parent().show();
-      
-    $("#agencies").trigger('filter', $(this).text() );  
-  });
-  
-  
-  $(".sub_agencies a").bind("click", function(e){
-    e.preventDefault();
-    var parent = $(this).parent();
-    if( !parent.hasClass("on") ) {
-      $(".sub_agencies li").removeClass("on");
-      $(this).parent().toggleClass("on");
-      $("#agencies li > ul").toggle();
-    }
-  });
-  
-  $(".agency_list_container .actions a").bind('click', function(e) {
-    e.preventDefault();
-    $(this).hasClass("asc") ?  $("#agency_list>li").tsort() :  $("#agency_list>li").tsort({order:"desc"});
-    $(".agency_list_container .actions li").removeClass("on");
-    $(this).parent().addClass("on");
-  });
-  
-  
-  $("#agencies").bind('filter', function( event, item ){
-    $("#agency_count").html( $("#agency_list > li:visible").size() );
-    $("h1.title span").text( 'Agencies - ' + item );
-  });
+    $("#agencies .filters .alpha li a").bind("click",
+    function (e) {
+        e.preventDefault();
+
+        $("#agencies .filters .alpha li").removeClass("on");
+        $(this).parent().addClass("on");
+
+        if ($(this).parent().hasClass("all")) {
+            $("#agency_list > li").show();
+        }
+        else {
+            $("#agency_list > li").hide().find("a:regex('^[" + $(this).html() + "]')").parent().show();
+        }
+        $("#agencies").trigger('filter', $(this).text());
+    });
+
+
+    $(".sub_agencies a").bind("click",
+    function (e) {
+        e.preventDefault();
+        var parent = $(this).parent();
+        if (!parent.hasClass("on")) {
+            $(".sub_agencies li").removeClass("on");
+            $(this).parent().toggleClass("on");
+            $("#agencies li > ul").toggle();
+        }
+    });
+
+    $(".agency_list_container .actions a").bind('click',
+    function (e) {
+        e.preventDefault();
+        if ($(this).hasClass("asc")) {
+            $("#agency_list>li").tsort();
+        } else {
+            $("#agency_list>li").tsort({
+                order: "desc"
+            });
+        }
+        $(".agency_list_container .actions li").removeClass("on");
+        $(this).parent().addClass("on");
+    });
+
+
+    $("#agencies").bind('filter',
+    function (event, item) {
+        $("#agency_count").html($("#agency_list > li:visible").size());
+        $("h1.title span").text('Agencies - ' + item);
+    });
 
 });
