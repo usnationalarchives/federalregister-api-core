21d1f5eaa2a5cb793ad581a55a78290bd89edaf8
Andrew Carpenter <acarpen@wested.org>
2010-08-29 12:26:11 -0700

    add basic sphinx delta index support

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,6 +9,7 @@
   place_id                 :integer(4)
   remote_call_in_available :boolean(1)
   event_type               :string(255)
+  delta                    :boolean(1)      default(TRUE), not null
 
 =end Schema Information
 
@@ -87,6 +88,8 @@ class Event < ApplicationModel
       "abstract" => 50,
       "full_text" => 25,
     }
+    
+    set_property :delta => true
   end
   
 end
