0855e54e74e3743eeaf92ffe0babf3d30feb885a
Andrew Carpenter <acarpen@wested.org>
2010-11-14 16:10:53 -0800

    annotate models

--- app/models/entry_regulation_id_number.rb
+++ app/models/entry_regulation_id_number.rb
@@ -1,3 +1,13 @@
+=begin Schema Information
+
+ Table name: entry_regulation_id_numbers
+
+  id                   :integer(4)      not null, primary key
+  entry_id             :integer(4)
+  regulation_id_number :string(255)
+
+=end Schema Information
+
 class EntryRegulationIdNumber < ApplicationModel
   belongs_to :entry
   has_many :regulatory_plans,
