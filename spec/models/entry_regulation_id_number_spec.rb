5e800143723794903d749f8a05a8f69a90d50935
Andrew Carpenter <acarpen@wested.org>
2010-11-14 16:10:53 -0800

    annotate models

--- spec/models/entry_regulation_id_number_spec.rb
+++ spec/models/entry_regulation_id_number_spec.rb
@@ -1,3 +1,13 @@
+=begin Schema Information
+
+ Table name: entry_regulation_id_numbers
+
+  id                   :integer(4)      not null, primary key
+  entry_id             :integer(4)
+  regulation_id_number :string(255)
+
+=end Schema Information
+
 require 'spec_helper'
 
 describe EntryRegulationIdNumber do
