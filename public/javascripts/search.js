d5d38d3b16254daba291f25a27a8909525b6e052
Andrew Carpenter <acarpen@wested.org>
2010-11-20 11:18:06 -0800

    #131 AJAX callback for the number of matching results

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,4 +1,60 @@
 $(document).ready(function () {
+    var populate_expected_results = function (count) {
+        $('#expected_result_count').text(count);
+    }
+    
+    var indicate_loading = function() {
+        $('#expected_result_count').text('loading');
+    }
+    
+    // ajax-y lookup of number of expected results
+    var calculate_expected_results = function () {
+        var form = $('#entry_search_form');
+        var url = '/articles/search/results.js?' + form.serialize();
+        
+        var cache = form.data('count_cache') || {};
+        
+        // don't go back to the server if you've checked this before
+        if (cache[url]) {
+            populate_expected_results(cache[url])
+        } else {
+            // record that this is the current results we're looking for
+            form.data('count_current_url', url);
+            indicate_loading();
+            
+            $.getJSON(url, function(data){
+                var form = $('#entry_search_form');
+                var cache = form.data('count_cache') || {};
+                cache[url] = data.count;
+                form.data('count_cache', cache);
+                
+                // don't show number if user has already made another request
+                if (form.data('count_current_url') == url) {
+                    populate_expected_results(data.count);
+                }
+            });
+        }
+    };
+    
+    
+    $('#entry_search_form select').blur(calculate_expected_results);
+    $('#entry_search_form input[type=checkbox]').click(calculate_expected_results)
+    
+    // basic check for pause between events
+    var typewatch = (function(){
+      var timer = 0;
+      return function(callback, ms){
+        clearTimeout (timer);
+        timer = setTimeout(callback, ms);
+      }  
+    })();
+    $('#entry_search_form input[type=text]').keyup(function () {
+        // only trigger if stopped typing for more than half a second
+        typewatch(function () {
+            calculate_expected_results();
+        }, 500);
+    });
+    
     $('a.load_facet').live('click',
     function () {
         var anchor = $(this);
