415943c54b37bd1a867d6d6ede17003dc81b06dc
Dave Augustine <dave@criticaljuncture.org>
2010-11-28 15:44:59 -0800

    Only hide dates that are empty

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -175,7 +175,9 @@ $(document).ready(function () {
         trigger: '.results a.add_to_calendar'
     });
     
-    $(".date_options .date").hide();
+    $(".date_options .date :input[value!='']").parent().hide();
+    
+    
     $("input[data-show-field]").bind('change', function(event) {
       var parent_fieldset = $(this).closest("fieldset");
       parent_fieldset.find(".date").hide().find(":input").disable(); 
