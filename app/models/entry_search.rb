6f50949fe11f50542676768137192aa793aeb3d3
Andrew Carpenter <acarpen@wested.org>
2011-01-03 17:19:07 -0800

    support 'harvard-style' citation searches

--- app/models/entry_search.rb
+++ app/models/entry_search.rb
@@ -183,7 +183,7 @@ class EntrySearch < ApplicationSearch
   
   def matching_entry_citation
     if term.present?
-      term.scan(/^\s*(\d+)\s*F\.?R\.?\s*(\d+)\s*$/i) do |volume, page|
+      term.scan(/^\s*(\d+)\s*(?:F\.?R\.?|Fed\.?\s*Reg\.?)\s*(\d+)\s*$/i) do |volume, page|
         return Citation.new(:citation_type => "FR", :part_1 => volume.to_i, :part_2 => page.to_i)
       end
     end
