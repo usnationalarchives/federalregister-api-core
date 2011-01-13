1350d9f3883ac3961284f3d61358b502ca9620c6
Andrew Carpenter <acarpen@wested.org>
2011-01-13 08:30:32 -0800

    finish delayed_job installation

--- db/schema.rb
+++ db/schema.rb
@@ -108,6 +108,21 @@ ActiveRecord::Schema.define(:version => 20110203000130) do
   add_index "citations", ["cited_entry_id", "citation_type", "source_entry_id"], :name => "cited_citation_source"
   add_index "citations", ["source_entry_id", "citation_type", "cited_entry_id"], :name => "source_citation_cited"
 
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+
   create_table "entries", :force => true do |t|
     t.text     "title"
     t.text     "abstract"
