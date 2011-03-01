244da4940e4a5f5f528e53ac52a1ed8dde8f4de8
Dave Augustine <dave@criticaljuncture.org>
2011-03-01 00:01:51 -0800

    Changing the rss icon to subscription icons

--- public/javascripts/rss.js
+++ public/javascripts/rss.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('a.rss').live('click',
+    $('a.rss, a.subscription').live('click',
     function () {
         generate_dialog();
         $('#modal').centerScreen().jqmShow();
@@ -18,6 +18,9 @@ $(document).ready(function () {
               '   <li>',
               '     <h4><a href="<%= this.href %>" title="Add to your Feed Reader"><%= this.title %></a></h4>',
               '      <ul class="horizontal">',
+              '         <li><a href="http://add.my.yahoo.com/rss?url=<%= escape(this.href)  %>" title="Add feed to Yahoo"><img src="http://us.i1.yimg.com/us.yimg.com/i/us/my/addtomyyahoo4.gif" /></a></li>',
+              '         <li><a href="http://fusion.google.com/add?feedurl=<%= escape(this.href) %>" title="Add feed to Google"><img src="http://buttons.googlesyndication.com/fusion/add.gif" /></a></li>',
+              '         <li><a href="http://www.netvibes.com/subscribe.php?url=<%= escape(this.href) %>" title="Add feed to Netvibes"><img src="http://www.netvibes.com/img/add2netvibes.gif" /></a></li>',
               '         <% if(this.subscription_action) { %>',
               '         <li>via email: ',
               '           <form action="<%= this.subscription_action %>" method="post">',
@@ -25,10 +28,7 @@ $(document).ready(function () {
               '             <input type="submit" value="Subscribe">',
               '           </form>',
               '         </li>',
-              '         <% } %>',
-              '         <li><a href="http://add.my.yahoo.com/rss?url=<%= escape(this.href)  %>" title="Add feed to Yahoo"><img src="http://us.i1.yimg.com/us.yimg.com/i/us/my/addtomyyahoo4.gif" /></a></li>',
-              '         <li><a href="http://fusion.google.com/add?feedurl=<%= escape(this.href) %>" title="Add feed to Google"><img src="http://buttons.googlesyndication.com/fusion/add.gif" /></a></li>',
-              '         <li><a href="http://www.netvibes.com/subscribe.php?url=<%= escape(this.href) %>" title="Add feed to Netvibes"><img src="http://www.netvibes.com/img/add2netvibes.gif" /></a></li>',
+              '         <% } %>',              
               '     </ul>',
               '   </li>',
               '  <% }); %>',
