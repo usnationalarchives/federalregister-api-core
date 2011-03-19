9eb8d3055b509342f5bd27b072f2385ecb5a90f3
Bob Burbach <bob.burbach@gmail.com>
2011-03-18 17:32:21 -0700

    Upgrade to async google analytics, track social features, and track font controls individually. [#293 #252 #181 state:ready_for_testing]

--- public/javascripts/analytics.js
+++ public/javascripts/analytics.js
@@ -1,45 +1,79 @@
 $(document).ready(function(){
-  $("a[href^=http]").bind('click', function(){
+  $("a[href^=http]").not('.button.social').bind('click', function(){
     var url = $(this).attr('href');
     if (! url.match(/federalregister\.gov/)) {
       var path = "/external/" + escape(url);
-      pageTracker._trackPageview(path);
+      _gaq.push(['_trackEvent', 'Links', 'External', path]);
     }
   });
   
+  
+  // track use of calendar features
   $("a.add_to_calendar").bind('click', function() {
     var path = $(this).attr('href') + '/add_to_calendar';
-    pageTracker._trackPageview(path);
+    _gaq.push(['_trackEvent', 'Calendar', 'Add', path]);
   });
   
   $(".result_set.events a.download").bind('click', function() {
     var path = $(this).attr('href');
-    pageTracker._trackPageview(path);
+    _gaq.push(['_trackEvent', 'Calendar', 'Download', path]);
   });
+  
   
   $("a.rss").bind('click', function(){
     var path = window.location.pathname + "/rss_modal";
-    pageTracker._trackPageview(path);
+    _gaq.push(['_trackEvent', 'RSS', 'View Modal', path]);
     return true;
   });
   
   $("a.shorter_url").bind('click', function() {
     var path = $(this).attr('href');
-    pageTracker._trackPageview(path);
+    _gaq.push(['_trackEvent', 'Short URL', 'Shorten', path]);
   });
   
   $('#disclaimer a').bind('click', function(){
     var path = window.location.pathname + "/legal_disclaimer";
-    pageTracker._trackPageview(path);
+    _gaq.push(['_trackEvent', 'Legal Disclaimer', 'View', path]);
     return true;
   });
   
-  $(["increase", "decrease", "reset", "serif", "sans"]).each(function(i, css_class){
-    $('.' + css_class).bind('click', function() {
-      var path = window.location.pathname + "/font/" + css_class;
-      pageTracker._trackPageview(path);
-    });
+  // track use of font controls
+  $("a.increase").bind('click', function() {
+    var path = window.location.pathname + "/font/increase";
+    _gaq.push(['_trackEvent', 'Font Controls', 'Increase', path]);
   });
   
-  // articlepage.js has additional analytics
+  $("a.decrease").bind('click', function() {
+    var path = window.location.pathname + "/font/decrease";
+    _gaq.push(['_trackEvent', 'Font Controls', 'Decrease', path]);
+  });
+  
+  $("a.reset").bind('click', function() {
+    var path = window.location.pathname + "/font/reset";
+    _gaq.push(['_trackEvent', 'Font Controls', 'Reset', path]);
+  });
+  
+  $("a.serif").bind('click', function() {
+    var path = window.location.pathname + "/font/serif";
+    _gaq.push(['_trackEvent', 'Font Controls', 'Serif', path]);
+  });
+  
+  $("a.sans").bind('click', function() {
+    var path = window.location.pathname + "/font/sans";
+    _gaq.push(['_trackEvent', 'Font Controls', 'Sans', path]);
+  });
+  
+  // track use of social features
+  $("a.button.social.email").bind('click',
+  function (event) {
+    _gaq.push(['_trackEvent', 'Social', 'Email', document.location.pathname]);
+  });
+  $("a.button.social.twitter").bind('click',
+  function (event) {
+    _gaq.push(['_trackEvent', 'Social', 'Twitter', document.location.pathname]);
+  });
+  $("a.button.social.facebook").bind('click',
+  function (event) {
+    _gaq.push(['_trackEvent', 'Social', 'Facebook', document.location.pathname]);
+  });
 });
