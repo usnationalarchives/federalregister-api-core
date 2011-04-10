ccef967f29e5423eefa03f2727ca87d54ff50f57
Andrew Carpenter <acarpen@wested.org>
2011-04-10 00:12:14 +0000

    use romans for chapter references

--- app/helpers/text_helper.rb
+++ app/helpers/text_helper.rb
@@ -20,4 +20,9 @@ module TextHelper
       end
     end
   end
+
+  ROMANS = %w(undef I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI XVII XVIII XIX XX XXI XXII XXIII XXIV XXV XXVI XXVII XXVIII XXIX XXX)
+  def number_to_roman(i)
+    ROMANS[i.to_i]
+  end
 end
