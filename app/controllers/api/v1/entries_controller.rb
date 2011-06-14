7cda99cfaad1ca6f514ab8a93ce5271a00e99f6b
Andrew Carpenter <acarpen@wested.org>
2011-06-13 20:43:35 -0400

    move logic inside respond_to block

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -1,36 +1,36 @@
 class Api::V1::EntriesController < ApiController
   def index
-    search = EntrySearch.new(params)
-    data = { :count => search.count }
-    
-    if search.count > 0 && search.results.count > 0
-      data[:total_pages] = search.results.total_pages
-      
-      if search.results.next_page
-        data[:next_page_url] = url_for(params.merge(:page => search.results.next_page))
-      end
-      
-      if search.results.previous_page
-        data[:previous_page_url] = url_for(params.merge(:page => search.results.previous_page))
-      end
-      
-      data[:results] = search.results.map do |entry|
-        {
-          :title            => entry.title,
-          :type             => entry.entry_type,
-          :abstract         => entry.abstract,
-          :excerpts         => entry.excerpts.full_text || entry.excerpts.abstract,
-          :document_number  => entry.document_number,
-          :url              => entry_url(entry),
-          :pdf_url          => entry.source_url('pdf'),
-          :publication_date => entry.publication_date,
-          :agencies         => entry.agency_names.map(&agency_proc)
-        }
-      end
-    end
-    
     respond_to do |wants|
       wants.json do
+        search = EntrySearch.new(params)
+        data = { :count => search.count }
+        
+        if search.count > 0 && search.results.count > 0
+          data[:total_pages] = search.results.total_pages
+          
+          if search.results.next_page
+            data[:next_page_url] = url_for(params.merge(:page => search.results.next_page))
+          end
+          
+          if search.results.previous_page
+            data[:previous_page_url] = url_for(params.merge(:page => search.results.previous_page))
+          end
+          
+          data[:results] = search.results.map do |entry|
+            {
+              :title            => entry.title,
+              :type             => entry.entry_type,
+              :abstract         => entry.abstract,
+              :excerpts         => entry.excerpts.full_text || entry.excerpts.abstract,
+              :document_number  => entry.document_number,
+              :url              => entry_url(entry),
+              :pdf_url          => entry.source_url('pdf'),
+              :publication_date => entry.publication_date,
+              :agencies         => entry.agency_names.map(&agency_proc)
+            }
+          end
+        end
+        
         render :json => data
       end
     end
