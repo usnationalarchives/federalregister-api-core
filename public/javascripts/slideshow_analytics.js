537c1b1ceb875184bb1fc0a7da5ed4bec6742616
Bob Burbach <bob.burbach@gmail.com>
2011-07-22 13:54:08 -0700

    Add tracking analytics to the home page. [#422]

---
+++ public/javascripts/slideshow_analytics.js
@@ -0,0 +1,34 @@
+function promo_selected(link) {
+  $(link).closest('li').hasClass('on') ? 1 : 0 ;
+}
+
+$(document).ready(function(){
+  /*
+   *  Top news slideshow highlight box analytics 
+   */
+
+  // SECTIONS
+  $(".aside_box.section_nav a#promo_money").bind('click', function() {
+    _gaq.push(['_trackEvent', 'Slide Show', 'Sections', "Money", promo_selected(this)]);
+  });
+
+  $(".aside_box.section_nav a#promo_environment").bind('click', function() {
+    _gaq.push(['_trackEvent', 'Slide Show', 'Sections', "Environment", promo_selected(this)]);
+  });
+
+  $(".aside_box.section_nav a#promo_world").bind('click', function() {
+    _gaq.push(['_trackEvent', 'Slide Show', 'Sections', "World", promo_selected(this)]);
+  });
+
+  $(".aside_box.section_nav a#promo_science-and-technology").bind('click', function() {
+    _gaq.push(['_trackEvent', 'Slide Show', 'Sections', "Science and Technology", promo_selected(this)]);
+  });
+
+  $(".aside_box.section_nav a#business-and-industry").bind('click', function() {
+    _gaq.push(['_trackEvent', 'Slide Show', 'Sections', "Business and Industry", promo_selected(this)]);
+  });
+
+  $(".aside_box.section_nav a#health-and-public-welfare").bind('click', function() {
+    _gaq.push(['_trackEvent', 'Slide Show', 'Sections', "Health and Public Welfare", promo_selected(this)]);
+  });
+});
