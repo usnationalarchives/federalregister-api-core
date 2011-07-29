6d4ad0d41bf6323c0fce417596944f2728627988
Andrew Carpenter <acarpen@wested.org>
2011-07-29 12:04:02 -0700

    fix api error messaging

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -9,15 +9,18 @@ class ApiController < ApplicationController
     end
   end
   
+  rescue_from Exception, :with => :server_error
   def server_error(exception)
     notify_hoptoad(exception)
     render :json => {:status => 500, :message => "Internal Server Error"}, :status => 500
   end
-
+  
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found      
   def record_not_found
     render :json => {:status => 404, :message => "Record Not Found"}, :status => 404
   end
-
+  
+  rescue_from ActionController::MethodNotAllowed, :with => :method_not_allowed
   def method_not_allowed
     render :json => {:status => 405, :message => "Method Not Allowed"}, :status => 405
   end
