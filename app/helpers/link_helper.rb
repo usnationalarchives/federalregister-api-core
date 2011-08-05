9297ef716cebc566e9b8dd96f57eb2164fe1bea2
Bob Burbach <bob.burbach@gmail.com>
2011-08-05 12:24:28 -0700

    Add some padding to short url so clippy isn't on top of it. Abstract path to clippy.swf in helper.

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -18,18 +18,19 @@ module LinkHelper
   end
 
   def clippy(text, bgcolor='#F5F8F9')
+    clippy_path = "/flash/clippy.swf"
     html = <<-EOF
       <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
               width="110"
               height="14"
               id="clippy" >
-      <param name="movie" value="/flash/clippy.swf"/>
+      <param name="movie" value="#{clippy_path}"/>
       <param name="allowScriptAccess" value="always" />
       <param name="quality" value="high" />
       <param name="scale" value="noscale" />
       <param NAME="FlashVars" value="text=#{text}">
       <param name="bgcolor" value="#{bgcolor}">
-      <embed src="/flash/clippy.swf"
+      <embed src="#{clippy_path}"
              width="110"
              height="14"
              name="clippy"
