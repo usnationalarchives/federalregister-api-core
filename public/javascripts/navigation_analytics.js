f4cd25646c6cd6d3965937cfe58d7d83a5e60113
Andrew Carpenter <acarpen@wested.org>
2011-08-05 09:14:57 -0700

    remove old header

--- public/javascripts/navigation_analytics.js
+++ public/javascripts/navigation_analytics.js
@@ -62,54 +62,6 @@ $(document).ready(function(){
   $("div.nav li.inline_search form").bind('submit', function() {
     _gaq.push(['_trackEvent', 'Navigation', 'Search', "Inline Search"]);
   });
-
-
-  /* 
-   *  Old header analytics 
-   */
-
-  // SECTIONS
-  $("#primary_nav li.money a").bind('click', function() {
-    _gaq.push(['_trackEvent', 'Navigation', 'Sections', "Money"]);
-  });
-
-  $("#primary_nav li.environment a").bind('click', function() {
-    _gaq.push(['_trackEvent', 'Navigation', 'Sections', "Environment"]);
-  });
-
-  $("#primary_nav li.world a").bind('click', function() {
-    _gaq.push(['_trackEvent', 'Navigation', 'Sections', "World"]);
-  });
-
-  $("#primary_nav li.science-and-technology a").bind('click', function() {
-    _gaq.push(['_trackEvent', 'Navigation', 'Sections', "Science and Technology"]);
-  });
-
-  $("#primary_nav li.business-and-industry a").bind('click', function() {
-    _gaq.push(['_trackEvent', 'Navigation', 'Sections', "Business and Industry"]);
-  });
-
-  $("#primary_nav li.health-and-public-welfare a").bind('click', function() {
-    _gaq.push(['_trackEvent', 'Navigation', 'Sections', "Health and Public Welfare"]);
-  });
-
-  // STATIC
-  $("#primary_nav li.blog a").bind('click', function() {
     _gaq.push(['_trackEvent', 'Navigation', 'Blog', "Blog"]);
   });
-
-  // NAVIGATION SEARCH FORM
-  $("div#header #search_bar #search_container form").bind('submit', function() {
-    _gaq.push(['_trackEvent', 'Navigation', 'Search', "Inline Search"]);
-  });
-
-  // BROWSE
-  $("#primary_nav #search_bar #browse_container a#agencies-browse").bind('click', function() {
-    _gaq.push(['_trackEvent', 'Navigation', 'Browse', "Agencies"]);
-  });
-
-  $("#primary_nav #search_bar #browse_container a#topics-browse").bind('click', function() {
-    _gaq.push(['_trackEvent', 'Navigation', 'Browse', "Topics"]);
-  });
-
 });
