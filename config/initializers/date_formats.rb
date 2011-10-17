2ede9d0e2b14ab66e613eb8fce79a2e2ddcf73e8
Bob Burbach <bob.burbach@gmail.com>
2011-10-17 14:39:36 -0700

    Fix date format or pi section last updated at. Update styles so page looks a bit more uniform/consistent visually. Move subscribe to the top of the side bar.

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -17,6 +17,6 @@ ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
 
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
   :HMS_Z => "%H-%M-%S_%Z",
-  :time_then_date => lambda { |time| time.strftime("%h:%M %p, on %A, %B #{time.day.ordinalize}, %Y") },
+  :time_then_date => lambda { |time| time.strftime("%I:%M %p, on %A, %B #{time.day.ordinalize}, %Y") },
   :short_date_then_time => "%m/%d/%Y at %I:%M %p"
 )
