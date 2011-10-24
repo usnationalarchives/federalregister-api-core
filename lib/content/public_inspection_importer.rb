789edfa3e5bd69c54dbaf6f200b4a01642465f46
Andrew Carpenter <acarpen@wested.org>
2011-10-24 08:31:29 -0700

    more tweaks to PI parsing

--- lib/content/public_inspection_importer.rb
+++ lib/content/public_inspection_importer.rb
@@ -50,9 +50,11 @@ module Content
     def initialize(attributes)
       @pi = PublicInspectionDocument.find_or_initialize_by_document_number(attributes.delete(:document_number))
       @new_record = @pi.new_record?
+      url = attributes.delete(:url)
       attributes.each_pair do |attr,val|
         send("#{attr}=", val)
       end
+      self.url = url
     end
 
     def new_record?
@@ -231,6 +233,7 @@ module Content
         when :toc_subject
           @toc_subject = @str
           @toc_doc = nil
+          @title = ''
         when :details
           @details = @str
 
@@ -256,7 +259,6 @@ module Content
             :url             => @url
           }
           @document_number = nil
-          @title = ''
         end
       end
 
