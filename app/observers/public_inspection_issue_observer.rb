e4b5b0031f91b4813bc1985fe256514906bd731c
Andrew Carpenter <acarpen@wested.org>
2011-10-24 08:14:11 -0700

    purge current PI page

--- app/observers/public_inspection_issue_observer.rb
+++ app/observers/public_inspection_issue_observer.rb
@@ -3,5 +3,6 @@ class PublicInspectionIssueObserver < CacheExpirer
 
   def after_save(public_inspection_issue)
     purge_cache(public_inspection_documents_by_date_path(public_inspection_issue.publication_date))
+    purge_cache(public_inspection_documents_path)
   end
 end
