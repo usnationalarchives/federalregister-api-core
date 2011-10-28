01139d9923ba06a04f077d2fa4c36be82aa25438
Andrew Carpenter <acarpen@wested.org>
2011-10-28 13:30:34 -0700

    put with_banner PI PDF style in root of bucket

--- app/models/public_inspection_document.rb
+++ app/models/public_inspection_document.rb
@@ -28,9 +28,9 @@ class PublicInspectionDocument < ApplicationModel
   has_attached_file :pdf,
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root}/config/amazon.yml",
-                    :path => ":style/:document_number.pdf",
                     :s3_alias_url => "http://public-inspection.#{APP_HOST_NAME}.s3.amazonaws.com/",
                     :bucket => "public-inspection.#{APP_HOST_NAME}",
+                    :path => ":style_if_not_with_banner:document_number.pdf",
                     :default_style => :with_banner,
                     :styles => {
                       :with_banner => { :processors => [:permalink_banner_adder] },
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,3 +5,13 @@ end
 Paperclip.interpolates :document_number do |attachment, style|
   attachment.instance.document_number
 end
+
+Paperclip.interpolates :style_if_not_with_banner do |attachment, style|
+  case style
+  when :with_banner
+    ''
+  else
+    "#{style}/"
+  end
+end
+
