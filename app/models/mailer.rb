08f76a1d0afc27d03cfd3f14d831153689f63d5c
Andrew Carpenter <acarpen@wested.org>
2011-10-28 18:33:35 -0700

    remove RouteBuilder from mailer and use basic routes

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,6 +1,6 @@
 class Mailer < ActionMailer::Base
   include SendGrid
-  include RouteBuilder
+  # include RouteBuilder
   helper :entry, :text
   
   sendgrid_enable :opentracking, :clicktracking, :ganalytics
