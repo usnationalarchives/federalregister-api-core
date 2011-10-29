88c75b5e56b15bb380e99a9ad36acc7009857db1
Andrew Carpenter <acarpen@wested.org>
2011-10-29 10:56:19 -0700

    show API errors in development mode

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -9,18 +9,18 @@ class ApiController < ApplicationController
     end
   end
   
-  rescue_from Exception, :with => :server_error
+  rescue_from Exception, :with => :server_error if RAILS_ENV != 'development'
   def server_error(exception)
     notify_hoptoad(exception)
     render :json => {:status => 500, :message => "Internal Server Error"}, :status => 500
   end
   
-  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found      
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found if RAILS_ENV != 'development'
   def record_not_found
     render :json => {:status => 404, :message => "Record Not Found"}, :status => 404
   end
   
-  rescue_from ActionController::MethodNotAllowed, :with => :method_not_allowed
+  rescue_from ActionController::MethodNotAllowed, :with => :method_not_allowed if RAILS_ENV != 'development'
   def method_not_allowed
     render :json => {:status => 405, :message => "Method Not Allowed"}, :status => 405
   end
