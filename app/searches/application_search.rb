d9bbbe271253b0a67804ea6676e8b2224044f95f
Andrew Carpenter <acarpen@wested.org>
2011-11-03 08:20:49 -0700

    fix hyphens in conditions as well as primary term; this will fix elements like docket numbers

--- app/searches/application_search.rb
+++ app/searches/application_search.rb
@@ -142,7 +142,7 @@ filter_name.to_s.sub(/_ids?$/,'').classify.constantize.find_all_by_id(ids.flatte
   def sphinx_conditions
     sphinx_conditions = {}
     @filters.select{|f| f.sphinx_type == :conditions }.each do |filter|
-      sphinx_conditions[filter.sphinx_attribute] = filter.sphinx_value
+      sphinx_conditions[filter.sphinx_attribute] = TermPreprocessor.process_term(filter.sphinx_value)
     end
     
     sphinx_conditions
