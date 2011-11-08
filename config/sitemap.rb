f968aa2157a7639a68053a91539e1d3d1712575f
Andrew Carpenter <acarpen@wested.org>
2011-11-07 16:12:06 -0800

    update sitemap to have searches, priorities, changefreq

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -13,52 +13,58 @@ SitemapGenerator::Sitemap.add_links do |sitemap|
   # Defaults: :priority => 0.5, :changefreq => 'weekly', 
   #           :lastmod => Time.now, :host => default_host
   
+  # SEARCHES
+  sitemap.add entries_search_path, :priority => 1
+#  sitemap.add public_inspection_search_path, :priorty => 0.75 
+#  sitemap.add events_search_path, :priority => 0.5
+#  sitemap.add regulatory_plans_search_path, :priority => 0.5
+
   # SECTIONS
-  Section.find_each do |section|
-    sitemap.add section_path(section)
-    sitemap.add about_section_path(section)
-  end
+#  Section.find_each do |section|
+#    sitemap.add section_path(section), :priority => 0.75, :changefreq => 'daily'
+#    sitemap.add about_section_path(section), :priority => 0.25, :changefreq => 'daily'
+#  end
 
   # CANNED SEARCHES
-  CannedSearch.active.each do |canned_search|
-    sitemap.add canned_search_path(canned_search)
-  end
+#  CannedSearch.active.each do |canned_search|
+#    sitemap.add canned_search_path(canned_search), :priority => 0.75, :changefreq => 'daily'
+#  end
   
   # ENTRIES
-  Entry.scoped(:joins => :issue, :conditions => "issues.completed_at IS NOT NULL").find_each do |entry|
-    sitemap.add entry_path(entry)
-  end
+#  Entry.scoped(:joins => :issue, :conditions => "issues.publication_date > '2011-10-01' && issues.completed_at IS NOT NULL").find_each do |entry|
+#    sitemap.add entry_path(entry), :changefreq => 'monthly', :lastmod => entry.updated_at
+#  end
   
-  Issue.completed.find_each do |issue|
-    sitemap.add entries_by_date_path(issue.publication_date)
-  end
+#  Issue.completed.find_each do |issue|
+#    sitemap.add entries_by_date_path(issue.publication_date), :priority => 0.75
+#  end
   
-  sitemap.add entries_current_issue_path
+#  sitemap.add entries_current_issue_path, :priority => 1.0, :changefreq => 'daily'
  
-  PublicInspectionIssue.published.find_each do |issue|
-    sitemap.add public_inspection_documents_by_date_path(issue.publication_date)
-  end
+#  PublicInspectionIssue.published.find_each do |issue|
+#    sitemap.add public_inspection_documents_by_date_path(issue.publication_date), :priority => 0.75
+#  end
 
-  sitemap.add public_inspection_documents_path
+#  sitemap.add public_inspection_documents_path, :priority => 1.0, :changefreq => 'hourly'
 
-  PublicInspectionDocument.unpublished.find_each do |document|
-    sitemap.add entry_path(document)
-  end
+#  PublicInspectionDocument.unpublished.find_each do |document|
+#    sitemap.add entry_path(document)
+#  end
 
   # TOPICS
-  sitemap.add topics_path
-  Topic.find_each do |topic|
-    sitemap.add topic_path(topic)
-  end
+#  sitemap.add topics_path
+#  Topic.find_each do |topic|
+#    sitemap.add topic_path(topic), :changefreq => 'daily'
+#  end
   
   # AGENCIES
-  sitemap.add agencies_path
-  Agency.find_each do |agency|
-    sitemap.add agency_path(agency)
-  end
+#  sitemap.add agencies_path
+#  Agency.find_each do |agency|
+#    sitemap.add agency_path(agency), :priority => 0.75, :changefreq => 'daily'
+#  end
   
   # REGULATIONS
-  RegulatoryPlan.find_each do |regulatory_plan|
-    sitemap.add regulatory_plan_path(regulatory_plan)
-  end
+#  RegulatoryPlan.find_each do |regulatory_plan|
+#    sitemap.add regulatory_plan_path(regulatory_plan), :changefreq => 'daily'
+#  end
 end
