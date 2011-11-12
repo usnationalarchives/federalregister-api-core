0a1d0725dbf632cbeebf33332c097c6f11a3ea57
Andrew Carpenter <acarpen@wested.org>
2011-11-11 16:19:31 -0800

    add tooltips to share links

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,11 +1,11 @@
 module LinkHelper
   def link_to_twitter(status)
     href = "http://twitter.com/home?status=#{CGI.escape status}"
-    link_to('Twitter', href, :target => :blank, :title => 'Twitter', :class => 'button list social twitter')
+    link_to('Twitter', href, :target => :blank, :title => 'Share on Twitter', :class => 'button list social twitter tip_under')
   end
   
   def link_to_facebook(url, title)
-    link_to "Facebook", "http://www.facebook.com/sharer.php?u=#{CGI.escape url}&t=#{CGI.escape title}", :target => :blank, :class => 'button list fb_link social facebook', :title => "Facebook"
+    link_to "Facebook", "http://www.facebook.com/sharer.php?u=#{CGI.escape url}&t=#{CGI.escape title}", :target => :blank, :class => 'button list fb_link social facebook tip_under', :title => "Share on Facebook"
   end
   
   def link_to_digg(url, title, description)
