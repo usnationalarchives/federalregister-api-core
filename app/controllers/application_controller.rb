ca80f829ea29845ab6bf3f35630a24c5163222cf
Andrew Carpenter <acarpen@wested.org>
2011-11-21 16:56:28 -0800

    disable ip_spoofing_checks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   
   include Locator
-  
+ 
+  # turn IP Spoofing detection off.
+  ActionController::Base.ip_spoofing_check = false
+
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password, :password_confirmation
   
