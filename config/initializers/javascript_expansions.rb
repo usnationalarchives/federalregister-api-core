eff86f703e4e0a95120371426c446139bac5885c
Andrew Carpenter <andrew@criticaljuncture.org>
2012-01-23 15:55:13 -0800

    tweak vendor inclusion order

--- config/initializers/javascript_expansions.rb
+++
@@ -1,1 +0,0 @@
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :vendor => Dir.glob('public/javascripts/vendor/*.js').map{|s| s.sub(/public\/javascripts\//,'')} - ['vendor/vendor.js']
