46999ff29b0ed6d58ab415025097c6c4efc79fc0
Andrew Carpenter <andrew@criticaljuncture.org>
2012-02-21 15:37:51 -0800

    first pass at regs.gov API integration

--- lib/content/regulations_dot_gov.rb
+++ lib/content/regulations_dot_gov.rb
@@ -1,143 +1,81 @@
 module Content
-  module RegulationsDotGov
-    class Client < GwtRpc::Client
-      domain 'www.regulations.gov'
-      js_url 'http://www.regulations.gov/Regs/'
-      gwt_permutation '96ED140EA002EA7F0224967DBF229721'
-      timeout 2000
-      num_attempts 3
-      
-      map_classes 'gov.egov.erule.regs.shared.action.LoadSearchResultsResult'            => 'Content::RegulationsDotGov::SearchResultPackage',
-                  'gov.egov.erule.regs.shared.models.SearchResultModel'                  => 'Content::RegulationsDotGov::SearchResult',
-                  'gov.egov.erule.regs.shared.models.Agency'                             => 'Content::RegulationsDotGov::Agency',
-                  'gov.egov.erule.regs.shared.models.DimensionCounterFilter'             => 'Content::RegulationsDotGov::DimensionCounter',
-                  'gov.egov.erule.regs.shared.models.CommentPeriod'                      => 'Content::RegulationsDotGov::CommentPeriod',
-                  'gov.egov.erule.regs.shared.models.DocumentSummaryModel'               => 'Content::RegulationsDotGov::DocumentSummary',
-                  'gov.egov.erule.regs.shared.resources.SharedConstants$DOCUMENT_STATUS' => 'Content::RegulationsDotGov::DocumentStatus',
-                  'gov.egov.erule.regs.shared.models.DocumentType'                       => 'Content::RegulationsDotGov::DocumentType',
-                  'gov.egov.erule.regs.shared.models.DocketType'                         => 'Content::RegulationsDotGov::DocketType'
-    
-      add_procedure(:search, :path => '/dispatch/LoadSearchResultsAction') do |term|
-        "7|0|17|http://www.regulations.gov/Regs/|E4625492BC992E3FD53A93EC85103116|com.gwtplatform.dispatch.shared.DispatchService|execute|java.lang.String/2004016611|com.gwtplatform.dispatch.shared.Action|9324e41757554f59f2c8f74e588b41040fab46547bea7264778f86992a4bdcd9.e38Sc3uTa3qQe3yTbi0|gov.egov.erule.regs.shared.action.LoadSearchResultsAction/125242584|gov.egov.erule.regs.shared.models.SearchQueryModel/1556278353|java.util.ArrayList/4159755760||#{term}|gov.egov.erule.regs.shared.models.DataFetchSettings/1603506619|java.lang.Integer/3438268394|docketId|DESC|java.lang.Boolean/476441737|1|2|3|4|2|5|6|7|8|0|9|10|0|11|11|11|10|0|3|10|1|5|12|13|14|0|15|16|14|10|-8|11|11|11|17|0|1|0|"
+  class RegulationsDotGov
+    class RecordNotFound < HTTParty::ResponseError; end
+    class ServerError < HTTParty::ResponseError; end
+
+    include HTTParty
+    base_uri 'http://www.regulations.gov/api/'
+
+    def initialize(api_key)
+      @api_key = api_key
+    end
+
+    def find_by_document_number(document_number)
+      begin
+        fetch_by_document_number(document_number)
+      rescue RecordNotFound, ServerError => e
+        revised_document_number = pad_document_number(document_number) 
+        if revised_document_number != document_number
+          fetch_by_document_number(revised_document_number)
+        else
+          nil
+        end
+      end  
+    end
+
+    private
+
+    def pad_document_number(document_number)
+      part_1, part_2 = document_number.split(/-/, 2)
+      sprintf("%s-%05d", part_1, part_2.to_i)
+    end
+
+    def fetch_by_document_number(document_number)
+      response = self.class.get('/getdocument/v1.json', :query => {:api_key => @api_key, :FR => document_number})
+      Document.new(response.parsed_response["document"])
+    end
+
+    def self.get(url, options)
+      response = super
+
+      case response.code
+      when 200
+        response
+      when 404
+        raise RecordNotFound.new(response)
+      when 500
+        raise ServerError.new(response)
+      else
+        raise HTTParty::ResponseError.new(response)
       end
     end
-    
-    class SearchResultPackage
-      def self.gwt_deserialize(reader)
-        reader.read_object
+
+    class Document
+      def initialize(raw_attributes)
+        @raw_attributes = raw_attributes
+        @metadata = {}
+        @raw_attributes["metadata"]["entry"].each do |hsh|
+          @metadata[ hsh["@name"] ] = hsh["$"]
+        end
       end
-    end
-  
-    class SearchResult
-      def self.gwt_deserialize(reader)
-        reader.read_object
-        reader.read_object
-        reader.read_object
+
+      def document_id
+        @raw_attributes['documentId']
       end
-    end
-  
-    class Agency
-      def self.gwt_deserialize(reader)
-        reader.read_string # abbr
-        reader.read_object # num_results
-        reader.read_string # id
-        reader.read_string # name
+
+      def comment_due_date
+        val = @metadata["Comment Due Date"]
+        if val.present?
+          DateTime.parse(val)
+        end
       end
-    end
-  
-    class DimensionCounter
-      def self.gwt_deserialize(reader)
-        reader.read_object
-        reader.read_object
-      end
-    end
-  
-    class CommentPeriod
-      def self.gwt_deserialize(reader)
-        reader.read_int
-      end
-    end
-  
-    class DocumentStatus
-      def self.gwt_deserialize(reader)
-        reader.read_int
-      end
-    end
-  
-    class DocumentType
-      def self.gwt_deserialize(reader)
-        reader.read_int
-      end
-    end
-  
-    class DocketType
-      def self.gwt_deserialize(reader)
-        reader.read_int
-      end
-    end
-    
-    class DocumentSummary
-      attr_accessor :document_id
-    
-      def initialize(document_id, allows_comments)
-        @document_id = document_id
-        @allows_comments = allows_comments
-      end
-    
-      def allows_comments?
-        @allows_comments
-      end
-      
+
       def comment_url
-        "http://www.regulations.gov/#!submitComment;D=#{document_id}" if allows_comments?
+        "http://www.regulations.gov/#!submitComment;D=#{document_id}"
       end
-      
+
       def url
         "http://www.regulations.gov/#!documentDetail;D=#{document_id}"
-      end
-    
-      def self.gwt_deserialize(reader)
-        # Some stuff we don't care about
-        reader.read_int
-        reader.read_string
-        reader.read_object
-        reader.read_int
-        
-        allows_comments = reader.read_object
-        
-        # Some more stuff we don't care about
-        reader.read_object
-        reader.read_string
-        reader.read_string
-        reader.read_int
-        reader.read_string
-        reader.read_string
-        reader.read_string
-        reader.read_string
-        reader.read_object
-        
-        document_id = reader.read_string
-        
-        # Some more stuff we don't care about
-        reader.read_object
-        reader.read_object
-        reader.read_object
-        reader.read_object
-        reader.read_object
-        reader.read_object
-        reader.read_object
-        reader.read_string
-        reader.read_string
-        reader.read_object
-        reader.read_string
-        reader.read_string
-        reader.read_string
-        reader.read_string
-        reader.read_string
-        reader.read_string
-        reader.read_object
-        
-        self.new(document_id, allows_comments)
       end
     end
   end
