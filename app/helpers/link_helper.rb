caebf8b08d3422b76ee17c4d964b0250bb893047
Bob Burbach <bob.burbach@gmail.com>
2012-02-27 13:37:23 -0800

    Update the share icons (email, twitter, facebook) for the new my_fr2 update. Update styles so that tipsy shows up above these icons and fix how the arrow is displayed here as it's not getting set properly.

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,11 +1,11 @@
 module LinkHelper
   def link_to_twitter(status)
     href = "http://twitter.com/home?status=#{CGI.escape status}"
-    link_to('Twitter', href, :target => :blank, :title => 'Share on Twitter', :class => 'button list social twitter tip_under')
+    link_to('Twitter', href, :target => :blank, :title => 'Share on Twitter', :class => 'button list social twitter tip_over')
   end
   
   def link_to_facebook(url, title)
-    link_to "Facebook", "http://www.facebook.com/sharer.php?u=#{CGI.escape url}&t=#{CGI.escape title}", :target => :blank, :class => 'button list fb_link social facebook tip_under', :title => "Share on Facebook"
+    link_to "Facebook", "http://www.facebook.com/sharer.php?u=#{CGI.escape url}&t=#{CGI.escape title}", :target => :blank, :class => 'button list fb_link social facebook tip_over', :title => "Share on Facebook"
   end
   
   def clippy(text, bgcolor='#F5F8F9')
