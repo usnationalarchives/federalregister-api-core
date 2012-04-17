usnationalarchives/federalregister-api-core

commit b769e3cb94ffd9e539d11d2c6b4731399e0c2f24

allow users to specify attributes in article API requests

app/controllers/api/v1/entries_controller.rb
@@ -1,14 +1,15 @@
 class Api::V1::EntriesController < ApiController
+  BASIC_FIELDS = [:title, :type, :abstract, :document_number, :html_url, :pdf_url, :public_inspection_pdf_url, :publication_date, :agenices, :excerpts]
+
   def index
     respond_to do |wants|
       wants.json do
         cache_for 1.day
         search = EntrySearch.new(params)
+        fields = specified_fields || BASIC_FIELDS
+
         render_search(search) do |result| 
-          basic_entry_data(result).merge(
-            :excerpts => result.excerpts.raw_text || result.excerpts.abstract,
-            :json_url => api_v1_entry_url(result.document_number, :format => :json)
-          )
+          entry_data(result, fields)
         end
       end
     end
@@ -18,8 +19,11 @@ class Api::V1::EntriesController < ApiController
     respond_to do |wants|
       wants.json do
         cache_for 1.day
+
+        fields = specified_fields || all_fields 
+
         render_one_or_more(Entry, params[:id]) do |entry|
-          full_entry_data(entry)
+          entry_data(entry, fields)
         end
       end
     end
@@ -27,64 +31,91 @@ class Api::V1::EntriesController < ApiController
  
   private
 
-  def index_url(options)
-    api_v1_entries_url(options)
+  def specified_fields
+    if params[:fields]
+      params[:fields].reject(&:blank?).map{|f| f.to_s.to_sym}
+    end
   end
-  
-  def basic_entry_data(entry)
-    {
-      :title            => entry.title,
-      :type             => entry.entry_type,
-      :abstract         => entry.abstract,
-      :document_number  => entry.document_number,
-      :html_url         => entry_url(entry),
-      :pdf_url          => entry.source_url('pdf'),
-      :public_inspection_pdf_url => entry.public_inspection_document.try(:pdf).try(:url),
-      :publication_date => entry.publication_date,
-      :agencies         => entry.agency_names.map{|agency_name|
-        agency = agency_name.agency
-        if agency
-          {
-            :raw_name => agency_name.name,
-            :name     => agency.name,
-            :id       => agency.id,
-            :url      => agency_url(agency),
-            :json_url => api_v1_agency_url(agency.id, :format => :json),
-            :parent_id => agency.parent_id
-          }
-        else
-          {
-            :raw_name => agency_name.name
-          }
-        end
-      }
+
+  def entry_data(entry, fields)
+    Hash[ fields.map do |field|
+      calculator = field_calculators[field]
+
+      if calculator.nil?
+        raise UnknownFieldError.new("#{field} is not a valid field")
+      end
+
+      [field, calculator.call(entry)]
+    end]
+  end
+
+  def field_calculators
+    @field_calculators ||= {
+      :abstract                  => Proc.new{|e| e.abstract},
+      :abstract_html_url         => Proc.new{|e| entry_abstract_url(e)},
+      :action                    => Proc.new{|e| e.action},
+      :agencies                  => Proc.new{|e| agencies_data(e)},
+      :body_html_url             => Proc.new{|e| entry_full_text_url(e)},
+      :cfr_references            => Proc.new{|e| cfr_references_info(e)}, 
+      :comments_close_on         => Proc.new{|e| e.comments_close_on},
+      :dates                     => Proc.new{|e| e.dates},
+      :docket_id                 => Proc.new{|e| e.docket_numbers.first.try(:number)}, # backwards compatible for now
+      :docket_ids                => Proc.new{|e| e.docket_numbers.map(&:number)},
+      :document_number           => Proc.new{|e| e.document_number},
+      :effective_on              => Proc.new{|e| e.effective_on},
+      :end_page                  => Proc.new{|e| e.end_page},
+      :excerpts                  => Proc.new{|e| (e.excerpts.raw_text || result.excerpts.abstract) if e.respond_to?(:excerpts)},
+      :full_text_xml_url         => Proc.new{|e| e.full_xml_updated_at ? entry_xml_url(e) : nil},
+      :html_url                  => Proc.new{|e| entry_url(e)},
+      :json_url                  => Proc.new{|e| api_v1_entry_url(e.document_number, :format => :json)},
+      :mods_url                  => Proc.new{|e| e.source_url(:mods)},
+      :pdf_url                   => Proc.new{|e| e.source_url('pdf')}, 
+      :public_inspection_pdf_url => Proc.new{|e| e.public_inspection_document.try(:pdf).try(:url)},
+      :publication_date          => Proc.new{|e| e.publication_date},
+      :regulation_id_number_info => Proc.new{|e| regulation_id_number_info(e)},
+      :regulation_id_numbers     => Proc.new{|e| e.entry_regulation_id_numbers.map(&:regulation_id_number)},
+      :regulations_dot_gov_url   => Proc.new{|e| e.regulationsdotgov_url},
+      :start_page                => Proc.new{|e| e.start_page},
+      :subtype                   => Proc.new{|e| e.presidential_document_type.try(:name)}, 
+      :title                     => Proc.new{|e| e.title},
+      :type                      => Proc.new{|e| e.entry_type},
+      :volume                    => Proc.new{|e| e.volume},
     }
   end
 
-  def full_entry_data(entry)
-    basic_entry_data(entry).merge({
-        :full_text_xml_url => entry.full_xml_updated_at ? entry_xml_url(entry) : nil,
-        :abstract_html_url => entry_abstract_url(entry),
-        :body_html_url => entry_full_text_url(entry),
-        :mods_url => entry.source_url(:mods),
-        :regulations_dot_gov_url => entry.regulationsdotgov_url,
-        :action => entry.action,
-        :dates  => entry.dates,
-        :effective_on  => entry.effective_on,
-        :comments_close_on  => entry.comments_close_on,
-        :start_page => entry.start_page,
-        :end_page => entry.end_page,
-        :volume => entry.volume,
-        :docket_id => entry.docket_numbers.first.try(:number), # backwards compatible for now
-        :docket_ids => entry.docket_numbers.map(&:number),
-        :regulation_id_numbers => entry.entry_regulation_id_numbers.map(&:regulation_id_number),
-        :regulation_id_number_info => regulation_id_number_info(entry),
-        :subtype => entry.presidential_document_type.try(:name),     
-        :cfr_references => entry.entry_cfr_references.map{|cfr_reference|
-          {:title => cfr_reference.title, :part => cfr_reference.part}
+  def all_fields
+    field_calculators.keys - [:excerpts]
+  end
+
+  def index_url(options)
+    api_v1_entries_url(options)
+  end
+
+
+  def agencies_data(entry)
+    entry.agency_names.map do |agency_name|
+      agency = agency_name.agency
+      if agency
+        {
+          :raw_name => agency_name.name,
+          :name     => agency.name,
+          :id       => agency.id,
+          :url      => agency_url(agency),
+          :json_url => api_v1_agency_url(agency.id, :format => :json),
+          :parent_id => agency.parent_id
         }
-      }
-    )
+      else
+        {
+          :raw_name => agency_name.name
+        }
+      end
+    end
+  end
+
+  def cfr_references_info(entry)
+    entry.entry_cfr_references.map do |cfr_reference|
+      {:title => cfr_reference.title, :part => cfr_reference.part}
+    end
   end
 
   def regulation_id_number_info(entry)

app/controllers/api_controller.rb
@@ -1,5 +1,9 @@
 class ApiController < ApplicationController
+  class RequestError < StandardError; end
+  class UnknownFieldError < RequestError; end
+
   private
+
   def render_json_or_jsonp(data, options = {})
     callback = params[:callback].to_s
     if callback =~ /^\w+$/
@@ -62,6 +66,11 @@ class ApiController < ApplicationController
   def server_error(exception)
     notify_airbrake(exception)
     render :json => {:status => 500, :message => "Internal Server Error"}, :status => 500
+  end
+
+  rescue_from RequestError, :with => :request_error if RAILS_ENV != 'development'
+  def request_error(exception)
+    render :json => {:status => 400, :message => exception.message}, :status => 400
   end
   
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found if RAILS_ENV != 'development'

commit ba345b9e0e1d7c732905cacf8ce2de0d9622a4b4

fix typo introduced in commit b769e3cb94ffd9e539d11d2c6b4731399e0c2f24
Signed-off-by: Joel Nimety <jnimety@continuity.net>

app/controllers/api/v1/entries_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::EntriesController < ApiController
-  BASIC_FIELDS = [:title, :type, :abstract, :document_number, :html_url, :pdf_url, :public_inspection_pdf_url, :publication_date, :agenices, :excerpts]
+  BASIC_FIELDS = [:title, :type, :abstract, :document_number, :html_url, :pdf_url, :public_inspection_pdf_url, :publication_date, :agencies, :excerpts]
 
   def index
     respond_to do |wants|