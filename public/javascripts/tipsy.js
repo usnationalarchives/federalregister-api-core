7c712c21dcf3758b32a7c8cf2adb8a9bcb1f8db2
Bob Burbach <bob.burbach@gmail.com>
2012-04-27 17:15:35 -0700

    Add tooltips for new document types

--- public/javascripts/tipsy.js
+++ public/javascripts/tipsy.js
@@ -9,6 +9,8 @@ $(document).ready(function(){
   $('.doc_rule.tipsy').tipsy(   {gravity: 'e', fallback: "Final Rule",            delayIn: 100, fade: true, offset: -4});
   $('.doc_prorule.tipsy').tipsy({gravity: 'e', fallback: "Proposed Rule",         delayIn: 100, fade: true, offset: -4});
   $('.doc_presdocu.tipsy').tipsy({gravity: 'e', fallback: "Presidential Document", delayIn: 100, fade: true, offset: -4});
+  $('.doc_unknown.tipsy').tipsy({gravity: 'e', fallback: "Document of Unknown Type", delayIn: 100, fade: true, offset: -4});
+  $('.doc_correct.tipsy').tipsy({gravity: 'e', fallback: "Correction", delayIn: 100, fade: true, offset: -4});
 
   $('.tip_left').tipsy({gravity:'east'});
   $('.tip_over').tipsy({gravity:'south'});
