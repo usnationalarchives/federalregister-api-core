6e0e8e4a45461808d42508aab0be1b98fdffb4fd
Bob Burbach <bob.burbach@gmail.com>
2012-05-10 15:26:13 -0700

    Add mini document items to the lists of documents on agency pages in order to display document type. Use presdocu subtype as the tooltip for presidential documents.

--- public/javascripts/tipsy.js
+++ public/javascripts/tipsy.js
@@ -5,12 +5,24 @@ $(document).ready(function(){
   
 
   /* CANNED SEARCH PAGE */
-  $('.doc_notice.tipsy').tipsy( {gravity: 'e', fallback: "Notice",                delayIn: 100, fade: true, offset: -4});
-  $('.doc_rule.tipsy').tipsy(   {gravity: 'e', fallback: "Final Rule",            delayIn: 100, fade: true, offset: -4});
-  $('.doc_prorule.tipsy').tipsy({gravity: 'e', fallback: "Proposed Rule",         delayIn: 100, fade: true, offset: -4});
-  $('.doc_presdocu.tipsy').tipsy({gravity: 'e', fallback: "Presidential Document", delayIn: 100, fade: true, offset: -4});
-  $('.doc_unknown.tipsy').tipsy({gravity: 'e', fallback: "Document of Unknown Type", delayIn: 100, fade: true, offset: -4});
-  $('.doc_correct.tipsy').tipsy({gravity: 'e', fallback: "Correction", delayIn: 100, fade: true, offset: -4});
+  $('#canned_searches .doc_notice.tipsy').tipsy( {gravity: 'e', fallback: "Notice",                delayIn: 100, fade: true, offset: -4});
+  $('#canned_searches .doc_rule.tipsy').tipsy(   {gravity: 'e', fallback: "Final Rule",            delayIn: 100, fade: true, offset: -4});
+  $('#canned_searches .doc_prorule.tipsy').tipsy({gravity: 'e', fallback: "Proposed Rule",         delayIn: 100, fade: true, offset: -4});
+  $('#canned_searches .doc_presdocu.tipsy').tipsy({gravity: 'e', fallback: "Presidential Document", delayIn: 100, fade: true, offset: -4});
+  $('#canned_searches .doc_unknown.tipsy').tipsy({gravity: 'e', fallback: "Document of Unknown Type", delayIn: 100, fade: true, offset: -4});
+  $('#canned_searches .doc_correct.tipsy').tipsy({gravity: 'e', fallback: "Correction", delayIn: 100, fade: true, offset: -4});
+
+  /* AGENCY PAGE */
+  $('#agencies .doc_presdocu.tipsy').tipsy({ gravity: 'e', delayIn: 100, fade: true, offset: 0,
+                                             title: function() { return $(this).data('tooltip');},
+                                             fallback: "Presidential Document"
+                                           });
+  $('#agencies .doc_notice.tipsy').tipsy( {gravity: 'e', fallback: "Notice",                   delayIn: 100, fade: true, offset: 0});
+  $('#agencies .doc_rule.tipsy').tipsy(   {gravity: 'e', fallback: "Final Rule",               delayIn: 100, fade: true, offset: 0});
+  $('#agencies .doc_prorule.tipsy').tipsy({gravity: 'e', fallback: "Proposed Rule",            delayIn: 100, fade: true, offset: 0});
+  $('#agencies .doc_unknown.tipsy').tipsy({gravity: 'e', fallback: "Document of Unknown Type", delayIn: 100, fade: true, offset: 0});
+  $('#agencies .doc_correct.tipsy').tipsy({gravity: 'e', fallback: "Correction",               delayIn: 100, fade: true, offset: 0});
+
 
   $('.tip_left').tipsy({gravity:'east'});
   $('.tip_over').tipsy({gravity:'south'});
