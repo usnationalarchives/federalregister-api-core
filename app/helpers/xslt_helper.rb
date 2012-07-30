7d6c121e6381e8a8d75589d9ae3914bd76f775de
Andrew Carpenter <andrew@criticaljuncture.org>
2012-07-30 14:44:34 -0700

    fix encoding by using older-style DOM manipulation

--- app/helpers/xslt_helper.rb
+++ app/helpers/xslt_helper.rb
@@ -10,7 +10,17 @@ module XsltHelper
       match = amdpar_node.content.strip.match(/^(\d+\.) (.*)/)
 
       if match
-        amdpar_node.children = %Q{<span class="amendment_part_number">#{match[1]}</span> <span class="amendment_part_text">#{match[2]}</span>}
+        amdpar_node.children.each{|x| x.remove}
+
+        part_number = Nokogiri::XML::Node.new "span", doc
+        part_number['class'] = 'amendment_part_number'
+        part_number.content = match[1]
+        amdpar_node.add_child(part_number)
+
+        part_text = Nokogiri::XML::Node.new "span", doc
+        part_text['class'] = 'amendment_part_next'
+        part_text.content = match[2] + ' '
+        amdpar_node.add_child(part_text)
       end
     end
 
@@ -26,20 +36,26 @@ module XsltHelper
       JOIN topics
         ON topics.id = topics_topic_names.topic_id
     SQL
+
     doc = Nokogiri::HTML::DocumentFragment.parse(xml)
     doc.css('.subject_list li').each do |topics_node|
       topic_names = topics_node.text.strip.sub(/\.$/, '').split(/\s*,\s*/)
 
       topic_names.reverse.each do |topic_name|
-        item = content_tag(:li) do
-          conjunction, name = topic_name.match(/^(\s*and\s*)?(.*)/)[1,2]
-          if topic_slug = @known_topics[name]
-            [conjunction, link_to(name, '/topics/' + topic_slug)].compact.join(' ')
-          else
-            topic_name
-          end
+        list_item = Nokogiri::XML::Node.new('li', doc)
+
+        conjunction, name = topic_name.match(/^(\s*and\s*)?(.*)/)[1,2]
+        if topic_slug = @known_topics[name]
+          list_item.content = conjunction
+          anchor = Nokogiri::XML::Node.new('a', doc)
+          anchor['href'] = '/topics/' + topic_slug
+          anchor.content = name
+
+          list_item.add_child(anchor)
+        else
+          list_item.content = topic_name
         end
-        topics_node.add_next_sibling(item)
+        topics_node.add_next_sibling(list_item)
       end
 
       topics_node.remove
