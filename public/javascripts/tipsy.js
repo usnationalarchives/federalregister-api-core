06e00041bd6a11698d44587c3f5ce3e0c2724be4
Bob Burbach <bob.burbach@gmail.com>
2012-08-08 16:27:30 -0700

    Add tooltips for signature and words of issuance

--- public/javascripts/tipsy.js
+++ public/javascripts/tipsy.js
@@ -28,7 +28,9 @@ $(document).ready(function(){
   $('#doc-type-search-filter li').tipsy( {gravity: 'n', fade: true, offset: 2, title: function() { return $(this).data('tooltip');} });
 
   /* ARTICLE PAGE */
-  $('#entries.show .reg_text .table_of_contents h2').tipsy( {gravity: 's', fade: true, offset: 2, fallback: "Table of Contents"});
+  $('body#entries.show .reg_text .table_of_contents h2').tipsy( {gravity: 's', fade: true, offset: 2, fallback: "Table of Contents"});
+  $('body#entries.show h2.signature_header').tipsy( {gravity: 's', fade: true, offset: 2, fallback: "Document Signature"});
+  $('body#entries.show .words_of_issuance h2').tipsy( {gravity: 's', fade: true, offset: 2, fallback: "Words of Issuance"});
 
   $('.tip_left').tipsy({gravity:'east'});
   $('.tip_over').tipsy({gravity:'south'});
