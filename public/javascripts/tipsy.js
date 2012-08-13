c914e808ded85397e39025f45b9fc82a9c15d4ed
Bob Burbach <bob.burbach@gmail.com>
2012-08-13 12:39:54 -0700

    Consolidate and simply the tipsy code for document icons. Specific tooltip selectors need to go before the generic selectors as tipsy can't be applied multiple times to the same element (the first on bound wins).

--- public/javascripts/tipsy.js
+++ public/javascripts/tipsy.js
@@ -13,28 +13,10 @@ $(document).ready(function(){
   /* CALENDAR */
   $('table.calendar td.holiday').tipsy({ gravity: 's', delayIn: 100, fade: true, offset: 0});
 
-  /* CANNED SEARCH PAGE */
-  $('#canned_searches .doc_notice.tipsy').tipsy( {gravity: 'e', fallback: "Notice",                delayIn: 100, fade: true, offset: -4});
-  $('#canned_searches .doc_rule.tipsy').tipsy(   {gravity: 'e', fallback: "Final Rule",            delayIn: 100, fade: true, offset: -4});
-  $('#canned_searches .doc_prorule.tipsy').tipsy({gravity: 'e', fallback: "Proposed Rule",         delayIn: 100, fade: true, offset: -4});
-  $('#canned_searches .doc_presdocu.tipsy').tipsy({gravity: 'e', fallback: "Presidential Document", delayIn: 100, fade: true, offset: -4});
-  $('#canned_searches .doc_unknown.tipsy').tipsy({gravity: 'e', fallback: "Document of Unknown Type", delayIn: 100, fade: true, offset: -4});
-  $('#canned_searches .doc_correct.tipsy').tipsy({gravity: 'e', fallback: "Correction", delayIn: 100, fade: true, offset: -4});
-
-  /* AGENCY PAGE */
-  $('#agencies .doc_presdocu.tipsy').tipsy({ gravity: 'e', delayIn: 100, fade: true, offset: 0,
-                                             title: function() { return $(this).data('tooltip');},
-                                             fallback: "Presidential Document"
-                                           });
-  $('#agencies .doc_notice.tipsy').tipsy( {gravity: 'e', fallback: "Notice",                   delayIn: 100, fade: true, offset: 0});
-  $('#agencies .doc_rule.tipsy').tipsy(   {gravity: 'e', fallback: "Final Rule",               delayIn: 100, fade: true, offset: 0});
-  $('#agencies .doc_prorule.tipsy').tipsy({gravity: 'e', fallback: "Proposed Rule",            delayIn: 100, fade: true, offset: 0});
-  $('#agencies .doc_unknown.tipsy').tipsy({gravity: 'e', fallback: "Document of Unknown Type", delayIn: 100, fade: true, offset: 0});
-  $('#agencies .doc_correct.tipsy').tipsy({gravity: 'e', fallback: "Correction",               delayIn: 100, fade: true, offset: 0});
-
+  /* Doctype Filters */
+  $('#doc-type-search-filter li').tipsy( {gravity: 'n', fade: true, offset: 2, title: function() { return $(this).data('tooltip');} });
 
   /* NAVIGATION (pluralized tooltips) */
-  /* CANNED SEARCH PAGE */
   $('#navigation .doc_notice.tipsy').tipsy(  {gravity: 's', fallback: "Notice",                   delayIn: 100, fade: true, offset: 4});
   $('#navigation .doc_rule.tipsy').tipsy(    {gravity: 's', fallback: "Final Rule",               delayIn: 100, fade: true, offset: 4});
   $('#navigation .doc_prorule.tipsy').tipsy( {gravity: 's', fallback: "Proposed Rule",            delayIn: 100, fade: true, offset: 4});
@@ -42,8 +24,16 @@ $(document).ready(function(){
   $('#navigation .doc_unknown.tipsy').tipsy( {gravity: 's', fallback: "Document of Unknown Type", delayIn: 100, fade: true, offset: 4});
   $('#navigation .doc_correct.tipsy').tipsy( {gravity: 's', fallback: "Correction",               delayIn: 100, fade: true, offset: 4});
 
-  /* Doctype Filters */
-  $('#doc-type-search-filter li').tipsy( {gravity: 'n', fade: true, offset: 2, title: function() { return $(this).data('tooltip');} });
+  /* AGENCY PAGE, TOPICS, CANNED SEARCHES */
+  $('.doc_presdocu.tipsy').tipsy({ gravity: 'e', delayIn: 100, fade: true, offset: 0,
+                                             title: function() { return $(this).data('tooltip');},
+                                             fallback: "Presidential Document"
+                                           });
+  $('.doc_notice.tipsy').tipsy(  {gravity: 'e', fallback: "Notice",                   delayIn: 100, fade: true, offset: 0});
+  $('.doc_rule.tipsy').tipsy(    {gravity: 'e', fallback: "Final Rule",               delayIn: 100, fade: true, offset: 0});
+  $('.doc_prorule.tipsy').tipsy( {gravity: 'e', fallback: "Proposed Rule",            delayIn: 100, fade: true, offset: 0});
+  $('.doc_unknown.tipsy').tipsy( {gravity: 'e', fallback: "Document of Unknown Type", delayIn: 100, fade: true, offset: 0});
+  $('.doc_correct.tipsy').tipsy( {gravity: 'e', fallback: "Correction",               delayIn: 100, fade: true, offset: 0});
 
 
   $('.tip_left').tipsy({gravity:'east'});
