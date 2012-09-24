4d9fc1087befa5f37afe721fe4d20001ba50eebc
Andrew Carpenter <andrew@criticaljuncture.org>
2012-09-24 14:43:52 -0700

    update regs.gov docket URLs for new structure

--- lib/route_builder.rb
+++ lib/route_builder.rb
@@ -204,11 +204,11 @@ module RouteBuilder
   end
 
   def regulations_dot_gov_docket_comments_url(docket_id)
-    "http://www.regulations.gov/#!docketDetail;dct=PS;rpp=100;so=DESC;sb=docId;po=0;D=#{docket_id}"
+    "http://www.regulations.gov/#!docketBrowser;dct=PS;rpp=100;so=DESC;sb=docId;po=0;D=#{docket_id}"
   end
 
   def regulations_dot_gov_docket_supporting_documents_url(docket_id)
-    "http://www.regulations.gov/#!docketDetail;dct=SR;rpp=100;so=DESC;sb=docId;po=0;D=#{docket_id}"
+    "http://www.regulations.gov/#!docketBrowser;dct=SR;rpp=100;so=DESC;sb=docId;po=0;D=#{docket_id}"
   end
 
   def regulations_dot_gov_document_url(document_id)
