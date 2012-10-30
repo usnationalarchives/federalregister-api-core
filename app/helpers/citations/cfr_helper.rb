b5a38889f3d35a9dce0f3df5bce4dceb095f64b6
Andrew Carpenter <andrew@criticaljuncture.org>
2012-10-30 12:10:11 -0700

    add CFR citation suggestion

--- app/helpers/citations/cfr_helper.rb
+++ app/helpers/citations/cfr_helper.rb
@@ -1,13 +1,17 @@
 module Citations::CfrHelper
-
+  PATTERN = /(\d+)\s+(?:CFR|C\.F\.R\.)\s+(?:[Pp]arts?|[Ss]ections?|[Ss]ec\.|&#xA7;|&#xA7;\s*&#xA7;)?\s*(\d+)(?:\.(\d+))?/
   def add_cfr_links(text, date = Time.current.to_date)
-    text.gsub(/(\d+)\s+(?:CFR|C\.F\.R\.)\s+(?:[Pp]arts?|[Ss]ections?|[Ss]ec\.|&#xA7;|&#xA7;\s*&#xA7;)?\s*(\d+)(?:\.(\d+))?/) do |str|
+    text.gsub(PATTERN) do |str|
       title = $1
       part = $2
       section = $3
       
       content_tag(:a, str, :href => select_cfr_citation_path(date,title,part,section), :class => "cfr external")
     end
+  end
+
+  def is_cfr_citation?(text)
+    text.strip =~ /^#{PATTERN}$/
   end
   
   def cfr_url(year, title, volume, part, section='')
@@ -18,6 +22,6 @@ module Citations::CfrHelper
   end
   
   def ecfr_url(title,part)
-    "http://ecfr.gpoaccess.gov/cgi/t/text/text-idx?type=simple&c=ecfr&cc=ecfr&idno=#{title}&region=DIV1&q1=#{part}&rgn=Part+Number"
+    "http://www.ecfr.gov/cgi-bin/searchECFR?idno=#{title}&q1=#{part}&rgn1=PARTNBR&op2=and&q2=&rgn2=Part"
   end
 end
