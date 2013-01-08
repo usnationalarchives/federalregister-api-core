fe0489d45d0ab342b4ab7039b77fa6b263682cb4
Andrew Carpenter <andrew@criticaljuncture.org>
2013-01-08 14:28:51 -0800

    update to rails 2.3.15

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 ENV["RAILS_ASSET_ID"] = `git log -n 1 --pretty=format:%H`
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.15' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
