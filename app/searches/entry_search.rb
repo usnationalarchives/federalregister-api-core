b16a1f426960b664c241ad6174a03ac2ffddef0e
Andrew Carpenter <andrew@criticaljuncture.org>
2013-01-08 16:54:46 -0800

    remove length references

--- app/searches/entry_search.rb
+++ app/searches/entry_search.rb
@@ -208,7 +208,7 @@ class EntrySearch < ApplicationSearch
   
   def find_options
     {
-      :select => "id, title, publication_date, document_number, granule_class, document_file_path, abstract, length, start_page, end_page, citation, signing_date, executive_order_number, presidential_document_type_id",
+      :select => "id, title, publication_date, document_number, granule_class, document_file_path, abstract, start_page, end_page, citation, signing_date, executive_order_number, presidential_document_type_id",
       :include => :agencies,
     }
   end
