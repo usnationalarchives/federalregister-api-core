1dad06abd8496e686371f2c3eca470baa15d48ff
Peregrinator <bob.burbach@gmail.com>
2013-01-09 14:17:29 -0800

    Add basic styles to FR Index year page.

--- app/controllers/indexes_controller.rb
+++ app/controllers/indexes_controller.rb
@@ -1,14 +1,11 @@
 class IndexesController < ApplicationController
   def year
     @year = params[:year].to_i
-#    raise ActiveRecord::RecordNotFound if @year < 2012
-
-    @agencies = FrIndexPresenter.agencies_in_year(@year)
+    @agencies_grouped_by_first_letter = FrIndexPresenter.agencies_in_year(@year).group_by{|a| a.name.chars.first}
   end
 
   def year_agency
     @year = params[:year].to_i
-#    raise ActiveRecord::RecordNotFound if @year < 2012
     @agency = Agency.find_by_slug!(params[:agency])
     @entries_by_toc_subject = FrIndexPresenter.entries_for_year_and_agency_grouped_by_toc_subject(@year, @agency)
     @entries_by_granule_class = FrIndexPresenter.entries_for_year_and_agency_grouped_by_granule_class(@year, @agency)
