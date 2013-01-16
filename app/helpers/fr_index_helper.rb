c6a4b93cb68570836eb46d9444b592079c812c53
Andrew Carpenter <andrew@criticaljuncture.org>
2013-01-16 14:02:03 -0800

    add cache needs attention counts and tooltips

--- app/helpers/fr_index_helper.rb
+++ app/helpers/fr_index_helper.rb
@@ -1,7 +1,9 @@
 module FrIndexHelper
-  def count_pill(count)
-    pill = ['&nbsp;']
-    pill << content_tag(:span, count, :class => "count_pill") if count > 1
-    pill.join(' ')
+  def count_pill(count, options={})
+   if count > 1
+      css_class = ["count_pill", options.delete(:class)].compact.join(' ')
+
+      "&nbsp;" + content_tag(:span, count, options.merge(:class => css_class))
+    end
   end
 end
---
+++ public/javascripts/admin/tipsy.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('.tip_left').tipsy({gravity:'east'});
+  $('.tip_over').tipsy({gravity:'south'});
+  $('.tip_right').tipsy({gravity:'west'});
+  $('.tip_under').tipsy({gravity:'north'});
+});
