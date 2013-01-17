a357c26b108ff0b411a737787058ad7a4d388bd4
Andrew Carpenter <andrew@criticaljuncture.org>
2013-01-16 19:42:18 -0800

    better needs attention tooltips; include even if only 1

--- app/helpers/fr_index_helper.rb
+++ app/helpers/fr_index_helper.rb
@@ -1,9 +1,18 @@
 module FrIndexHelper
   def count_pill(count, options={})
-   if count > 1
+    min = options.delete(:min) || 1
+    if count > min
       css_class = ["count_pill", options.delete(:class)].compact.join(' ')
 
       "&nbsp;" + content_tag(:span, count, options.merge(:class => css_class))
     end
   end
+
+  def needs_attention_pill(count)
+    count_pill(count,
+      :min => 0,
+      :class => "needs_attention tip_under",
+      :title => "#{pluralize count, 'item'} #{count > 1 ? 'need' : 'needs'} attention"
+    )
+  end 
 end
