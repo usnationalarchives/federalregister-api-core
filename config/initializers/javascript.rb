aa0bc751ef624980fe1b066a452912c6935db6c9
Peregrinator <bob.burbach@gmail.com>
2013-01-31 13:37:47 -0800

    Add rake task for compiling our vendored javascripts to one file via closure compiler. Update the js initializer so that it can be used by the new rake task ensuring development is more like production.

--- config/initializers/javascript.rb
+++ config/initializers/javascript.rb
@@ -1,8 +1,7 @@
-vendor_initial_libs = ['jquery-1.8.3.min.js', 'jrails.js', 'jquery-ui-1.9.2.custom.min.js']
-vendor_exclude_libs = ['vendor.js', 'vendor-20120907.js', 'vendor-20121119.js']
-all_vendor_libs = Dir.entries("#{RAILS_ROOT}/public/javascripts/vendor").select{|n| n =~ /\.js$/}.sort
+VENDOR_INITIAL_LIBS = ['jquery-1.8.3.min.js', 'jrails.js', 'jquery-ui-1.9.2.custom.min.js', 'jquery-ui-1.9.2.effects.min.js']
+ALL_VENDOR_LIBS = Dir.entries("#{RAILS_ROOT}/public/javascripts/vendor").select{|n| n =~ /\.js$/}.sort
 
-vendor_lib_list = (vendor_initial_libs + (all_vendor_libs - vendor_initial_libs - vendor_exclude_libs)).map{|filename| "vendor/#{filename}"}
+vendor_lib_list = (VENDOR_INITIAL_LIBS + (ALL_VENDOR_LIBS - VENDOR_INITIAL_LIBS)).map{|filename| "vendor/#{filename}"}
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :vendor => vendor_lib_list
 
 
---
+++ lib/tasks/closure_compiler.rake
@@ -0,0 +1,9 @@
+namespace :closure_compiler do 
+  namespace :create do
+    task :vendor_js => :environment do
+      vendor_lib_list = (VENDOR_INITIAL_LIBS + (ALL_VENDOR_LIBS - VENDOR_INITIAL_LIBS)).map{|filename| "public/javascripts/vendor/#{filename}"}
+      `java -jar compiler.jar #{vendor_lib_list.join(' --js ')} --js_output_file public/javascripts/vendor/vendor-#{Time.new.strftime("%Y-%m-%d")}.js --compilation_level SIMPLE_OPTIMIZATIONS`
+    end
+  end
+end
+
