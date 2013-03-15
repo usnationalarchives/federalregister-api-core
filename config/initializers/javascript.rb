d2f7b593344b509d5fa10d7ac01df3def47aa3ea
Peregrinator <bob.burbach@gmail.com>
2013-03-15 14:59:09 -0700

    Update what js is loaded when

--- config/initializers/javascript.rb
+++ config/initializers/javascript.rb
@@ -3,7 +3,7 @@
 # http://closure-compiler.googlecode.com/svn/trunk/contrib/externs/
 VENDOR_INITIAL_LIBS = ['jrails.js', 'jquery-ui-1.9.2.custom.min.js', 'jquery-ui-1.9.2.effects.min.js']
 ALL_VENDOR_LIBS = Dir.entries("#{RAILS_ROOT}/public/javascripts/vendor").select{|n| n =~ /\.js$/}.sort
-vendor_exclude_libs = ['vendor-2013-01-31.js']
+vendor_exclude_libs = ['vendor-2013-02-26.js']
 if Rails.env == "development"
   vendor_lib_list = (VENDOR_INITIAL_LIBS + (ALL_VENDOR_LIBS - VENDOR_INITIAL_LIBS - vendor_exclude_libs)).map{|filename| "vendor/#{filename}"}
 else
