c7d5d3e92debef253aa5d1ce1b30670d6feec1b6
Peregrinator <bob.burbach@gmail.com>
2013-05-30 11:08:56 -0700

    Some cleanup for jshint before updating for new feedback workflow

--- public/javascripts/vendor/tender_widget.js
+++ public/javascripts/vendor/tender_widget.js
@@ -9,35 +9,35 @@
       }
     }
     return host;
-  }
+  };
   
   var visible = false;
   var initialized = false;
   var host = getHost();
-  if (!host || host == "") host = "https://help.tenderapp.com/";
+  if (!host || host === "") host = "https://help.tenderapp.com/";
   
   var showWidget = function(){
     if (!initialized) initialize();
     else show();
-  }
+  };
   
   var show = function(){
     document.getElementById('tender_window').style.display = '';
     visible = true;        
-  }
+  };
   
   var hide = function(){
     document.getElementById('tender_window').style.display = 'none';
-    if (typeof(Tender) == "undefined" || !Tender.hideToggle) document.getElementById('tender_toggler').style.display = '';
+    if (typeof(Tender) === "undefined" || !Tender.hideToggle) document.getElementById('tender_toggler').style.display = '';
     visible = false;
-  }
+  };
 
   var initialize = function(){
     var element = document.createElement('div');
     var url     = host + 'widget/discussion/new?r=' + Math.random() + '&discussion[body]=' + encodeURIComponent( "\n\n\n\n\n-----------------\n" + "URL: " + window.location + "\n" + "BROWSER: " + navigator.userAgent) ;
-    if (typeof(Tender) != "undefined" && Tender.sso)
+    if (typeof(Tender) !== "undefined" && Tender.sso)
       url += "&sso=" + encodeURIComponent(Tender.sso)
-    if(typeof Tender != 'undefined' && Tender.widgetEmail)
+    if(typeof Tender !== 'undefined' && Tender.widgetEmail)
       url += '&email=' + encodeURIComponent(Tender.widgetEmail)
       
     var wrapper = '<div id="tender_window"><a href="#" id="tender_closer">Close</a><div id="tender_frame"><iframe src="' + url + '" scrolling="no" frameborder="0" width="100%" height="100%"></iframe></div></div>';
@@ -48,11 +48,11 @@
     close_link.onclick = function(){
       hide();
       return false;
-    } 
+    };
 
-    initialized = true;    
+    initialized = true;
     show();
-  }
+  };
   
   if (typeof(Tender) != "undefined" && Tender.widgetToggles){
     for(var i=0; i<Tender.widgetToggles.length; i++){
@@ -61,7 +61,7 @@
       toggle.onclick = function(){
         showWidget();
         return false;
-      }
+      };
     }
   }
   
@@ -70,7 +70,7 @@
   styles +=    "#tender_window #tender_frame{ width:100%; height:100%; background:url(" + host + "images/widget/loader.gif) 50% 50% no-repeat #fff; } ";
   styles +=    "#tender_closer{ position:absolute; top:18px; right:18px; color:#fff; font-family:Helvetica, Arial, sans-serif; font-size:12px; font-weight:bold; text-decoration:none; border:none; } ";
   styles +=    "#tender_toggler{ position:absolute; top:100px; right:0px; width:33px; height:105px; padding:3px 0 3px 3px; background:url(" + host + "images/widget/overlay_back.png); } ";
-  styles +=    "#tender_toggler_link{ display:block; width:100%; height:100%; text-decoration:none; border:none; background:#006699; text-indent:-9999px; background:url(" + host + "images/widget/tab_text.gif); } "
+  styles +=    "#tender_toggler_link{ display:block; width:100%; height:100%; text-decoration:none; border:none; background:#006699; text-indent:-9999px; background:url(" + host + "images/widget/tab_text.gif); } ";
   
   var style = document.createElement('style');
   style.setAttribute("type", "text/css");
