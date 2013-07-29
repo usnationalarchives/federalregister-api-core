fbf70f5fb22fdb432b2d76ca85f1c553a886a2cb
Andrew Carpenter <andrew@criticaljuncture.org>
2013-07-29 13:49:57 -0700

    support handlebars helper generation in background jobs

--- lib/content.rb
+++ lib/content.rb
@@ -27,7 +27,19 @@ module Content
   
   def self.render_erb(template_path, locals = {})
     view = ActionView::Base.new(Rails::Configuration.new.view_path, {})
-    [ActionView::Helpers::UrlHelper, ActionController::UrlWriter, ApplicationHelper, HtmlHelper, Html5Helper, CitationsHelper, Citations::CfrHelper, XsltHelper, RegulatoryPlanHelper, TextHelper].each do |mod|
+    [
+      ActionView::Helpers::UrlHelper,
+      ActionController::UrlWriter,
+      ApplicationHelper,
+      HandlebarsHelper,
+      HtmlHelper,
+      Html5Helper,
+      CitationsHelper,
+      Citations::CfrHelper,
+      XsltHelper,
+      RegulatoryPlanHelper,
+      TextHelper
+    ].each do |mod|
       view.extend mod
     end
     
