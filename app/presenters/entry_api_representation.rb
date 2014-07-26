e27bceac744a366a3243f7c9e9adac5579c2c392
Andrew Carpenter <andrew@criticaljuncture.org>
2014-07-26 08:52:54 -0700

    improve preloading of associations in API response

--- app/presenters/entry_api_representation.rb
+++ app/presenters/entry_api_representation.rb
@@ -9,7 +9,7 @@ class EntryApiRepresentation < ApiRepresentation
   field(:abstract)
   field(:abstract_html_url, :select => :document_file_path) {|e| entry_abstract_url(e)}
   field(:action)
-  field(:agencies, :select => :id) do |entry|
+  field(:agencies, :select => :id, :include => {:agency_names => :agency}) do |entry|
     entry.agency_names.map do |agency_name|
       agency = agency_name.agency
       if agency
@@ -28,7 +28,7 @@ class EntryApiRepresentation < ApiRepresentation
       end
     end
   end
-  field(:agency_names, :select => :id) {|e| e.agency_names.map{|a| a.agency.try(:name) || a.name}}
+  field(:agency_names, :include => {:agency_names => :agency}) {|e| e.agency_names.map{|a| a.agency.try(:name) || a.name}}
   field(:body_html_url, :select => :document_file_path) {|e| entry_full_text_url(e)}
   field(:cfr_references, :include => :entry_cfr_references) do |entry|
     entry.entry_cfr_references.map do |cfr_reference|
@@ -36,7 +36,7 @@ class EntryApiRepresentation < ApiRepresentation
     end
   end
   field(:citation)
-  field(:comments_close_on, :include => :events){|e| e.comments_close_on}
+  field(:comments_close_on, :include => :comments_close_date){|e| e.comments_close_on}
   field(:comment_url){|e| e.comment_url}
   field(:corrections, :include => :corrections){|e| e.corrections.map{|c| api_v1_entry_url(c.document_number, :format => :json)}}
   field(:correction_of, :include => :correction_of, :select => :correction_of_id){|e| api_v1_entry_url(e.correction_of.document_number, :format => :json) if e.correction_of}
@@ -44,10 +44,10 @@ class EntryApiRepresentation < ApiRepresentation
   field(:docket_id, :include => :docket_numbers){|e| e.docket_numbers.first.try(:number)} # backwards compatible for now
   field(:docket_ids, :include => :docket_numbers){|e| e.docket_numbers.map(&:number)}
   field(:document_number)
-  field(:effective_on, :include => :events)
+  field(:effective_on, :include => :effective_date)
   field(:end_page)
   field(:excerpts, :select => [:document_file_path, :abstract]) {|e| (e.excerpts.raw_text || e.excerpts.abstract) if e.respond_to?(:excerpts) && e.excerpts}
-  field(:executive_order_notes) 
+  field(:executive_order_notes)
   field(:executive_order_number)
   field(:full_text_xml_url, :select => [:document_file_path, :full_xml_updated_at]){|e| entry_xml_url(e) if e.should_have_full_xml?}
   field(:html_url, :select => [:publication_date, :document_number]){|e| entry_url(e)}
@@ -55,7 +55,7 @@ class EntryApiRepresentation < ApiRepresentation
   field(:mods_url, :select => [:publication_date, :document_number]){|e| e.source_url(:mods)}
   field(:page_length, :select => [:start_page, :end_page]) {|e| e.human_length }
   field(:pdf_url, :select => [:publication_date, :document_number]){|e| e.source_url('pdf')}
-  field(:public_inspection_pdf_url, :select => :document_number) {|e| e.public_inspection_document.try(:pdf).try(:url)}
+  field(:public_inspection_pdf_url, :select => :document_number, :include => :public_inspection_document) {|e| e.public_inspection_document.try(:pdf).try(:url)}
   field(:president, :select => [:granule_class, :signing_date, :publication_date]) do |entry|
     president = entry.president
     if president
@@ -64,10 +64,9 @@ class EntryApiRepresentation < ApiRepresentation
   end
   field(:publication_date)
   field(:raw_text_url, :select => [:document_file_path]){|e| entry_raw_text_url(e)}
-  field(:regulation_id_number_info, :include => :entry_regulation_id_numbers) do |entry|
-    values = entry.entry_regulation_id_numbers.map(&:regulation_id_number).map do |rin|
-      regulatory_plan = entry.current_regulatory_plans.detect{|r| r.regulation_id_number == rin}
-
+  field(:regulation_id_number_info, :include => {:entry_regulation_id_numbers => :current_regulatory_plan}) do |entry|
+    values = entry.entry_regulation_id_numbers.map do |e_rin|
+      regulatory_plan = e_rin.current_regulatory_plan
       if regulatory_plan
         regulatory_plan_info = {
           :xml_url => regulatory_plan.source_url(:xml),
@@ -76,9 +75,9 @@ class EntryApiRepresentation < ApiRepresentation
           :priority_category => regulatory_plan.priority_category
         }
       end
-      [rin, regulatory_plan_info]
+      [e_rin.regulation_id_number, regulatory_plan_info]
     end
-    
+
     Hash[*values.flatten]
   end
   field(:regulations_dot_gov_info, :include => :docket, :select => :regulations_dot_gov_docket_id) do |entry|
@@ -103,7 +102,7 @@ class EntryApiRepresentation < ApiRepresentation
   field(:title)
   field(:toc_subject)
   field(:toc_doc)
-  field(:topics, :include => {:topic_assignments => :topic}) {|e| e.topics.map(&:name)}
+  field(:topics, :include => {:topic_assignments => :topic}) {|e| e.topic_assignments.map{|x| x.topic.name}}
   field(:type, :select => :granule_class){|e| e.entry_type}
   field(:volume)
 end
