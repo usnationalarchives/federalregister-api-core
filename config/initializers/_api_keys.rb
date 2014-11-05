2ef5fc9d1ae75837f11d9e6765eaaeb7da28dc1b
Peregrinator <bob.burbach@gmail.com>
2014-11-05 15:22:32 -0800

    Consolidate api_keys.yml into secrets.yml and remove old cruft

--- config/initializers/_api_keys.rb
+++ config/initializers/_api_keys.rb
@@ -1,4 +1,4 @@
-config = YAML::load(File.open("#{RAILS_ROOT}/config/api_keys.yml"))
+config = YAML::load(File.open("#{RAILS_ROOT}/config/secrets.yml"))['api_keys']
 
 config.each_pair do |key, value|
   ENV["#{key}_api_key"] = value
