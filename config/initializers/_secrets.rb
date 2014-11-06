cff07cddd2fecf60e574eaa79c4ee61ebf79d942
Peregrinator <bob.burbach@gmail.com>
2014-11-05 16:02:00 -0800

    Replace the multiple constant declarations/uses with a single SECRETS constant.

--- config/initializers/_secrets.rb
+++ config/initializers/_secrets.rb
@@ -1,4 +1,1 @@
-secrets = File.open( File.join(File.dirname(__FILE__), '..', 'secrets.yml') ) { |yf| YAML::load( yf ) }
-SECRET_SESSION_KEY = secrets['session_key']
-SECRET_EMAIL_SALT = secrets['email_salt']
-SECRET_REG_GOV_API_KEY = secrets['data_dot_gov']['api_key']
+SECRETS = File.open( File.join(File.dirname(__FILE__), '..', 'secrets.yml') ) { |yf| YAML::load( yf ) }
