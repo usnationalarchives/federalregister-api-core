bea0cf87cc1aa3a2433c8e638f859ef02d1b0e77
Peregrinator <bob.burbach@gmail.com>
2014-11-10 15:02:17 -0800

    Don’t consider requests local on staging.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -7,7 +7,7 @@ APP_HOST_NAME = 'fr2.criticaljuncture.org'
 config.cache_classes = true
 
 # Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = true
+config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
