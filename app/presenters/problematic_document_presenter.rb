9e227887f07b6c35a78dc52e3b4bdbd40716993e
Rich Wallett <richmang@gmail.com>
2014-12-05 23:44:40 -0800

    add multiple comment dates to admin import email

--- app/presenters/problematic_document_presenter.rb
+++ app/presenters/problematic_document_presenter.rb
@@ -1,9 +1,10 @@
 class ProblematicDocumentPresenter
   delegate :publication_date, :to => :issue
-  attr_reader :issue
+  attr_reader :issue, :date
 
   def initialize(date)
     @issue = Issue.find_by_publication_date!(date)
+    @date = date
   end
 
   def special_documents
@@ -20,5 +21,29 @@ class ProblematicDocumentPresenter
         doc.effective_on.blank? &&
         !doc.document_number.match(/^C-/)
       end
+  end
+
+  def multiple_comment_dates
+    issue.entries.each_with_object({}) do |doc, hsh|
+      mods_node = Content::EntryImporter::ModsFile.new(date, false).
+        find_entry_node_by_document_number(doc.document_number)
+      comment_date = mods_node.css("commentDate").first
+      if comment_date.present?
+        date_text = mods_node.css('dates').first.try(:content)
+        extracted_dates = PotentialDateExtractor.extract(date_text)
+        if extracted_dates.uniq.size > 1
+          extracted_dates.each do |extracted_date|
+            date_text.gsub!(extracted_date) do |date|
+              if date.to_date == doc.comments_close_on
+                "<span style='color: red; font-weight: bold'>#{date}</span>"
+              else
+                "<span style='font-weight: bold;'>#{date}</span>"
+              end
+            end
+          end
+          hsh[doc.document_number] = date_text
+        end
+      end
+    end
   end
 end
