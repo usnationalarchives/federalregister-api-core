bf895aac8b6c8ea80e2d9dae230b25fe69ea1f53
Peregrinator <bob.burbach@gmail.com>
2015-01-26 14:22:04 -0800

    Update to use rvm role

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,9 +67,11 @@ task :production do
 
   role :proxy,  "proxy.fr2.ec2.internal"
   role :app,    "app-server-1.fr2.ec2.internal", "app-server-2.fr2.ec2.internal", "app-server-3.fr2.ec2.internal", "app-server-4.fr2.ec2.internal", "app-server-5.fr2.ec2.internal"
-  role :db,     "database.fr2.ec2.internal", {:primary => true}
+  #role :db,     "database.fr2.ec2.internal", {:primary => true}
   role :sphinx, "sphinx.fr2.ec2.internal"
   role :worker, "worker.fr2.ec2.internal", {:primary => true} #monster image
+
+  role :rvm, "app-server-1.fr2.ec2.internal", "app-server-2.fr2.ec2.internal", "app-server-3.fr2.ec2.internal", "app-server-4.fr2.ec2.internal", "app-server-5.fr2.ec2.internal"
 
   set :github_user_repo, 'usnationalarchives'
   set :github_project_repo, 'federalregister-api-core'
