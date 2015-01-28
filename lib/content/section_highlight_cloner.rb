c846f8397fd766527e91d5c0a6965d24e346a998
Peregrinator <bob.burbach@gmail.com>
2015-01-28 13:29:21 -0800

    Place the limit in the proper place so that we only clone 6 highlighted articles each day :P

--- lib/content/section_highlight_cloner.rb
+++ lib/content/section_highlight_cloner.rb
@@ -5,11 +5,10 @@ module Content
         prior_date = Entry.first(
           :select => "publication_date",
           :conditions => ["publication_date < ?", date],
-          :order => "publication_date DESC",
-          :limit => 6
+          :order => "publication_date DESC"
         ).publication_date
 
-        SectionHighlight.find_all_by_publication_date(prior_date, :order => "position").each do |highlight|
+        SectionHighlight.find_all_by_publication_date(prior_date, :order => "position", :limit => 6).each do |highlight|
           new_highlight = highlight.clone
           new_highlight.publication_date = date
           new_highlight.save!
