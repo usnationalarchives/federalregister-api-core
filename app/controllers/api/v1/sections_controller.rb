6ca4785a7eeb1e95f9f31dc08f5af0e53bfd5266
Peregrinator <bob.burbach@gmail.com>
2015-03-23 16:46:22 -0700

    Fix date parsing to allow for no conditions being passed

--- app/controllers/api/v1/sections_controller.rb
+++ app/controllers/api/v1/sections_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::SectionsController < ApiController
   def index
-    publication_date = Date.parse(params[:conditions][:publication_date][:is])
+    publication_date = parse_pub_date(params[:conditions])
     sections = []
 
     Section.all.each do |section|
@@ -18,6 +18,12 @@ class Api::V1::SectionsController < ApiController
   end
 
   private
+
+  def parse_pub_date(conditions)
+    if conditions && conditions[:publication_date] && conditions[:publication_date][:is].present?
+      Date.parse(params[:conditions][:publication_date][:is])
+    end
+  end
 
   def highlighted_documents(section, publication_date)
     section.
