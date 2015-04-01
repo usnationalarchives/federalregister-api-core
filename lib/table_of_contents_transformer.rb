ce0ab3749c7f3839caed403c387ac88e7c25ee05
Brandon Carag <brandon.carag@gmail.com>
2015-04-01 16:40:48 -0700

    Re-factored logic to serially process category nodes.

--- lib/table_of_contents_transformer.rb
+++ lib/table_of_contents_transformer.rb
@@ -18,7 +18,7 @@ class TableOfContentsTransformer
         url: "www.TODO.com",
         see_also: parse_see_also(agcy_node.css('SEE')),
         document_categories: parse_category(agcy_node.css('CAT'))
-      })
+      }.delete_if{|k,v| v.nil?})
     end
 
     #Logic for saving actual file
@@ -45,56 +45,52 @@ class TableOfContentsTransformer
     nodes.each do |cat_node|
       categories.push({
         :name => cat_node.css('HD').text,
-        # :documents => parse_documents(cat_node.css('SJDENT'))
-        :documents => CategoryDocumentAssembler.new(cat_node).documents_array
+        :documents => CategoryDocumentAssembler.new(cat_node).serially_process
       })
     end
     categories
   end
 
   class CategoryDocumentAssembler
-    attr_reader :category_node
+    attr_reader :category_node, :documents
 
     def initialize(category_node)
       @category_node = category_node
+      @documents = []
     end
 
-    def sjdent_documents
-      sjdent_documents = []
-      category_node.css('SJDENT').each do |sjdent_node|
-        sjdent_documents.push({
-          subject_1: find_previous_sibling('SJ', sjdent_node).text,
-          subject_2: sjdent_node.at_css('SJDOC').text,
-          document_numbers: parse_document_numbers(sjdent_node.css('FRDOCBP'))
-        })
+    def serially_process
+      category_node.children.each do |node|
+        process_docent(node) if node.name == "DOCENT"
+        process_ssjdent(node) if node.name == "SSJDENT"
+        process_sjdent(node) if node.name == "SJDENT"
       end
-      sjdent_documents
+      documents
     end
 
-    def docent_documents
-      category_node.css('DOCENT').map do |docent_node|
-        {
+    def process_docent(docent_node)
+        documents << {
           subject_1: docent_node.at_css('DOC').text,
           document_numbers: parse_document_numbers(docent_node.css('FRDOCBP'))
         }
-      end
     end
 
-    def ssjdent_documents
-      category_node.css('SSJDENT').map do |ssjdent_node|
-        {
-          subject1: find_previous_sibling('SJ', ssjdent_node).text,
-          subject2: find_previous_sibling('SUBSJ', ssjdent_node).text,
-          subject3: (ssjdent_node.css('SUBSJDOC').text if ssjdent_node.css('SUBSJDOC')),
-          document_numbers: parse_document_numbers(ssjdent_node.css('FRDOCBP'))
-        }#.delete_if{|k,v| v.nil?}
-      end
+    def process_ssjdent(ssjdent_node)
+      documents << {
+        subject1: find_previous_sibling('SJ', ssjdent_node).text,
+        subject2: find_previous_sibling('SUBSJ', ssjdent_node).text,
+        subject3: (ssjdent_node.css('SUBSJDOC').text if ssjdent_node.css('SUBSJDOC')),
+        document_numbers: parse_document_numbers(ssjdent_node.css('FRDOCBP'))
+      }
     end
 
-    def documents_array
-      sjdent_documents + ssjdent_documents + docent_documents
+    def process_sjdent(sjdent_node)
+      documents << {
+        subject_1: find_previous_sibling('SJ', sjdent_node).text,
+        subject_2: sjdent_node.at_css('SJDOC').text,
+        document_numbers: parse_document_numbers(sjdent_node.css('FRDOCBP'))
+      }
     end
-
 
     def find_previous_sibling(tag, node)
       result = nil
@@ -120,11 +116,6 @@ class TableOfContentsTransformer
     end
 
   end
-
-
-
-
-
 
   def agency_exists?(agency)
     #TODO Lookup in the DB
