c75e34106dabab6df98abe97b8da361aebd480d4
Peregrinator <bob.burbach@gmail.com>
2015-04-22 16:39:57 -0700

    pass the date along when running the command - background jobs won’t have the date available otherwise

--- lib/content/entry_compiler.rb
+++ lib/content/entry_compiler.rb
@@ -2,8 +2,7 @@ module Content
   module EntryCompiler
     def self.perform(date)
       document_numbers = Entry.published_on(date).map(&:document_number)
-
-      Content.run_myfr2_command "bundle exec rake documents:html:compile:all[\"#{document_numbers.join(';')}\"]"
+      Content.run_myfr2_command "bundle exec rake documents:html:compile:all[\"#{document_numbers.join(';')}\"] ENV['DATE']='#{date}'"
     end
   end
 end
