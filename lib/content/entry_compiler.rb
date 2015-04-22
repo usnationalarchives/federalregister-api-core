e5b8adbd87c028582f7eedd1351f95f28717baa8
Peregrinator <bob.burbach@gmail.com>
2015-04-22 14:06:42 -0700

    Use semicolon as the separator instead of spaces

--- lib/content/entry_compiler.rb
+++ lib/content/entry_compiler.rb
@@ -3,7 +3,7 @@ module Content
     def self.perform(date)
       document_numbers = Entry.published_on(date).map(&:document_number)
 
-      Content.run_myfr2_command "bundle exec rake documents:html:compile:all[\"#{document_numbers.join(' ')}\"]"
+      Content.run_myfr2_command "bundle exec rake documents:html:compile:all[\"#{document_numbers.join(';')}\"]"
     end
   end
 end
