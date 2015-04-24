f040f41580c1656bc5cfb73c8ba8713d771a7026
Peregrinator <bob.burbach@gmail.com>
2015-04-24 10:32:47 -0700

    Fix how we pass date when compiling html

--- lib/content/entry_compiler.rb
+++ lib/content/entry_compiler.rb
@@ -2,7 +2,7 @@ module Content
   module EntryCompiler
     def self.perform(date)
       document_numbers = Entry.published_on(date).map(&:document_number)
-      Content.run_myfr2_command "bundle exec rake documents:html:compile:all[\"#{document_numbers.join(';')}\"] ENV['DATE']='#{date}'"
+      Content.run_myfr2_command "bundle exec rake documents:html:compile:all[\"#{document_numbers.join(';')}\"] DATE='#{date}'"
     end
   end
 end
