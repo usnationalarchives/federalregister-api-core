c30c57e353b7735a963ac87722691b7456e853db
Brandon Carag <brandon.carag@gmail.com>
2015-04-27 12:52:27 -0700

    Clean up ordering and add perform class methods.

--- lib/index_compiler.rb
+++ lib/index_compiler.rb
@@ -18,11 +18,30 @@ class IndexCompiler
     }
   end
 
-  def self.perform(year, agency_id)
+  def self.perform(date)
+    date = date.is_a?(Date) ? date : Date.parse(date)
+    year = date.strftime('%Y')
+    Agency.all.each do |agency|
+      if IndexCompiler.new(year, agency.id).any_documents? #TODO: Refactor to be more performant
+        IndexCompiler.process_agency(year, agency.id)
+      end
+    end
+  end
+
+  def any_documents?
+    if entries.present?
+      true
+    else
+      false
+    end
+  end
+
+  def self.process_agency(year, agency_id)
     agency_representation = new(year, agency_id)
     agency_representation.process_entries
     agency_representation.process_see_also
-    agency_representation.save(agency_representation.doc_data)
+    agency_representation.save(agency_representation.ordered_json)
+    # agency_representation.save(agency_representation.doc_data) #TODO: Backup code that works
   end
 
   def descendant_agency_ids(parent=agency)
@@ -35,6 +54,7 @@ class IndexCompiler
   end
 
   def entries
+    puts agency.id
     @entries ||= Agency.find_as_hashes([
      "SELECT
         entries.document_number,
@@ -74,28 +94,19 @@ class IndexCompiler
   def process_documents(doc_representations)
     hsh = {}
     doc_representations.each do |doc_representation|
-      intermediary = define_document(doc_representation)
+      formatted_doc = format_subjects(doc_representation)
 
-      if hsh[subject_1: intermediary[:subject_1], subject_2: intermediary[:subject_2]]
-        hsh[subject_1: intermediary[:subject_1], subject_2: intermediary[:subject_2]][:document_numbers] << doc_representation["document_number"]
+      if hsh[subject_1: formatted_doc[:subject_1], subject_2: formatted_doc[:subject_2]]
+        hsh[subject_1: formatted_doc[:subject_1], subject_2: formatted_doc[:subject_2]][:document_numbers] << doc_representation["document_number"]
       else
-        hsh[subject_1: intermediary[:subject_1], subject_2: intermediary[:subject_2]] = intermediary
+        hsh[subject_1: formatted_doc[:subject_1], subject_2: formatted_doc[:subject_2]] = formatted_doc
       end
 
     end
-    hsh.values.sort_by{|k,v|[k[:subject_1],k[:subject_2]]}
+    hsh.values.sort_by{|k,v|[k[:subject_1],k[:subject_2]]}.each {|k,v|k[:document_numbers].sort!}
   end
 
-  def process_see_also
-    doc_data[:see_also] = agency.children.map do |child_agency|
-      {
-        name: child_agency.name,
-        slug: child_agency.slug
-      }
-    end if agency.children.present?
-  end
-
-  def define_document(doc_representation)
+  def format_subjects(doc_representation)
     if doc_representation["subject_1"].blank?
       {
         subject_1: doc_representation["subject_2"],
@@ -111,22 +122,53 @@ class IndexCompiler
     end
   end
 
+  def process_see_also
+    doc_data[:see_also] = agency.children.map do |child_agency|
+      {
+        name: child_agency.name,
+        slug: child_agency.slug
+      }
+    end if agency.children.present?
+    #TODO: Should all child agencies be displayed or only agencies with associated entries?
+  end
+
+  def ordered_json
+    ordered_doc_data = {
+      name: agency.try(:name),
+      slug: agency.try(:slug),
+      url: agency.try(:url),
+      document_categories: []
+    }
+
+    rules = doc_data[:document_categories].find{|cat|cat[:name]=="RULE"}
+    prorules = doc_data[:document_categories].find{|cat|cat[:name]=="PRORULE"}
+    notices = doc_data[:document_categories].find{|cat|cat[:name]=="NOTICE"}
+    presdocs = doc_data[:document_categories].find{|cat|cat[:name]=="PRESDOCU"}
+    unknown_docs = doc_data[:document_categories].find{|cat|cat[:name]=="UNKNOWN"}
+
+    ordered_doc_data[:document_categories] << rules unless rules.nil?
+    ordered_doc_data[:document_categories] << prorules unless prorules.nil?
+    ordered_doc_data[:document_categories] << notices unless notices.nil?
+    ordered_doc_data[:document_categories] << presdocs unless presdocs.nil?
+    ordered_doc_data[:document_categories] << unknown_docs unless unknown_docs.nil?
+    # puts "There are #{ordered_doc_data[:document_categories].size} categories in ordered doc data: #{ordered_doc_data[:document_categories].map{|x|x[:name]}}"
+    ordered_doc_data
+  end
+
   def save(document_data)
-    FileUtils.mkdir_p(json_index_dir)
+    FileUtils.mkdir_p(path_manager.index_json_dir)
 
     File.open json_index_path, 'w' do |f|
       f.write(document_data.to_json)
     end
   end
 
+
   private
 
   def json_index_path
-    "data/fr_index/2015/#{agency.slug}.json"
-  end
-
-  def json_index_dir
-    path_manager.index_json_dir
+    # "data/fr_index/2015/sample_agency.json"
+    "#{path_manager.index_json_dir}#{agency.slug}.json" #TODO: Remove static stub
   end
 
 end
