38f5fddd6b8e4a72293016abae910996bd14ccf2
Brandon Carag <brandon.carag@gmail.com>
2015-04-27 15:04:58 -0700

    Add fr_index to rake task, remove comments, class method through selectively iterating through agencies.

--- lib/index_compiler.rb
+++ lib/index_compiler.rb
@@ -22,26 +22,17 @@ class IndexCompiler
     date = date.is_a?(Date) ? date : Date.parse(date)
     year = date.strftime('%Y')
     Agency.all.each do |agency|
-      if IndexCompiler.new(year, agency.id).any_documents? #TODO: Refactor to be more performant
-        IndexCompiler.process_agency(year, agency.id)
-      end
+      IndexCompiler.process_agency_with_docs(year, agency.id)
     end
   end
 
-  def any_documents?
-    if entries.present?
-      true
-    else
-      false
-    end
-  end
-
-  def self.process_agency(year, agency_id)
+  def self.process_agency_with_docs(year, agency_id)
     agency_representation = new(year, agency_id)
-    agency_representation.process_entries
-    agency_representation.process_see_also
-    agency_representation.save(agency_representation.ordered_json)
-    # agency_representation.save(agency_representation.doc_data) #TODO: Backup code that works
+    if agency_representation.any_documents?
+      agency_representation.process_entries
+      agency_representation.process_see_also
+      agency_representation.save(agency_representation.ordered_json)
+    end
   end
 
   def descendant_agency_ids(parent=agency)
@@ -54,7 +45,6 @@ class IndexCompiler
   end
 
   def entries
-    puts agency.id
     @entries ||= Agency.find_as_hashes([
      "SELECT
         entries.document_number,
@@ -81,8 +71,6 @@ class IndexCompiler
   end
 
   def process_entries
-    puts "Number of doc types: #{entries.size}"
-    puts entries.keys
     entries.each do |doc_type, doc_representations|
       @doc_data[:document_categories] << {
         name: doc_type,
@@ -97,7 +85,8 @@ class IndexCompiler
       formatted_doc = format_subjects(doc_representation)
 
       if hsh[subject_1: formatted_doc[:subject_1], subject_2: formatted_doc[:subject_2]]
-        hsh[subject_1: formatted_doc[:subject_1], subject_2: formatted_doc[:subject_2]][:document_numbers] << doc_representation["document_number"]
+        hsh[subject_1: formatted_doc[:subject_1], subject_2: formatted_doc[:subject_2]]
+          [:document_numbers] << doc_representation["document_number"]
       else
         hsh[subject_1: formatted_doc[:subject_1], subject_2: formatted_doc[:subject_2]] = formatted_doc
       end
@@ -129,7 +118,6 @@ class IndexCompiler
         slug: child_agency.slug
       }
     end if agency.children.present?
-    #TODO: Should all child agencies be displayed or only agencies with associated entries?
   end
 
   def ordered_json
@@ -151,7 +139,6 @@ class IndexCompiler
     ordered_doc_data[:document_categories] << notices unless notices.nil?
     ordered_doc_data[:document_categories] << presdocs unless presdocs.nil?
     ordered_doc_data[:document_categories] << unknown_docs unless unknown_docs.nil?
-    # puts "There are #{ordered_doc_data[:document_categories].size} categories in ordered doc data: #{ordered_doc_data[:document_categories].map{|x|x[:name]}}"
     ordered_doc_data
   end
 
@@ -163,12 +150,20 @@ class IndexCompiler
     end
   end
 
+  def any_documents?
+    if entries.present?
+      true
+    else
+      false
+    end
+  end
+
 
   private
 
   def json_index_path
     # "data/fr_index/2015/sample_agency.json"
-    "#{path_manager.index_json_dir}#{agency.slug}.json" #TODO: Remove static stub
+    "#{path_manager.index_json_dir}#{agency.slug}.json"
   end
 
 end
--- lib/tasks/content_entries_json.rake
+++ lib/tasks/content_entries_json.rake
@@ -3,6 +3,11 @@ namespace :content do
     namespace :json do
       namespace :compile do
         desc "Compile json for table of contents"
+        task :all => %w(
+                  daily_toc
+                  fr_index
+                )
+
         task :daily_toc => :environment do
           dates = Content.parse_dates(ENV['DATE'])
 
@@ -11,6 +16,17 @@ namespace :content do
             XmlTableOfContentsTransformer.perform(date)
           end
         end
+
+        task :fr_index => :environment do
+          #BC TODO: Optimize if multiple dates passed in so index data not unnecessarily re-processed.
+          dates = Content.parse_dates(ENV['DATE'])
+
+          dates.each do |date|
+            puts "compiling fr_index json for #{date}..."
+            IndexCompiler.perform(date)
+          end
+        end
+
       end
     end
   end
