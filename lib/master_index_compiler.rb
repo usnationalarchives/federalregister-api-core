5ba23995780c5caa960e5475396068e3bc74aba4
Brandon Carag <brandon.carag@gmail.com>
2015-05-11 20:11:37 -0700

    Implemented JSON compiler for master fr index page.

---
+++ lib/master_index_compiler.rb
@@ -0,0 +1,55 @@
+class MasterIndexCompiler
+  attr_reader :index, :agencies, :path_manager
+
+  def initialize(year)
+    @year = year.to_i
+    @agencies = FrIndexPresenter.new(@year).agencies_with_pseudonyms
+    @path_manager = FileSystemPathManager.new(Date.new(@year,01,01).to_s(:iso))
+    @index = {agencies:[]}
+  end
+
+  def self.perform(year)
+    master_index_compiler = new(year)
+    master_index_compiler.process_agencies
+    master_index_compiler.save(master_index_compiler.index)
+  end
+
+  def process_agencies
+    agencies.each do |agency|
+      if agency.class == FrIndexPresenter::AgencyPseudonym
+        index[:agencies] << {
+          name: agency.agency.pseudonym,
+          slug: nil,
+          see_also: [{name: agency.agency.name, slug: agency.agency.slug}]
+        }
+      else
+        index[:agencies] << {
+          name: agency.agency.name,
+          slug: agency.agency.slug
+        }.
+        tap do |hsh| hsh["see_also"] =
+          process_see_also(agency.children) if process_see_also(agency.children).present?
+        end
+      end
+    end
+    agencies
+  end
+
+  def process_see_also(child_agencies)
+    child_agencies.map do |child_agency|
+      {
+        name: child_agency.agency.name,
+        slug: child_agency.agency.slug
+      }
+    end
+  end
+
+  def save(index)
+    FileUtils.mkdir_p(path_manager.master_index_json_dir)
+
+    File.open path_manager.master_index_json_path, 'w' do |f|
+      f.write(index.to_json)
+    end
+  end
+
+end
