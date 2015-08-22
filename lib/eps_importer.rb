c973e36c980ae9b1928aa5d48887db1859c6b95c
Brandon Carag <brandon.carag@gmail.com>
2015-08-21 17:43:07 -0700

    Refine importer logic/incorporate fog-based S3 connection.

--- lib/eps_importer.rb
+++ lib/eps_importer.rb
@@ -1,7 +1,7 @@
 class EpsImporter
   require 'zlib'
 
-  def self.perform
+  def self.run
     processor = new
     processor.process
   end
@@ -11,7 +11,7 @@ class EpsImporter
       initial_list = filenames_with_sizes(sftp)
       sleep 5
       delayed_list = filenames_with_sizes(sftp)
-      files_unchanged = initial_list & delayed_list
+      files_unchanged = initial_list & delayed_list #BC TODO: Test this logic.
       filenames_to_download = files_unchanged.map{|f|f[0]}
 
       puts "Beginning download of the following files: #{filenames_to_download.each{|f|puts f}}"
@@ -22,15 +22,20 @@ class EpsImporter
 
       md_5 = create_md5(filenames_to_download, "tmp/tmp_image_files")
       create_zip(md_5, filenames_to_download)
+      store_image("#{md_5}.zip")
     end
   end
 
-  def store_image(file)
-    uploader = EPSUploader.new
-    uploader.store!(my_file)
-  end
-
   private
+
+  def fog_aws_connection
+    @connection ||= Fog::Storage.new({
+      :provider                 => 'AWS',
+      :aws_access_key_id        => secrets["s3"]["username"],
+      :aws_secret_access_key    => secrets["s3"]["password"],
+      :endpoint => 'https://s3.amazonaws.com/'
+    })
+  end
 
   def secrets
     secrets ||= YAML::load_file File.join(Rails.root, 'config', 'secrets.yml')
@@ -54,17 +59,26 @@ class EpsImporter
 
   def create_zip(archive_name, filenames_to_download)
     folder = "tmp/tmp_image_files"
-    input_filenames = filenames_to_download
     zipfile_name = "tmp/#{archive_name}.zip"
 
     Zip::ZipFile.open(zipfile_name, Zip::ZipFile::CREATE) do |zipfile|
-      input_filenames.each do |filename|
+      filenames_to_download.each do |filename|
         # Two arguments:
         # - The name of the file as it will appear in the archive
         # - The original file, including the path to find it
         zipfile.add(filename, folder + '/' + filename)
       end
     end
+  end
+
+  def store_image(file_name)
+    bucket_name = 'eps.images.fr2.criticaljuncture.org'
+    bucket = fog_aws_connection.directories.new(:key => bucket_name)
+    bucket.files.create(
+      :key    => "#{Date.current.to_s(:ymd)}/#{file_name}",
+      :body   => File.open("tmp/#{file_name}"),
+      :public => false
+    )
   end
 
 end
