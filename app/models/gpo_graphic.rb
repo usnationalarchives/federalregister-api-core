77403324ceb1afa7913d5b8a85ceed4aaddf9a13
Brandon Carag <brandon.carag@gmail.com>
2015-08-25 15:00:25 -0700

    Reference existing AWS secrets in lieu of new key.

--- app/models/gpo_graphic.rb
+++ app/models/gpo_graphic.rb
@@ -4,8 +4,8 @@ class GpoGraphic < ActiveRecord::Base
                     #BC TODO: Define :styles key for image format.
                     :storage => :s3,
                     :s3_credentials => {
-                      :access_key_id     => SECRETS["s3"]["username"],
-                      :secret_access_key => SECRETS["s3"]["password"]
+                      :access_key_id     => SECRETS["aws"]["access_key_id"],
+                      :secret_access_key => SECRETS["aws"]["secret_access_key"]
                     },
                     :s3_protocol => 'https',
                     :bucket => 'processed.images.fr2.criticaljuncture.org.test', #BC TODO: Make this dyanmic for the domain.
