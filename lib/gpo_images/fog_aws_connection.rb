972ac8547b9f4253cecd80d2f0639c02d2a362c8
Brandon Carag <brandon.carag@gmail.com>
2015-09-10 17:14:22 -0700

    Add bucket settings for development, suppress fog error message, alter default date.

--- lib/gpo_images/fog_aws_connection.rb
+++ lib/gpo_images/fog_aws_connection.rb
@@ -8,7 +8,8 @@ class GpoImages::FogAwsConnection
       :provider                 => 'AWS',
       :aws_access_key_id        => SECRETS["aws"]["access_key_id"],
       :aws_secret_access_key    => SECRETS["aws"]["secret_access_key"],
-      :endpoint => 'https://s3.amazonaws.com/'
+      :endpoint => 'https://s3.amazonaws.com/',
+      :path_style => true
     })
   end
 
