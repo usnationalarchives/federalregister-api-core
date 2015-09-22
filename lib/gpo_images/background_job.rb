bd25db22124c0602aa58a11171255389792c32a5
Brandon Carag <brandon.carag@gmail.com>
2015-09-22 12:55:02 -0700

    Implement a module for image identifier normalization and also normalize identifiers in the daily_issue_image_processor.

--- lib/gpo_images/background_job.rb
+++ lib/gpo_images/background_job.rb
@@ -1,88 +1,88 @@
-class GpoImages::BackgroundJob
-  @queue = :gpo_image_import
+module GpoImages
+  class BackgroundJob
+    include GpoImages::ImageIdentifierNormalizer
+    @queue = :gpo_image_import
 
-  attr_reader :eps_filename, :zipped_filename, :ftp_transfer_date,
-              :compressed_image_bundles_path, :uncompressed_eps_images_path,
-              :temp_image_files_path, :bucketed_zip_filename, :mark_public
+    attr_reader :eps_filename, :zipped_filename, :ftp_transfer_date,
+                :compressed_image_bundles_path, :uncompressed_eps_images_path,
+                :temp_image_files_path, :bucketed_zip_filename, :mark_public
 
-  def initialize(eps_filename, bucketed_zip_filename, ftp_transfer_date)
-    @eps_filename = eps_filename
-    @bucketed_zip_filename = bucketed_zip_filename
-    @zipped_filename = File.basename(@bucketed_zip_filename) #BC TODO: Change this to base_filename
-    @ftp_transfer_date = ftp_transfer_date.is_a?(Date) ? ftp_transfer_date : Date.parse(ftp_transfer_date)
-    @compressed_image_bundles_path = GpoImages::FileLocationManager.compressed_image_bundles_path
-    @uncompressed_eps_images_path = GpoImages::FileLocationManager.uncompressed_eps_images_path
-  end
-
-  def self.perform(eps_filename, zipped_filename, ftp_transfer_date)
-    new(eps_filename, zipped_filename, ftp_transfer_date).perform
-  end
-
-  def perform
-    gpo_graphic = find_or_create_gpo_graphic
-    if gpo_graphic.save
-      gpo_graphic.move_to_public_bucket if mark_public
-      remove_from_redis_key
-      remove_local_image
-      if redis_file_queue_empty?
-        mark_zipfile_as_converted
-        remove_zip_file
-      end
-    else
-      Honeybadger.notify(
-        :error_class   => "GpoGraphic failed to save",
-        :error_message => gpo_graphic.errors.full_messages.to_sentence,
-        :parameters    => {:identifier => identifier}
-      )
+    def initialize(eps_filename, bucketed_zip_filename, ftp_transfer_date)
+      @eps_filename = eps_filename
+      @bucketed_zip_filename = bucketed_zip_filename
+      @zipped_filename = File.basename(@bucketed_zip_filename) #BC TODO: Change this to base_filename
+      @ftp_transfer_date = ftp_transfer_date.is_a?(Date) ? ftp_transfer_date : Date.parse(ftp_transfer_date)
+      @compressed_image_bundles_path = GpoImages::FileLocationManager.compressed_image_bundles_path
+      @uncompressed_eps_images_path = GpoImages::FileLocationManager.uncompressed_eps_images_path
     end
+
+    def self.perform(eps_filename, zipped_filename, ftp_transfer_date)
+      new(eps_filename, zipped_filename, ftp_transfer_date).perform
+    end
+
+    def perform
+      gpo_graphic = find_or_create_gpo_graphic
+      if gpo_graphic.save
+        gpo_graphic.move_to_public_bucket if mark_public
+        remove_from_redis_key
+        remove_local_image
+        if redis_file_queue_empty?
+          mark_zipfile_as_converted
+          remove_zip_file
+        end
+      else
+        Honeybadger.notify(
+          :error_class   => "GpoGraphic failed to save",
+          :error_message => gpo_graphic.errors.full_messages.to_sentence,
+          :parameters    => {:identifier => identifier}
+        )
+      end
+    end
+
+    private
+
+    def find_or_create_gpo_graphic
+      gpo_graphic = GpoGraphic.find_by_identifier(identifier)
+      gpo_graphic ? @mark_public = true : gpo_graphic = GpoGraphic.new(:identifier => identifier)
+      gpo_graphic.graphic = image
+      gpo_graphic
+    end
+
+    def image
+      @image ||= File.open(File.join(uncompressed_eps_images_path, eps_filename))
+    end
+
+    def identifier
+      normalize_image_identifier(File.basename(eps_filename, File.extname(eps_filename)))
+    end
+
+    def redis
+      Redis.new
+    end
+
+    def remove_from_redis_key
+      redis.srem(redis_key, eps_filename)
+    end
+
+    def mark_zipfile_as_converted
+      GpoImages::ImagePackage.new(ftp_transfer_date, bucketed_zip_filename).mark_as_completed!
+    end
+
+    def redis_file_queue_empty?
+      redis.scard(redis_key) == 0
+    end
+
+    def redis_key
+      "images_left_to_convert:#{zipped_filename}"
+    end
+
+    def remove_local_image
+      FileUtils.rm(File.join(uncompressed_eps_images_path, eps_filename))
+    end
+
+    def remove_zip_file
+      FileUtils.rm(File.join(compressed_image_bundles_path, zipped_filename), :force => true)
+    end
+
   end
-
-  private
-
-  def find_or_create_gpo_graphic
-    gpo_graphic = GpoGraphic.find_by_identifier(identifier)
-    gpo_graphic ? @mark_public = true : gpo_graphic = GpoGraphic.new(:identifier => identifier)
-    gpo_graphic.graphic = image
-    gpo_graphic
-  end
-
-  def image
-    @image ||= File.open(File.join(uncompressed_eps_images_path, eps_filename))
-  end
-
-  def identifier
-    File.basename(eps_filename, File.extname(eps_filename)).
-      gsub('.eps',"").
-      gsub('.EPS',"").
-      downcase
-  end
-
-  def redis
-    Redis.new
-  end
-
-  def remove_from_redis_key
-    redis.srem(redis_key, eps_filename)
-  end
-
-  def mark_zipfile_as_converted
-    GpoImages::ImagePackage.new(ftp_transfer_date, bucketed_zip_filename).mark_as_completed!
-  end
-
-  def redis_file_queue_empty?
-    redis.scard(redis_key) == 0
-  end
-
-  def redis_key
-    "images_left_to_convert:#{zipped_filename}"
-  end
-
-  def remove_local_image
-    FileUtils.rm(File.join(uncompressed_eps_images_path, eps_filename))
-  end
-
-  def remove_zip_file
-    FileUtils.rm(File.join(compressed_image_bundles_path, zipped_filename), :force => true)
-  end
-
 end
--- lib/gpo_images/daily_issue_image_processor.rb
+++ lib/gpo_images/daily_issue_image_processor.rb
@@ -1,84 +1,87 @@
-class GpoImages::DailyIssueImageProcessor
-  attr_reader :date, :documents, :fog_aws_connection
+module GpoImages
+  class DailyIssueImageProcessor
+    attr_reader :date, :documents, :fog_aws_connection
 
-  XML_IMAGE_TAGS = ['GID', 'MID']
+    XML_IMAGE_TAGS = ['GID', 'MID']
 
-  def initialize
-    custom_date = Date.parse ENV['DATE'] if ENV['DATE']
-    @date ||= custom_date || DateTime.current.in_time_zone.to_date
-    @documents ||= Entry.find_all_by_publication_date(@date)
-  end
-
-  def self.perform
-    new.perform
-  end
-
-  def perform
-    scan_documents_for_images
-  end
-
-  class ImageUsage
-    attr_reader :image_identifier, :document_number, :fog_aws_connection,
-    :private_bucket, :public_bucket
-    delegate :graphic_file_name?, :move_to_public_bucket, :to => :gpo_graphic
-
-    def initialize(image_identifier, document_number)
-      @image_identifier = image_identifier
-      @document_number = document_number
+    def initialize
+      custom_date = Date.parse ENV['DATE'] if ENV['DATE']
+      @date ||= custom_date || DateTime.current.in_time_zone.to_date
+      @documents ||= Entry.find_all_by_publication_date(@date)
     end
 
-    def gpo_graphic
-      @gpo_graphic ||= GpoGraphic.find_by_identifier(image_identifier)
+    def self.perform
+      new.perform
     end
 
-    def gpo_graphic_exists?
-      gpo_graphic.present?
+    def perform
+      scan_documents_for_images
     end
 
-    def create_gpo_graphic_usage_unless_existent
-      if GpoGraphicUsage.find_by_identifier_and_document_number(
-        image_identifier,
-        document_number
-        ).nil?
+    class ImageUsage
+      attr_reader :image_identifier, :document_number, :fog_aws_connection,
+      :private_bucket, :public_bucket
+      delegate :graphic_file_name?, :move_to_public_bucket, :to => :gpo_graphic
+      include ImageIdentifierNormalizer
 
-        create_graphic_usage
+      def initialize(image_identifier, document_number)
+        @image_identifier = image_identifier
+        @document_number = document_number
       end
-    end
 
-    def create_graphic_usage
-      GpoGraphicUsage.create(
-        :identifier => image_identifier,
-        :document_number => document_number
-      )
-    end
+      def gpo_graphic
+        @gpo_graphic ||= GpoGraphic.find_by_identifier(image_identifier)
+      end
 
-  end
+      def gpo_graphic_exists?
+        gpo_graphic.present?
+      end
 
-  private
+      def create_gpo_graphic_usage_unless_existent
+        if GpoGraphicUsage.find_by_identifier_and_document_number(
+          image_identifier,
+          document_number
+          ).nil?
 
-  def scan_documents_for_images
-    XML_IMAGE_TAGS.each do |xml_tag|
-      image_usages(xml_tag).each do |image_usage|
-        if image_usage.gpo_graphic_exists?
-          if image_usage.graphic_file_name?
-            image_usage.move_to_public_bucket
-          end
-        else
-          GpoGraphic.create(:identifier => image_usage.image_identifier)
+          create_graphic_usage
         end
+      end
 
-        image_usage.create_gpo_graphic_usage_unless_existent
+      def create_graphic_usage
+        GpoGraphicUsage.create(
+          :identifier => normalize_image_identifier(image_identifier),
+          :document_number => document_number
+        )
+      end
+
+    end
+
+    private
+
+    def scan_documents_for_images
+      XML_IMAGE_TAGS.each do |xml_tag|
+        image_usages(xml_tag).each do |image_usage|
+          if image_usage.gpo_graphic_exists?
+            if image_usage.graphic_file_name?
+              image_usage.move_to_public_bucket
+            end
+          else
+            GpoGraphic.create(:identifier => image_usage.image_identifier)
+          end
+
+          image_usage.create_gpo_graphic_usage_unless_existent
+        end
       end
     end
-  end
 
-  def image_usages(xml_tag)
-    documents.each_with_object([]) do |document, image_usages|
-      xml_doc = Nokogiri::XML(document.full_xml)
-      xml_doc.css(xml_tag).each do |node|
-        image_usages << ImageUsage.new(node.text, document.document_number)
+    def image_usages(xml_tag)
+      documents.each_with_object([]) do |document, image_usages|
+        xml_doc = Nokogiri::XML(document.full_xml)
+        xml_doc.css(xml_tag).each do |node|
+          image_usages << ImageUsage.new(node.text, document.document_number)
+        end
       end
     end
-  end
 
+  end
 end
