c865fe00646b504d84f48127d249df53976ae7ce
Brandon Carag <brandon.carag@gmail.com>
2015-09-23 13:31:09 -0700

    Explicitly specify authentication method for GPO SFTP image site in order to avoid logging clutter that occurs when local public keys are utilized on initial connection attempts.

--- lib/gpo_images/sftp.rb
+++ lib/gpo_images/sftp.rb
@@ -14,7 +14,12 @@ class GpoImages::Sftp
   def remove_files_from_sftp_server(filenames)
     # NOTE: Net::SFTP.start is called instead of the private method 'connection'
     # in order to use a new, non-memoized connection that accepts a block.
-    Net::SFTP.start('ftp.gpo.gov', SECRETS["gpo_sftp"]["username"], :password => SECRETS["gpo_sftp"]["password"]) do |sftp|
+    Net::SFTP.start(
+      'ftp.gpo.gov',
+      SECRETS["gpo_sftp"]["username"],
+      :password => SECRETS["gpo_sftp"]["password"],
+      :auth_methods => ["password"]
+    ) do |sftp|
       filenames.each do |filename|
         Rails.logger.info("Deleting #{filename} from GPO SFTP...")
         sftp.remove(filename)
@@ -25,6 +30,11 @@ class GpoImages::Sftp
   private
 
   def connection
-    @connection ||= Net::SFTP.start('ftp.gpo.gov', SECRETS["gpo_sftp"]["username"], :password => SECRETS["gpo_sftp"]["password"])
+    @connection ||= Net::SFTP.start(
+      'ftp.gpo.gov',
+      SECRETS["gpo_sftp"]["username"],
+      :password => SECRETS["gpo_sftp"]["password"],
+      :auth_methods => ["password"]
+    )
   end
 end
