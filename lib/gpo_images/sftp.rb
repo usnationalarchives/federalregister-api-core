b65ef0aaf9d8821dddbe00c7ae274a0d35c7de1f
Brandon Carag <brandon.carag@gmail.com>
2015-09-24 15:25:51 -0700

    Add connection manager for sftp_connection and use ! methods to ensure blocking takes place.

--- lib/gpo_images/sftp.rb
+++ lib/gpo_images/sftp.rb
@@ -12,29 +12,29 @@ class GpoImages::Sftp
   end
 
   def remove_files_from_sftp_server(filenames)
-    # NOTE: Net::SFTP.start is called instead of the private method 'connection'
-    # in order to use a new, non-memoized connection that accepts a block.
-    Net::SFTP.start(
-      'ftp.gpo.gov',
-      SECRETS["gpo_sftp"]["username"],
-      :password => SECRETS["gpo_sftp"]["password"],
-      :auth_methods => ["password"]
-    ) do |sftp|
-      filenames.each do |filename|
-        Rails.logger.info("Deleting #{filename} from GPO SFTP...")
-        sftp.remove(filename)
-      end
+    filenames.each do |filename|
+      Rails.logger.info("Deleting #{filename} from GPO SFTP...")
+      connection.remove!(filename)
     end
   end
 
   private
 
   def connection
-    @connection ||= Net::SFTP.start(
+    if @connection && @connection.open?
+      @connection
+    else
+      @connection = start_connection
+    end
+  end
+
+  def start_connection
+    Net::SFTP.start(
       'ftp.gpo.gov',
       SECRETS["gpo_sftp"]["username"],
       :password => SECRETS["gpo_sftp"]["password"],
       :auth_methods => ["password"]
     )
   end
+
 end
