232e19bbddc6db5505d9c8d0ab5180861b43c922
Peregrinator <bob.burbach@gmail.com>
2015-11-03 11:19:34 -0800

    Remove the destroy callback as this is already handled by the association dependent destroy

--- app/models/agency_name_assignment.rb
+++ app/models/agency_name_assignment.rb
@@ -4,23 +4,20 @@ class AgencyNameAssignment < ApplicationModel
   belongs_to :agency_name
   belongs_to :assignable, :polymorphic => true
   belongs_to :entry, :foreign_key => :assignable_id
-  
+
   has_one :agency_assignment, :foreign_key => :id, :dependent => :destroy
   acts_as_list :scope => 'assignable_id = #{assignable_id} AND assignable_type = \'#{assignable_type}\''
-  
+
   after_create :create_agency_assignments
-  after_destroy :destroy_agency_assignments
+
   private
+
   def create_agency_assignments
     if agency_name.agency
-      assignable.agency_assignments << AgencyAssignment.new(:agency => agency_name.agency, :agency_name_id => agency_name.id)
-    end
-    true
-  end
-  
-  def destroy_agency_assignments
-    if agency_name.agency
-      assignable.agencies = assignable.agencies - [agency_name.agency]
+      assignable.agency_assignments << AgencyAssignment.new(
+        :agency => agency_name.agency,
+        :agency_name_id => agency_name.id
+      )
     end
     true
   end
