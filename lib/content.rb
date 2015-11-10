6bc7a6cbf03ef05b90e6cd51cabe6396cb5128ec
Peregrinator <bob.burbach@gmail.com>
2015-11-10 10:30:15 -0800

    Add a parse dates methods that doesn’t check whether an issue is present when returning dates. Use this new method when importing eps / converting images.

--- lib/content.rb
+++ lib/content.rb
@@ -1,4 +1,5 @@
 module Content
+  # returns only dates that have issues if using all or >
   def self.parse_dates(date)
     if date == 'all'
       dates = Entry.find_as_array(
@@ -18,6 +19,18 @@ module Content
         :conditions => {:publication_date => Date.parse("#{date}-01-01") .. Date.parse("#{date}-12-31")},
         :order => "publication_date"
       )
+    elsif date.present?
+      dates = [date.is_a?(String) ? date : date.to_s(:iso)]
+    else
+      dates = [Time.current.to_date]
+    end
+  end
+
+  # returns dates regardless of whether they have issues associated
+  def self.parse_all_dates(date)
+    if date =~ /^>/
+      date = Date.parse(date.sub(/^>/, ''))
+      dates = [date .. Time.current.to_date]
     elsif date.present?
       dates = [date.is_a?(String) ? date : date.to_s(:iso)]
     else
@@ -53,13 +66,13 @@ module Content
     ].each do |mod|
       view.extend mod
     end
-    
+
     class << view.class
       def default_url_options
         {:host => "federalregister.gov"}
       end
     end
-    
+
     # Monkeypatching url_for to deal with this issue: https://rails.lighthouseapp.com/projects/8994/tickets/1560#ticket-1560-4
     class << view
       include RouteBuilder
@@ -70,7 +83,7 @@ module Content
           url_for_without_string_support(options)
         end
       end
-      
+
       alias_method_chain :url_for, :string_support
     end
 
@@ -80,7 +93,7 @@ module Content
       path = "#{template_path}.html.erb"
     end
 
-    
+
     view.render(:file => path, :locals => locals)
   end
 end
