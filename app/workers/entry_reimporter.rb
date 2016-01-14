8d16e4cffb894f2a4f2af9b9856435dba0f3c167
Peregrinator <bob.burbach@gmail.com>
2016-01-13 16:18:11 -0800

    Recompile the table of contents and the entries when we reimport an issue.

--- app/workers/entry_reimporter.rb
+++ app/workers/entry_reimporter.rb
@@ -3,5 +3,8 @@ module EntryReimporter
 
   def self.perform(*args)
     Content::EntryImporter.process_all_by_date(*args)
+    date, attributes = *args
+    XmlTableOfContentsTransformer.perform(date)
+    Content::EntryCompiler.perform(date)
   end
 end
