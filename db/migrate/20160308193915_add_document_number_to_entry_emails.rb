94ee1b07154c2541e7b372d4c567a5ab62586abe
Peregrinator <bob.burbach@gmail.com>
2016-03-22 16:58:18 -0700

    Convert to use connection explicitely

--- db/migrate/20160308193915_add_document_number_to_entry_emails.rb
+++ db/migrate/20160308193915_add_document_number_to_entry_emails.rb
@@ -2,10 +2,10 @@ class AddDocumentNumberToEntryEmails < ActiveRecord::Migration
   def self.up
     add_column :entry_emails, :document_number, :string
 
-    execute <<-SQL
+    ActiveRecord::Base.connection.execute(<<-SQL)
       UPDATE entry_emails, entries
       SET entry_emails.document_number = entries.document_number
-      WHERE entries.id = entry_emails.id
+      WHERE entries.id = entry_emails.entry_id
     SQL
   end
 
