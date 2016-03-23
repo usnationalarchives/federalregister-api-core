79d0667967c0205c212d7cf6b0208d2d5a9b59fe
Peregrinator <bob.burbach@gmail.com>
2016-03-23 13:59:53 -0700

    Also reprocess the image xml_identifiers when reprocessing the issue

--- app/workers/entry_reimporter.rb
+++ app/workers/entry_reimporter.rb
@@ -4,6 +4,7 @@ module EntryReimporter
   def self.perform(*args)
     Content::EntryImporter.process_all_by_date(*args)
     date, attributes = *args
+    GpoImages::DailyIssueImageProcessor.perform(date)
     XmlTableOfContentsTransformer.perform(date)
     Content::EntryCompiler.perform(date)
   end
