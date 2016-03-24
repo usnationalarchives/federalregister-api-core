a3ad5f92808b6de49ef19ca25a92fcaf4343b44b
Peregrinator <bob.burbach@gmail.com>
2016-03-24 14:56:05 -0700

    We should only attempt to move an image to the public bucket if it has graphic usages regardless of whether it’s existed before

--- lib/gpo_images/background_job.rb
+++ lib/gpo_images/background_job.rb
@@ -22,7 +22,7 @@ module GpoImages
     def perform
       gpo_graphic = find_or_create_gpo_graphic
       if gpo_graphic.save
-        gpo_graphic.move_to_public_bucket if mark_public
+        gpo_graphic.move_to_public_bucket if gpo_graphic.gpo_graphic_usages.present?
         remove_from_redis_key
         remove_local_image
 
@@ -48,11 +48,11 @@ module GpoImages
 
     def find_or_create_gpo_graphic
       gpo_graphic = GpoGraphic.find_by_identifier(identifier)
-      if gpo_graphic
-        @mark_public = true
-      else
+
+      unless gpo_graphic
         gpo_graphic = GpoGraphic.new(:identifier => identifier)
       end
+
       gpo_graphic.graphic = image
       gpo_graphic
     end
