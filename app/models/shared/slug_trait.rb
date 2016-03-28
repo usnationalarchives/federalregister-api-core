e60539fc4c8a82f3bc94763952640901e376af38
Peregrinator <bob.burbach@gmail.com>
2016-03-28 09:16:04 -0700

    Only update the slug when creating it. Otherwise the slugs in use by api users will change without warning.

--- app/models/shared/slug_trait.rb
+++ app/models/shared/slug_trait.rb
@@ -1,10 +1,10 @@
 module Shared::SlugTrait
   as_trait do |options|
-    before_validation :slugify
+    before_validation_on_create :slugify
     validates_uniqueness_of :slug
 
     private
-  
+
     define_method :slugify do
       self.slug = "#{self.send(options[:based_on]).downcase.gsub(/[^a-z0-9]+/, '-')}"
     end
