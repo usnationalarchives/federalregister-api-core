534e5e42cbffbae49af906a59bd77310e58137fb
Peregrinator <bob.burbach@gmail.com>
2016-07-28 13:44:50 -0700

    Update PI API search controller to make use of find_options_for so that searches are more efficient (and fixes the bug that was reporting filing type wrong in searches - but not document lookups). Update the corresponding api representation to have the required fields (and inclusions) defined properly.

--- app/presenters/public_inspection_document_api_representation.rb
+++ app/presenters/public_inspection_document_api_representation.rb
@@ -21,9 +21,9 @@ class PublicInspectionDocumentApiRepresentation < ApiRepresentation
   field(:document_number)
   field(:filed_at)
   field(:editorial_note)
-  field(:excerpts) {|document| document.excerpts.raw_text if document.respond_to?(:excerpts) && document.excerpts}
-  field(:filing_type, :select => :special_filing) {|document| document.special_filing? ? 'special' : 'regular'}
-  field(:html_url, :select => [:publication_date, :filed_at, :document_number, :slug]) {|document| entry_url(document) }
+  field(:excerpts, :select => [:document_file_path]) {|document| document.excerpts.raw_text if document.respond_to?(:excerpts) && document.excerpts}
+  field(:filing_type, :select => :special_filing) {|document| document.special_filing ? 'special' : 'regular'}
+  field(:html_url, :select => [:publication_date, :filed_at, :document_number]) {|document| entry_url(document) }
   field(:json_url, :select => :document_number) {|document| api_v1_public_inspection_document_url(document.document_number, :format => :json) }
   field(:raw_text_url, :select => :document_number) {|document|
     public_inspection_raw_text_url(document)
@@ -31,12 +31,13 @@ class PublicInspectionDocumentApiRepresentation < ApiRepresentation
   field(:num_pages)
   field(:publication_date)
   field(:type, :select => :granule_class) {|document| document.entry_type}
+  field(:pdf_file_name)
   field(:pdf_file_size)
   field(:pdf_updated_at)
   field(:pdf_url, :select => :document_number) {|document| document.pdf.url(:with_banner, false)}
-  field(:title)
-  field(:toc_doc)
-  field(:toc_subject)
+  field(:subject_1)
+  field(:subject_2)
+  field(:subject_3)
 
   def self.default_index_fields_json
     all_fields
