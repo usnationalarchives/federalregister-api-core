23da1bcd709a4fbc2137b84b02ed7f7fb1b9f3cf
Peregrinator <bob.burbach@gmail.com>
2016-08-01 15:20:23 -0700

    Only compile html if we expect to have full text xml available.

--- lib/content/entry_compiler.rb
+++ lib/content/entry_compiler.rb
@@ -1,10 +1,14 @@
 module Content
   module EntryCompiler
     @queue = :reimport
-    
+
     def self.perform(date)
       document_numbers = Entry.published_on(date).map(&:document_number)
-      Content.run_myfr2_command "bundle exec rake documents:html:compile:all[\"#{document_numbers.join(';')}\"] DATE=#{date}"
+      date = date.is_a?(String) ? Date.parse(date) : date
+
+      if date >= Date.parse('2000-01-18')
+        Content.run_myfr2_command "bundle exec rake documents:html:compile:all[\"#{document_numbers.join(';')}\"] DATE=#{date}"
+      end
     end
   end
 end
