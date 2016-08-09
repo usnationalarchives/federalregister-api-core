df6947c748b88776da1a2ef8eaecb0c67083f31d
Peregrinator <bob.burbach@gmail.com>
2016-08-09 10:46:32 -0700

    Update app to save connection in a $redis variable (and reconnect when passenger forks). This will allow us to have a consistent setup in our connection to redis.

---
+++ config/initializers/redis.rb
@@ -0,0 +1,21 @@
+REDIS_CONFIG = YAML.load_file(Rails.root + 'config/redis.yml')[Rails.env].freeze
+
+REDIS_CONNECTION_SETTINGS = {
+  :db   => REDIS_CONFIG['db'],
+  :host => REDIS_CONFIG['host'],
+  :port => REDIS_CONFIG['port']
+}
+
+$redis = Redis.new(REDIS_CONNECTION_SETTINGS)
+
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    # We're in smart spawning mode.
+    if forked
+      $redis.client.disconnect
+      $redis = Redis.new(REDIS_CONNECTION_SETTINGS)
+    end
+  end
+end
+
+Resque.redis = $redis
