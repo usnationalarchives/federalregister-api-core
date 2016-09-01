d180cd1f328a067d45f79776c8e18197097885c5
Peregrinator <bob.burbach@gmail.com>
2016-09-01 16:43:58 -0700

    Update the presidential document importer to fall back to the mods file for information if the bulkdata file isn’t available. This is mostly a fix for pre-2000 documents that don’t have bulkdata.

--- lib/content/entry_importer/presidential_document_details.rb
+++ lib/content/entry_importer/presidential_document_details.rb
@@ -7,30 +7,56 @@ module Content::EntryImporter::PresidentialDocumentDetails
     :executive_order_notes
 
   def presidential_document_type_id
-    child_node = @bulkdata_node && @bulkdata_node.xpath('./*').first
-    if child_node
-      return PresidentialDocumentType.find_by_node_name(child_node.name).try(:id)
+    if @bulkdata_node
+      child_node = @bulkdata_node.xpath('./*').first
+      if child_node
+        return PresidentialDocumentType.find_by_node_name(child_node.name).try(:id)
+      end
+    elsif @mods_node
+      presdoc_node = @mods_node.css('presidentialDoc')
+      if presdoc_node.present?
+        return PresidentialDocumentType.find_by_node_name(
+          presdoc_node.attr('type').try(:value)
+        ).try(:id)
+      end
     end
   end
 
   def signing_date
-    date_node = @bulkdata_node && @bulkdata_node.css('DATE').first
-    if date_node && date_node.text =~ /(\w+ \d+, \d{4})/
-      # eg 'January 18, 2007'
-      str = $1
-      begin
-        Date.strptime(str, '%B %d, %Y')
-      rescue
-        nil
+    if @bulkdata_node
+      date_node = @bulkdata_node.css('DATE').first
+      if date_node && date_node.text =~ /(\w+ \d+, \d{4})/
+        # eg 'January 18, 2007'
+        str = $1
+        begin
+          Date.strptime(str, '%B %d, %Y')
+        rescue
+          nil
+        end
+      end
+    elsif @mods_node
+      presdoc_node = @mods_node.css('presidentialDoc')
+
+      if presdoc_node.present?
+        return presdoc_node.attr('date').try(:value)
       end
     end
   end
 
   def executive_order_number
-    execordr_node = @bulkdata_node && @bulkdata_node.css('EXECORDR').first
-    if execordr_node
-      execordr_node.text.scan(/Executive Order (\d+)/) do |captures|
-        return captures.first.to_i
+    if @bulkdata_node
+      execordr_node = @bulkdata_node.css('EXECORDR').first
+
+      if execordr_node
+        execordr_node.text.scan(/Executive Order (\d+)/) do |captures|
+          return captures.first.to_i
+        end
+      end
+    elsif @mods_node
+      presdoc_node = @mods_node.css('presidentialDoc')
+
+      if presdoc_node.present?
+        return presdoc_node.attr('number').try(:value)
       end
     end
   end
