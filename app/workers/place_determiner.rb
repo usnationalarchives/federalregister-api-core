3d837c19e305fd66175622c376446a125eccc584
Brandon Carag <brandon@criticaljuncture.org>
2016-10-11 18:31:39 -0700

    Refactor logic out of the .perform class method to an instance method to better encapsulate state logic.  Implement chunking in API requests to accommodate what seems to be a 100,000 character limit.  Scan entire document text in lieu of just the abstract.

--- app/workers/place_determiner.rb
+++ app/workers/place_determiner.rb
@@ -1,46 +1,53 @@
 class PlaceDeterminer
-  MAX_RETRIES = 5
-  RETRY_DELAY_IN_SECONDS = 5
   @queue = :default
 
+  MAX_RETRIES             = 5
+  RETRY_DELAY_IN_SECONDS  = 5
+  CHARACTER_REQUEST_LIMIT = 99000
+
   def self.perform(entry_id)
+    new(entry_id).perform
+  end
+
+  def initialize(entry_id)
+    @entry = Entry.find(entry_id)
+  end
+
+  def perform
     begin
       retries ||= 0
-      entry = Entry.find(entry_id)
 
-      if entry.abstract.present?
-        locations = OpenCalais::ClientWrapper.new(entry.abstract).locations
-
+      if entry.raw_text.present?
         locations.each do |location|
-          if location[:latitude] && location[:longitude] #NOTE: OpenCalais sometimes sends locations without latitudes and longitudes.
-            place = Place.find_by_open_calais_guid(location[:guid])
-            unless place
-              place = Place.create(
-                name: location[:name],
-                open_calais_guid: location[:guid],
-                latitude: location[:latitude],
-                longitude: location[:longitude],
-                place_type: location[:type],
-              )
-            end
+          place = Place.find_by_open_calais_guid(location[:guid])
 
-            unless PlaceDetermination.find(
-              :first,
-              :conditions => {:entry_id => entry.id, :place_id => place.id}
+          unless place
+            place = Place.create(
+              name:             location[:name],
+              open_calais_guid: location[:guid],
+              latitude:         location[:latitude],
+              longitude:        location[:longitude],
+              place_type:       location[:type]
             )
-              PlaceDetermination.create(
-                entry_id: entry.id,
-                place_id: place.id,
-                string: location[:name],
-                relevance_score: location[:score]
-              )
-            end
           end
+
+          unless PlaceDetermination.find(
+            :first,
+            :conditions => {:entry_id => entry.id, :place_id => place.id}
+          )
+            PlaceDetermination.create(
+              entry_id:        entry.id,
+              place_id:        place.id,
+              string:          location[:name],
+              relevance_score: location[:score]
+            )
+          end
+
         end
 
-      entry.places_determined_at = Time.now
-      entry.save
-    end
+        entry.places_determined_at = Time.now
+        entry.save
+      end
 
     rescue StandardError => e
       if e.class == Faraday::ParsingError
@@ -52,6 +59,35 @@ class PlaceDeterminer
 
       Honeybadger.notify(e)
     end
+  end
+
+
+  private
+
+  attr_reader :entry
+
+  def locations
+    entry_text_segments.each_with_object([]) do |text_segment, responses|
+      location_response = OpenCalais::ClientWrapper.new(text_segment).locations
+      if location_response.present?
+        responses << location_response
+      end
+    end.
+    flatten.
+    select{|location| location[:latitude] && location[:longitude] }
+    #NOTE: OpenCalais sometimes sends locations without latitudes and longitudes.
+  end
+
+  def entry_text_segments
+    segments    = []
+    index_start = 0
+
+    while index_start <= entry.raw_text.size
+      segments << entry.raw_text.slice(index_start, CHARACTER_REQUEST_LIMIT)
+      index_start += CHARACTER_REQUEST_LIMIT
+    end
+
+    segments
   end
 
 end
