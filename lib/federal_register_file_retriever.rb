22984615b6b388c4d80288d7251dbaec17f15272
Peregrinator <bob.burbach@gmail.com>
2017-03-16 11:52:38 -0700

    Update the file retriever to notify us when there is a problem retrieving a url (40x, 50x).

--- lib/federal_register_file_retriever.rb
+++ lib/federal_register_file_retriever.rb
@@ -2,17 +2,27 @@ class FederalRegisterFileRetriever
   def self.download(url, path)
     puts "downloading #{url} to #{path}"
 
-    Curl::Easy.download(url, path) do |c|
+    curl = Curl::Easy.download(url, path) do |c|
       c.follow_location = follow_location
       c.headers["User-Agent"] = user_agent
     end
+    curl.on_missing{|c, code| notify_error(c, code, url)}
+    curl.on_failure{|c, code| notify_error(c, code, url)}
+
+    curl.peform
+    curl
   end
 
   def self.http_get(url)
-    Curl::Easy.http_get(url) do |c|
+    curl = Curl::Easy.http_get(url) do |c|
       c.follow_location = follow_location
       c.headers["User-Agent"] = user_agent
     end
+    curl.on_missing{|c, code| notify_error(c, code, url)}
+    curl.on_failure{|c, code| notify_error(c, code, url)}
+
+    curl.perform
+    curl
   end
 
   def self.user_agent
@@ -21,5 +31,19 @@ class FederalRegisterFileRetriever
 
   def self.follow_location
     true
+  end
+
+  private
+
+  def self.notify_error(curl, code, url)
+    Honeybadger.notify(
+      error_class: 'FederalRegisterFileRetriever::DownloadError',
+      error_message: "Problem downloading #{url}",
+      context: {
+        code: code,
+        curl: curl,
+        url: url,
+      }
+    )
   end
 end
