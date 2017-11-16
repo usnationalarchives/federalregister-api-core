0d6e4248a45d7c716f7ae5fa16fa5feb828c0fb0
Peregrinator <bob.burbach@gmail.com>
2017-11-15 17:25:13 -0800

    Support finding documents by citation in API. This supports fuzzy matching of documents via start page and end page such that a citation for any page of a document will return that document.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -53,9 +53,19 @@ class ApiController < ApplicationController
     render_json_or_jsonp(data)
   end
 
-  def render_one_or_more(model, document_numbers, find_options={})
+  def render_one_or_more(model, document_numbers_or_citations, find_options={}, &block)
+    if document_numbers_or_citations =~ /FR/
+      data = render_via_citations(model, document_numbers_or_citations, find_options, &block)
+    else
+      data = render_via_document_numbers(model, document_numbers_or_citations, find_options, &block)
+    end
+
+    render_json_or_jsonp data
+  end
+
+  def render_via_document_numbers(model, document_numbers, find_options={}, &block)
     if document_numbers =~ /,/
-      document_numbers = params[:id].split(',')
+      document_numbers = document_numbers.split(',')
       records = model.all(find_options.merge(
         :conditions => {:document_number => document_numbers}
       ))
@@ -73,7 +83,43 @@ class ApiController < ApplicationController
       record = model.find_by_document_number!(document_numbers)
       data = yield(record)
     end
-    render_json_or_jsonp data
+
+    data
+  end
+
+  def render_via_citations(model, citations, find_options={}, &block)
+    if citations =~ /,/
+      citations = citations.split(',')
+    else
+      citations = Array(citations)
+    end
+
+    records = []
+    matched_citations = []
+    citations.each do |citation|
+      volume, fr_str, page = citation.split(' ')
+      matches = model.all(find_options.merge(
+        :conditions => ["volume = ? AND start_page <= ? AND end_page >= ?", volume.to_i, page.to_i, page.to_i]
+      ))
+
+      if matches.present?
+        matched_citations << citation
+        records << matches
+      end
+    end
+    records.flatten!
+
+    data = {
+      :count => records.count,
+      :results => records.map{|record| yield(record)}
+    }
+
+    missing = citations - matched_citations
+    if missing.present?
+      data[:errors] = {:not_found => missing}
+    end
+
+    data
   end
 
   def specified_fields
