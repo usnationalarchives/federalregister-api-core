63f4788f81ea53d773df1da5d10e191d1b38e207
Peregrinator <bob.burbach@gmail.com>
2017-12-20 15:59:33 -0800

    Make sphinx config (file locations) less environment dependent

--- lib/sphinx_indexer.rb
+++ lib/sphinx_indexer.rb
@@ -1,5 +1,6 @@
 module SphinxIndexer
   def self.perform(*index_names)
-    system("/usr/local/bin/indexer -c #{Rails.root.join('config', "#{Rails.env}.sphinx.conf")} #{index_names.join(' ')} --rotate")
+    config = ThinkingSphinx::Configuration.instance
+    system("/usr/local/bin/indexer -c #{config.config_file} #{index_names.join(' ')} --rotate")
   end
 end
