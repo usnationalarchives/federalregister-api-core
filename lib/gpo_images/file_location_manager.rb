0833ac7d04be8c5eba5701bb0e4b2eae560fe40c
Peregrinator <bob.burbach@gmail.com>
2018-02-27 10:01:32 -0800

    Change location of where we store images during processing to be available to all instances of the service via EFS

--- lib/gpo_images/file_location_manager.rb
+++ lib/gpo_images/file_location_manager.rb
@@ -1,22 +1,28 @@
 class GpoImages::FileLocationManager
 
   def self.temp_images_path
-    File.join(Rails.root, 'tmp', 'gpo_images', 'temp_image_files')
+    File.join(gpo_image_dir, 'temp_image_files')
   end
 
   def self.temp_zip_files_path
-    File.join(Rails.root, 'tmp', 'gpo_images', 'temp_zip_files')
+    File.join(gpo_image_dir, 'temp_zip_files')
   end
 
   def self.compressed_image_bundles_path
-    File.join(Rails.root, 'tmp', 'gpo_images', 'compressed_image_bundles')
+    File.join(gpo_image_dir, 'compressed_image_bundles')
   end
 
   def self.uncompressed_eps_images_path(package_identifer)
-    File.join(Rails.root, 'tmp', 'gpo_images', 'uncompressed_eps_images', package_identifer)
+    File.join(gpo_image_dir, 'uncompressed_eps_images', package_identifer)
   end
 
   def self.eps_image_manifest_path
-    File.join(Rails.root, 'data', 'gpo_images')
+    File.join(gpo_image_dir, 'manifests')
+  end
+
+  private
+
+  def self.gpo_image_dir
+    File.join(Rails.root, 'data', 'efs', 'gpo_images')
   end
 end
