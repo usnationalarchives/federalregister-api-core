78817bf98e5fa8ab604306f3e52ec29e9ec5686e
Peregrinator <bob.burbach@gmail.com>
2018-03-24 16:25:38 -0700

    Add reg_gov queue to worker_2. Move gpo_image_import queue higher in priority than reimport.

--- lib/scripts/resque_worker_default_1.sh
+++ lib/scripts/resque_worker_default_1.sh
@@ -2,4 +2,4 @@
 source /usr/local/rvm/environments/ruby-1.9.3-p551
 
 cd /var/www/apps/federalregister-api-core
-nohup /usr/local/rvm/wrappers/ruby-1.9.3-p551/bundle exec rake environment resque:work RAILS_ENV=production QUEUE=public_inspection,fr_index_pdf_previewer,fr_index_pdf_publisher,default,reimport,gpo_image_import INTERVAL=1 VERBOSE=1 PIDFILE=/var/www/apps/federalregister-api-core/tmp/pids/resque_worker_default_1.pid >> /var/www/apps/federalregister-api-core/log/resque_worker_default.log 2>&1 &
+nohup /usr/local/rvm/wrappers/ruby-1.9.3-p551/bundle exec rake environment resque:work RAILS_ENV=production QUEUE=public_inspection,fr_index_pdf_previewer,fr_index_pdf_publisher,default,gpo_image_import,reimport INTERVAL=1 VERBOSE=1 PIDFILE=/var/www/apps/federalregister-api-core/tmp/pids/resque_worker_default_1.pid >> /var/www/apps/federalregister-api-core/log/resque_worker_default.log 2>&1 &
