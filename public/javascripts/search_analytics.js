726de38169b14d258263e13ad85e56516dce8ee2
Peregrinator <bob.burbach@gmail.com>
2018-11-13 12:54:12 -0800

    Remove old assets

--- public/javascripts/search_analytics.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  // SEACH SUGGESTIONS
-  $("#search").on('click', '.suggestions .suggestion a', function() {
-    _gaq.push(['_trackEvent', 'Search', 'Suggestion', ""]);
-  });
-});
