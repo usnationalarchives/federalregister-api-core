02be448c78ae5762607ddabfb0bf278350084a18
Brandon Carag <brandon@criticaljuncture.org>
2019-03-20 17:08:34 -0700

    Add spec around raising an error for large date ranges

--- spec/lib/effective_date_generator_spec.rb
+++ spec/lib/effective_date_generator_spec.rb
@@ -2,6 +2,17 @@ require 'spec_helper'
 
 describe EffectiveDateGenerator do
 
+  it "returns an error if a date range larger than 120 days is requested" do
+    expect do
+      EffectiveDateGenerator.
+        new.
+        perform(
+          Date.new(2019,1,1),
+          Date.new(2019,5,3)
+        )
+    end.to raise_error(EffectiveDateGenerator::DateRangeTooLarge)
+  end
+
   it "returns the correct date in a normal (non-weekend/non-holiday) scenario" do
     result = EffectiveDateGenerator.
       new.
