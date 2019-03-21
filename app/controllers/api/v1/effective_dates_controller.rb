f23e2c72152dc7f6e262585ed56f8f65da569c8a
Brandon Carag <brandon@criticaljuncture.org>
2019-03-20 17:09:14 -0700

    Render more descriptive JSON on large-sized request failures

--- app/controllers/api/v1/effective_dates_controller.rb
+++ app/controllers/api/v1/effective_dates_controller.rb
@@ -5,12 +5,18 @@ class Api::V1::EffectiveDatesController < ApiController
       cache_for 1.day
 
       wants.json do
-        effective_dates = EffectiveDateGenerator.new.perform(
-          Date.parse(params[:start_date]),
-          Date.parse(params[:end_date])
-        )
-
-        render_json_or_jsonp effective_dates
+        begin
+          effective_dates = EffectiveDateGenerator.new.perform(
+            Date.parse(params[:start_date]),
+            Date.parse(params[:end_date])
+          )
+          render_json_or_jsonp effective_dates
+        rescue EffectiveDateGenerator::DateRangeTooLarge
+          render_json_or_jsonp(
+            {error: "Request size must be smaller than #{EffectiveDateGenerator::MAX_DAYS_ALLOWED} days"},
+            status: 400
+          )
+        end
       end
     end
   end
