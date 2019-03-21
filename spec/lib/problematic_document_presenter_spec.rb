1b9bdc6209fc61d6f4b43e54d4e9a51492aea1fe
Brandon Carag <brandon@criticaljuncture.org>
2019-03-21 13:10:50 -0700

    Add specs around missing TOC/XML documents

---
+++ spec/lib/problematic_document_presenter_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe ProblematicDocumentPresenter do
+  let(:toc_json) {
+    <<-JSON
+      {
+        "agencies": [
+          {
+            "name": "Veterans Affairs Department",
+            "slug": "veterans-affairs-department",
+            "document_categories": [
+              {
+                "type": "Notice",
+                "documents": [
+                  {
+                    "subject_1": "Cost-of-Living Adjustments for Service-Connected Benefits",
+                    "document_numbers": ["2019-05287"]
+                  },
+                  {
+                    "subject_1": "Meetings:",
+                    "subject_2": "Veterans' Rural Health Advisory Committee",
+                    "document_numbers": ["2019-05272"]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    JSON
+   }
+
+  it "#documents_present_in_toc_but_not_in_xml" do
+    date  = Date.new(2019,3,20)
+    issue = Factory(:issue, publication_date: date)
+    entry = Factory(:entry, document_number: '2019-05287', publication_date: date)
+    ProblematicDocumentPresenter.stub_any_instance(:toc_json_file_contents => StringIO.new(toc_json).read)
+
+    result = ProblematicDocumentPresenter.new(date).documents_present_in_toc_but_not_in_xml
+
+    result.should == ["2019-05272"]
+  end
+
+  it "#documents_present_in_xml_but_not_in_toc" do
+    date  = Date.new(2019,3,20)
+    issue = Factory(:issue, publication_date: date)
+    entry = Factory(:entry, document_number: '2019-05289', publication_date: date)
+    ProblematicDocumentPresenter.stub_any_instance(:toc_json_file_contents => StringIO.new(toc_json).read)
+
+    result = ProblematicDocumentPresenter.new(date).documents_present_in_xml_but_not_in_toc
+
+    result.should == ['2019-05289']
+  end
+
+end
