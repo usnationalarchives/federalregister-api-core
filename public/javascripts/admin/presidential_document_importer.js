c280f3da3002e5b4ef684a190b2d0e6bdebcc7e3
Brandon Carag <brandon@criticaljuncture.org>
2019-03-25 11:12:00 -0700

    Add semicolon js punctuation per linter

--- public/javascripts/admin/presidential_document_importer.js
+++ public/javascripts/admin/presidential_document_importer.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
-  var delayInterval = 5000
+  var delayInterval = 5000;
 
-  if ($('.eo-import-status').data('status') == 'continue-polling') {
-    setInterval(function() {window.location.reload()}, delayInterval)
+  if ($('.eo-import-status').data('status') === 'continue-polling') {
+    setInterval(function() {window.location.reload();}, delayInterval);
   }
 
 });
