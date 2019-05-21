fd241bd2d46ecff8ce841e0ebdd2f267a0762674
Peregrinator <bob.burbach@gmail.com>
2019-05-21 14:39:32 -0700

    Update: Add new missing v3 reg gov search attributes to class. #93

--- lib/regulations_dot_gov/search_document.rb
+++ lib/regulations_dot_gov/search_document.rb
@@ -3,12 +3,54 @@
 # V3 of the Reg.gov document search API does not nest attributes. We
 # deal with that difference here - and only for attributes we care about.
 class RegulationsDotGov::SearchDocument < RegulationsDotGov::Document
+  def agency_acronym
+    raw_attributes['agencyAcronym']
+  end
+
+  def allow_late_comment
+    raw_attributes['allowLateComment']
+  end
+
+  def attachment_count
+    raw_attributes['attachmentCount']
+  end
+
+  def comment_count
+    raw_attributes['numberofCommentsReceived']
+  end
+
+  def comment_start_date
+    val = raw_attributes["commentStartDate"]
+    if val.present?
+      DateTime.parse(val)
+    end
+  end
+
   def document_id
     raw_attributes['documentId']
   end
 
+  def docket_id
+    raw_attributes['docket_id']
+  end
+
+  def docketTitle
+    raw_attributes['docketTitle']
+  end
+
+  def posted_date
+    val = raw_attributes['postedDate']
+    if val.present?
+      DateTime.parse(val)
+    end
+  end
+
   def title
     raw_attributes['title']
+  end
+
+  def federal_register_document_number
+    raw_attributes['frNumber']
   end
 
   # backwards compatibility
