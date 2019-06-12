b75b8e8fe8719c4ab745ffa05306391fa2d2b115
Peregrinator <bob.burbach@gmail.com>
2019-06-12 13:10:07 -0700

    Update: Move resque throttle settings from a rake task to an initializer. Move log permission setting from individual worker files (redundant) to container startup.

---
+++ config/initializers/resque.rb
@@ -0,0 +1,9 @@
+# at: jobs
+# per: second
+
+# hourly rate limit of 1000
+# docket importer makes 4 total requests per job
+Resque.rate_limit(:reg_gov,
+  at: SETTINGS['regulations_dot_gov']['throttle']['at'],
+  per: SETTINGS['regulations_dot_gov']['throttle']['per']
+)
