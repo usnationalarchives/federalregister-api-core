f32202fcf0f3eb984d2a07f796d41b8aa6e742a1
Brandon Carag <brandon@criticaljuncture.org>
2019-11-12 14:27:32 -0800

    WIP—experimenting

--- Rakefile
+++ Rakefile
@@ -1,11 +1,12 @@
 # Add your own tasks in files placed in lib/tasks ending in .rake,
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
-
+require File.expand_path('../config/application', __FILE__)
 require(File.join(File.dirname(__FILE__), 'config', 'environment'))
 
 require 'rake'
 require 'rake/testtask'
 require 'rdoc/task'
+
 
 require 'tasks/rails'
 require 'honeybadger/tasks'
@@ -15,3 +16,14 @@ begin
   require 'thinking_sphinx/tasks'
 rescue LoadError
 end
+
+
+
+# Brandon's new stuff
+# puts Rails.application.class.parent_name
+
+
+
+# require File.expand_path('../config/application', __FILE__)
+
+# FR2::Application.load_tasks
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
-
 # Use deployed git commit hash as quick & easy cache busting strategy
 ENV["RAILS_ASSET_ID"] = `git log -n 1 --pretty=format:%H`
 
@@ -9,34 +8,34 @@ if (File.basename($0) == 'rake' && (ARGV.include?('db:migrate')|| ARGV.include?(
 end
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.18.17' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '3.1.12' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+
 
 # require our patched logger
 require File.join(RAILS_ROOT, "lib", "binary_buffered_logger")
 
 # patch for Rails 2.x with Ruby 2+
 # TODO: BB remove after upgrade
-if Rails::VERSION::MAJOR == 2 && RUBY_VERSION >= '2.0.0'
-  module Gem
-    def self.source_index
-      sources
-    end
-    def self.cache
-      sources
-    end
-    SourceIndex = Specification
-    class SourceList
-      # If you want vendor gems, this is where to start writing code.
-      def search(*args); []; end
-      def each(&block); end
-      include Enumerable
-    end
-  end
-end
-
+# if Rails::VERSION::MAJOR == 2 && RUBY_VERSION >= '2.0.0'
+#   module Gem
+#     def self.source_index
+#       sources
+#     end
+#     def self.cache
+#       sources
+#     end
+#     SourceIndex = Specification
+#     class SourceList
+#       # If you want vendor gems, this is where to start writing code.
+#       def search(*args); []; end
+#       def each(&block); end
+#       include Enumerable
+#     end
+#   end
+# end
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
@@ -76,6 +75,8 @@ Rails::Initializer.run do |config|
   # config.i18n.default_locale = :de
 
   config.action_controller.session = {
+    # :key => '_myapp_session',
+    # :secret => 'Abcd1234',
     :cookie_only => true,
     :path => '/admin',
   }
