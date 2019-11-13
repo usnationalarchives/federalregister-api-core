bd3e2caac112fc751142432b080f1c331dc73c81
Brandon Carag <brandon@criticaljuncture.org>
2019-11-13 11:46:08 -0800

    Switch all requests local API to Rails 3 version

--- vendor/plugins/sitemap_generator/test/mock_app/config/environments/production.rb
+++ vendor/plugins/sitemap_generator/test/mock_app/config/environments/production.rb
@@ -5,7 +5,7 @@
 config.cache_classes = true
 
 # Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
+config.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
