9c1b0cacf2d8ee4cde4a51cd362a80af9597971d
Brandon Carag <brandon@criticaljuncture.org>
2019-11-13 11:42:42 -0800

    Wrap environment configuration settings in new Rails 3 block config syntax

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,8 +12,7 @@ FR2::Application.configure do
   config.whiny_nils = true
 
   # Show full error reports and disable caching
-  # config.action_controller.consider_all_requests_local = true     # TODO: BC Look into
-
+  config.consider_all_requests_local = true
 
   # config.action_view.debug_rjs                         = true
   config.action_controller.perform_caching             = false
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,57 +1,59 @@
 APP_HOST_NAME = 'fr2.criticaljuncture.org'
 
-# Settings specified here will take precedence over those in config/environment.rb
+FR2::Application.configure do
+  # Settings specified here will take precedence over those in config/environment.rb
 
-# The production environment is meant for finished, "live" apps.
-# Code is not reloaded between requests
-config.cache_classes = true
+  # The production environment is meant for finished, "live" apps.
+  # Code is not reloaded between requests
+  config.cache_classes = true
 
-# Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
-config.action_view.cache_template_loading            = true
+  # Full error reports are disabled and caching is turned on
+  config.consider_all_requests_local = false
+  config.action_controller.perform_caching             = true
+  config.action_view.cache_template_loading            = true
 
-# See everything in the log (default is :info)
-config.log_level = :debug
+  # See everything in the log (default is :info)
+  config.log_level = :debug
 
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
+  # Use a different logger for distributed setups
+  # config.logger = SyslogLogger.new
 
-# Use a different cache store in production
-# config.cache_store = :mem_cache_store
+  # Use a different cache store in production
+  # config.cache_store = :mem_cache_store
 
-# Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host = "http://assets.example.com"
+  # Enable serving of images, stylesheets, and javascripts from an asset server
+  # config.action_controller.asset_host = "http://assets.example.com"
 
-SECRETS = YAML::load(
-  ERB.new(
-    File.read(
-      File.join(File.dirname(__FILE__), '..', 'secrets.yml')
-    )
-  ).result
-)
+  SECRETS = YAML::load(
+    ERB.new(
+      File.read(
+        File.join(File.dirname(__FILE__), '..', 'secrets.yml')
+      )
+    ).result
+  )
 
-smtp_settings = {
-  :address        => "smtp.sendgrid.net",
-  :port           => "587",
-  :domain         => "#{APP_HOST_NAME}",
-  :user_name      => SECRETS['sendgrid']['username'],
-  :password       => SECRETS['sendgrid']['password'],
-  :authentication => :plain
-}
+  smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "587",
+    :domain         => "#{APP_HOST_NAME}",
+    :user_name      => SECRETS['sendgrid']['username'],
+    :password       => SECRETS['sendgrid']['password'],
+    :authentication => :plain
+  }
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings   = smtp_settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings   = smtp_settings
 
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
-config.action_mailer.default_url_options = {:host => "www.#{APP_HOST_NAME}"}
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {:host => "www.#{APP_HOST_NAME}"}
 
-# Enable threaded mode
-# config.threadsafe!
+  # Enable threaded mode
+  # config.threadsafe!
 
-# Commented out in as password protection is really annoying
-# config.action_controller.asset_host = "http://assets%d.fr2.criticaljuncture.org"
+  # Commented out in as password protection is really annoying
+  # config.action_controller.asset_host = "http://assets%d.fr2.criticaljuncture.org"
+end
 
 # Put gems in Gemfile...
 ThinkingSphinx.remote_sphinx = true
