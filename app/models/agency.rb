31e8b6da73e0dae83f3fe240d8e32229d2490964
Brandon Carag <brandon@criticaljuncture.org>
2019-11-15 17:07:28 -0800

    Add scopes formerly provided by search logic

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -64,6 +64,7 @@ class Agency < ApplicationModel
   scope :with_logo, :conditions => "agencies.logo_file_name IS NOT NULL"
   scope :with_entries, :conditions => "agencies.entries_count > 0"
   scope :alphabetically, :order => "agencies.name"
+  scope :active, where(active: true)
 
   # consider using sphinx instead...
   def self.named_approximately(name)
--- app/models/regulatory_plan.rb
+++ app/models/regulatory_plan.rb
@@ -44,6 +44,8 @@ class RegulatoryPlan < ApplicationModel
   has_many :agencies, :through => :agency_assignments, :extend => Agency::AssociationExtensions
   has_and_belongs_to_many :small_entities
 
+  scope :current, where(current: true)
+
   def entries
     Entry.with_regulation_id_number(self.regulation_id_number)
   end
