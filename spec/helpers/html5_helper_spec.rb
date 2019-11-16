706e6efad4e945f3c20beee34a877654c2e723e3
Brandon Carag <brandon@criticaljuncture.org>
2019-11-15 17:12:05 -0800

    Address #eval_erb deprecations for non-block invocations of html5_helper

--- spec/helpers/html5_helper_spec.rb
+++ spec/helpers/html5_helper_spec.rb
@@ -1,46 +1,40 @@
+require 'spec_helper'
+include Html5Helper
+include ActionView::Helpers::TagHelper
+
 describe Html5Helper do
   describe "html5_tag" do
     context "with a tag name" do
       it "creates the html5 tag and child div" do
-        output = eval_erb <<-ERB
-          <%= html5_tag(:section) %>
-        ERB
+        output = html5_tag(:section)
         output.strip.should == '<section><div class="section"></div></section>'
       end
     end
 
     context "with a tag name and some content" do
       it "creates the html5 tag and child div with the content inside" do
-        output = eval_erb <<-ERB
-          <%= html5_tag(:section, "hi") %>
-        ERB
+        output = html5_tag(:section, "hi")
         output.strip.should == '<section><div class="section">hi</div></section>'
       end
     end
 
     context "with a tag name and a class" do
       it "creates the html5 tag and a div with the class" do
-        output = eval_erb <<-ERB
-          <%= html5_tag(:section, :class => "primary") %>
-        ERB
+        output = html5_tag(:section, :class => "primary")
         output.strip.should == '<section><div class="section primary"></div></section>'
       end
     end
 
     context "with a tag name and a class and an id" do
       it "creates the html5 tag and a div with the class and an id" do
-        output = eval_erb <<-ERB
-          <%= html5_tag(:section, :class => "primary", :id => "footer") %>
-        ERB
+        output = html5_tag(:section, :class => "primary", :id => "footer")
         output.strip.should == '<section><div class="section primary" id="footer"></div></section>'
       end
     end
 
     context "with a tag name and an arbitrary attribute" do
       it "creates the html5 tag with the attribute and a div with the attribute and the class" do
-        output = eval_erb <<-ERB
-          <%= html5_tag(:section, :class => "primary", :foo => "bar") %>
-        ERB
+        output = html5_tag(:section, :class => "primary", :foo => "bar")
         output.strip.should == '<section foo="bar"><div class="section primary" foo="bar"></div></section>'
       end
     end
@@ -66,13 +60,8 @@ describe Html5Helper do
 
   describe "section_tag" do
     it "should work like html5_tag(:section)" do
-      section_tag_output = eval_erb <<-ERB
-        <%= section_tag("Hi", :class => "start") %>
-      ERB
-
-      html5_tag_output = eval_erb <<-ERB
-        <%= html5_tag(:section, "Hi", :class => "start") %>
-      ERB
+      section_tag_output = section_tag("Hi", :class => "start")
+      html5_tag_output   = html5_tag(:section, "Hi", :class => "start")
 
       section_tag_output.strip.should == html5_tag_output.strip
     end
