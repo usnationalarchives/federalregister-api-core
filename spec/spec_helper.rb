1fc9b7be7f8602f1758524bbe5f117eccbc2193f
Brandon Carag <brandon@criticaljuncture.org>
2019-11-19 13:07:06 -0800

    Add stand-in methods to support existing specs failing due to deprecations in Rails 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@ require "factory_girl"
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
+include Rails3UpgradeSpecHelperMethods
+
 RSpec.configure do |config|
   # == Mock Framework
   #
