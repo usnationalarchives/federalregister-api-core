79e6b679b96adef99732849783f3266785f880a6
Brandon Carag <brandon@criticaljuncture.org>
2019-11-20 12:09:01 -0800

    Remove fragment_fu plugin since none of the helpers appear to be called

--- vendor/plugins/fragment_fu/init.rb
+++
@@ -1,1 +0,0 @@
-require "fragment_fu"
--- vendor/plugins/fragment_fu/lib/fragment_fu.rb
+++
@@ -1,4 +0,0 @@
-require 'fragment_fu/render_with_esi_option'
-require "fragment_fu/invalidation_helper"
-ActionController::Base.send(:include, FragmentFu::InvalidationHelper)
-ActionView::Base.send(:include, FragmentFu::RenderWithEsi)
