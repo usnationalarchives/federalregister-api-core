64d190e64caa76ec758c604f2d8772259bb277d3
Brandon Carag <brandon@criticaljuncture.org>
2019-11-25 16:19:55 -0800

    Re-enable API key assignment for reg_dot_gov client

--- config/initializers/regulations_dot_gov.rb
+++ config/initializers/regulations_dot_gov.rb
@@ -1,6 +1,1 @@
-# RegulationsDotGov::Client.api_key = SECRETS['data_dot_gov']['api_key'] || 'DEMO_KEY'
-
-if false#Rails.env.development? || Rails.env.test? || Rails.env.staging?
-  RegulationsDotGov::Client.override_base_uri('http://api.data.gov/TEST/regulations/v3/')
-end
-
+RegulationsDotGov::Client.api_key = SECRETS['data_dot_gov']['api_key'] || 'DEMO_KEY'
