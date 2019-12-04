51acd8a604cf4fc7e9d7e50ffe28650babccb8dc
Brandon Carag <brandon@criticaljuncture.org>
2019-12-04 14:25:18 -0800

    Temporarily comment out sphinx indices since new version requires app/indices-style definitions

--- app/models/public_inspection_document.rb
+++ app/models/public_inspection_document.rb
@@ -33,76 +33,76 @@ class PublicInspectionDocument < ApplicationModel
   scope :revoked, :conditions => {:publication_date => nil}
   include Shared::DoesDocumentNumberNormalization
 
-  define_index do
-    # fields
-    indexes <<-SQL, :as => :title
-      CONCAT(
-        IFNULL(public_inspection_documents.subject_1, ''),
-        ' ',
-        IFNULL(public_inspection_documents.subject_2, ''),
-        ' ',
-        IFNULL(public_inspection_documents.subject_3, '')
-      )
-    SQL
-    indexes "CONCAT('#{FileSystemPathManager.data_file_path}/public_inspection/raw/', public_inspection_documents.document_file_path, '.txt')", :as => :full_text, :file => true
-    indexes "GROUP_CONCAT(DISTINCT docket_numbers.number SEPARATOR ' ')", :as => :docket_id
+  # define_index do
+  #   # fields
+  #   indexes <<-SQL, :as => :title
+  #     CONCAT(
+  #       IFNULL(public_inspection_documents.subject_1, ''),
+  #       ' ',
+  #       IFNULL(public_inspection_documents.subject_2, ''),
+  #       ' ',
+  #       IFNULL(public_inspection_documents.subject_3, '')
+  #     )
+  #   SQL
+  #   indexes "CONCAT('#{FileSystemPathManager.data_file_path}/public_inspection/raw/', public_inspection_documents.document_file_path, '.txt')", :as => :full_text, :file => true
+  #   indexes "GROUP_CONCAT(DISTINCT docket_numbers.number SEPARATOR ' ')", :as => :docket_id
 
-    # attributes
-    has "CRC32(document_number)", :as => :document_number, :type => :integer
-    has "public_inspection_documents.id", :as => :public_inspection_document_id, :type => :integer
-    has "CRC32(IF(public_inspection_documents.granule_class = 'SUNSHINE', 'NOTICE', public_inspection_documents.granule_class))", :as => :type, :type => :integer
-    has agency_assignments(:agency_id), :as => :agency_ids
-    has publication_date
-    has filed_at
-    has special_filing
+  #   # attributes
+  #   has "CRC32(document_number)", :as => :document_number, :type => :integer
+  #   has "public_inspection_documents.id", :as => :public_inspection_document_id, :type => :integer
+  #   has "CRC32(IF(public_inspection_documents.granule_class = 'SUNSHINE', 'NOTICE', public_inspection_documents.granule_class))", :as => :type, :type => :integer
+  #   has agency_assignments(:agency_id), :as => :agency_ids
+  #   has publication_date
+  #   has filed_at
+  #   has special_filing
 
-    join docket_numbers
+  #   join docket_numbers
 
-    join public_inspection_issues
+  #   join public_inspection_issues
 
-    set_property :field_weights => {
-      "title" => 100,
-      "full_text" => 25,
-      "agency_name" => 10
-    }
+  #   set_property :field_weights => {
+  #     "title" => 100,
+  #     "full_text" => 25,
+  #     "agency_name" => 10
+  #   }
 
 
-    if AppConfig.sphinx.use_local_pil_date
-      where <<-SQL
-        public_inspection_postings.issue_id =
-          (
-            SELECT id
-            FROM public_inspection_issues
-            WHERE published_at >= #{AppConfig.sphinx.pil_index_since_date}
-            ORDER BY publication_date DESC
-            LIMIT 1
-          )
-        AND (
-          publication_date IS NULL
-          OR publication_date > #{AppConfig.sphinx.pil_index_since_date}
-        )
-      SQL
-    else
-      where <<-SQL
-        public_inspection_postings.issue_id =
-          (
-            SELECT id
-            FROM public_inspection_issues
-            WHERE published_at IS NOT NULL
-            ORDER BY publication_date DESC
-            LIMIT 1
-          )
-        AND (
-          public_inspection_documents.publication_date IS NULL
-          OR public_inspection_documents.publication_date > (
-            SELECT MAX(publication_date)
-            FROM issues
-            WHERE issues.completed_at IS NOT NULL
-          )
-        )
-      SQL
-    end
-  end
+  #   if AppConfig.sphinx.use_local_pil_date
+  #     where <<-SQL
+  #       public_inspection_postings.issue_id =
+  #         (
+  #           SELECT id
+  #           FROM public_inspection_issues
+  #           WHERE published_at >= #{AppConfig.sphinx.pil_index_since_date}
+  #           ORDER BY publication_date DESC
+  #           LIMIT 1
+  #         )
+  #       AND (
+  #         publication_date IS NULL
+  #         OR publication_date > #{AppConfig.sphinx.pil_index_since_date}
+  #       )
+  #     SQL
+  #   else
+  #     where <<-SQL
+  #       public_inspection_postings.issue_id =
+  #         (
+  #           SELECT id
+  #           FROM public_inspection_issues
+  #           WHERE published_at IS NOT NULL
+  #           ORDER BY publication_date DESC
+  #           LIMIT 1
+  #         )
+  #       AND (
+  #         public_inspection_documents.publication_date IS NULL
+  #         OR public_inspection_documents.publication_date > (
+  #           SELECT MAX(publication_date)
+  #           FROM issues
+  #           WHERE issues.completed_at IS NOT NULL
+  #         )
+  #       )
+  #     SQL
+  #   end
+  # end
 
   # Note: the concept of 'unpublished' is different from that of 'pending publication'
   def self.unpublished
--- app/models/regulatory_plan.rb
+++ app/models/regulatory_plan.rb
@@ -50,29 +50,29 @@ class RegulatoryPlan < ApplicationModel
     Entry.with_regulation_id_number(self.regulation_id_number)
   end
 
-  define_index do
-    # Will require a index rebuild when new regulatory plan issue comes in...
-    where "regulatory_plans.issue = '#{RegulatoryPlan.current_issue}'"
+  # define_index do
+  #   # Will require a index rebuild when new regulatory plan issue comes in...
+  #   where "regulatory_plans.issue = '#{RegulatoryPlan.current_issue}'"
 
-    # fields
-    indexes title
-    indexes abstract
-    indexes "CONCAT('#{FileSystemPathManager.data_file_path}/regulatory_plans/', issue, '/', regulation_id_number, '.xml')", :as => :full_text, :file => true
-    indexes priority_category, :facet => true
+  #   # fields
+  #   indexes title
+  #   indexes abstract
+  #   indexes "CONCAT('#{FileSystemPathManager.data_file_path}/regulatory_plans/', issue, '/', regulation_id_number, '.xml')", :as => :full_text, :file => true
+  #   indexes priority_category, :facet => true
 
-    # attributes
-    has agency_assignments(:agency_id), :as => :agency_ids
+  #   # attributes
+  #   has agency_assignments(:agency_id), :as => :agency_ids
 
-    set_property :field_weights => {
-      "title" => 100,
-      "abstract" => 50,
-      "full_text" => 25,
-    }
+  #   set_property :field_weights => {
+  #     "title" => 100,
+  #     "abstract" => 50,
+  #     "full_text" => 25,
+  #   }
 
-    set_property :delta => ThinkingSphinx::Deltas::ManualDelta
-  end
-  # this line must appear after the define_index block
-  include ThinkingSphinx::Deltas::ManualDelta::ActiveRecord
+  #   set_property :delta => ThinkingSphinx::Deltas::ManualDelta
+  # end
+  # # this line must appear after the define_index block
+  # include ThinkingSphinx::Deltas::ManualDelta::ActiveRecord
 
   def self.current_issue
     RegulatoryPlan.first(:select => :issue, :order => "issue DESC").try(:issue)
