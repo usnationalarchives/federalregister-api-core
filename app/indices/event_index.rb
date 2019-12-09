5e2299c8fd43bcb060a42f0394d9774a408a6108
Brandon Carag <brandon@criticaljuncture.org>
2019-12-09 08:59:33 -0800

    Add most of event index attributes

---
+++ app/indices/event_index.rb
@@ -0,0 +1,24 @@
+ThinkingSphinx::Index.define :event, :with => :active_record do
+  # fields
+  indexes "entries.title", :as => :title
+  indexes entry.abstract
+  indexes place.name, :as => :place
+  indexes event_type, :as => :type, :facet => true
+#   indexes "CONCAT('#{entries.document_path}/full_text/raw/', entries.document_file_path, '.txt')", :as => :entry_full_text, :file => true
+
+  # attributes
+  has date
+  # has entry.agency_assignments(:agency_id), :as => :agency_ids
+  has place_id
+
+  set_property :field_weights => {
+    "title" => 100,
+    "place" => 50,
+    "abstract" => 50,
+    "full_text" => 25,
+  }
+  where "events.date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND DATE_ADD(NOW(), INTERVAL 2 YEAR) AND event_type != 'RegulationsDotGovCommentsCloseDate'"
+#   set_property :delta => ThinkingSphinx::Deltas::ManualDelta
+#   # this line must appear after the define_index block
+#   # include ThinkingSphinx::Deltas::ManualDelta::ActiveRecord
+end
