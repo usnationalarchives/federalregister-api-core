d92399486149ef03443fe1a656dd5046b32024c5
Brandon Carag <brandon@criticaljuncture.org>
2019-12-13 13:24:18 -0800

    Add settings/defaults for toggling no-longer-used sphinx indices

--- app/indices/event_index.rb
+++ app/indices/event_index.rb
@@ -1,24 +1,28 @@
-ThinkingSphinx::Index.define :event, :with => :active_record, :delta => ThinkingSphinx::Deltas::ManualDelta do
-  # fields
-  indexes "entries.title", :as => :title
-  indexes entry.abstract
-  indexes place.name, :as => :place
-  indexes event_type, :as => :type, :facet => true
-#   indexes "CONCAT('#{entries.document_path}/full_text/raw/', entries.document_file_path, '.txt')", :as => :entry_full_text, :file => true
+if SETTINGS["sphinx"]["enable_event_index"]
 
-  # attributes
-  has date
-  # has entry.agency_assignments(:agency_id), :as => :agency_ids
-  has place_id
+  ThinkingSphinx::Index.define :event, :with => :active_record, :delta => ThinkingSphinx::Deltas::ManualDelta do
+    # fields
+    indexes "entries.title", :as => :title
+    indexes entry.abstract
+    indexes place.name, :as => :place
+    indexes event_type, :as => :type, :facet => true
+  #   indexes "CONCAT('#{entries.document_path}/full_text/raw/', entries.document_file_path, '.txt')", :as => :entry_full_text, :file => true
 
-  set_property :field_weights => {
-    "title" => 100,
-    "place" => 50,
-    "abstract" => 50,
-    "full_text" => 25,
-  }
-  where "events.date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND DATE_ADD(NOW(), INTERVAL 2 YEAR) AND event_type != 'RegulationsDotGovCommentsCloseDate'"
+    # attributes
+    has date
+    # has entry.agency_assignments(:agency_id), :as => :agency_ids
+    has place_id
 
-  # this line must appear after the define_index block
-  # include ThinkingSphinx::Deltas::ManualDelta::ActiveRecord
+    set_property :field_weights => {
+      "title" => 100,
+      "place" => 50,
+      "abstract" => 50,
+      "full_text" => 25,
+    }
+    where "events.date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND DATE_ADD(NOW(), INTERVAL 2 YEAR) AND event_type != 'RegulationsDotGovCommentsCloseDate'"
+
+    # this line must appear after the define_index block
+    # include ThinkingSphinx::Deltas::ManualDelta::ActiveRecord
+  end
+
 end
--- app/indices/regulatory_plan_index.rb
+++ app/indices/regulatory_plan_index.rb
@@ -1,22 +1,26 @@
-ThinkingSphinx::Index.define :regulatory_plan, :with => :active_record, :delta => ThinkingSphinx::Deltas::ManualDelta do
-  # Will require a index rebuild when new regulatory plan issue comes in...
-  where "regulatory_plans.issue = '#{RegulatoryPlan.current_issue}'"
+if SETTINGS["sphinx"]["enable_regulatory_plan_index"]
 
-  # fields
-  indexes title
-  indexes abstract
-  indexes "CONCAT('#{FileSystemPathManager.data_file_path}/regulatory_plans/', issue, '/', regulation_id_number, '.xml')", :as => :full_text, :file => true
-  indexes priority_category, :facet => true
+  ThinkingSphinx::Index.define :regulatory_plan, :with => :active_record, :delta => ThinkingSphinx::Deltas::ManualDelta do
+    # Will require a index rebuild when new regulatory plan issue comes in...
+    where "regulatory_plans.issue = '#{RegulatoryPlan.current_issue}'"
 
-  # attributes
-  has agency_assignments(:agency_id), :as => :agency_ids
+    # fields
+    indexes title
+    indexes abstract
+    indexes "CONCAT('#{FileSystemPathManager.data_file_path}/regulatory_plans/', issue, '/', regulation_id_number, '.xml')", :as => :full_text, :file => true
+    indexes priority_category, :facet => true
 
-  set_property :field_weights => {
-    "title" => 100,
-    "abstract" => 50,
-    "full_text" => 25,
-  }
+    # attributes
+    has agency_assignments(:agency_id), :as => :agency_ids
 
-  # this line must appear after the define_index block
-  # include ThinkingSphinx::Deltas::ManualDelta::ActiveRecord
+    set_property :field_weights => {
+      "title" => 100,
+      "abstract" => 50,
+      "full_text" => 25,
+    }
+
+    # this line must appear after the define_index block
+    # include ThinkingSphinx::Deltas::ManualDelta::ActiveRecord
+  end
+
 end
