60c52d3dbd66426aa84ad1071aad24aeefb0541f
Brandon Carag <brandon@criticaljuncture.org>
2020-01-10 11:10:53 -0800

    Move filtered parameter logging to an initializer per new convention

--- config/application.rb
+++ config/application.rb
@@ -77,9 +77,6 @@ module FederalregisterApiCore
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
-
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
@@ -95,9 +92,6 @@ module FederalregisterApiCore
     config.assets.version = '1.0'
 
     config.action_dispatch.ip_spoofing_check = false
-
-    config.filter_parameters << :password
-    config.filter_parameters << :password_confirmation
 
     config.active_support.deprecation = :log
   end
