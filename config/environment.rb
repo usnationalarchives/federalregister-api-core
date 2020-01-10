3f4928315b9fd2fbb43743a69e3605b40c936b52
Brandon Carag <brandon@criticaljuncture.org>
2020-01-10 10:25:44 -0800

    Simplify/modernize environment.rb per rails all:update

--- config/environment.rb
+++ config/environment.rb
@@ -7,42 +7,14 @@ if (File.basename($0) == 'rake' && (ARGV.include?('db:migrate')|| ARGV.include?(
   ENV["ASSUME_UNITIALIZED_DB"] = '1'
 end
 
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '3.1.12' unless defined? RAILS_GEM_VERSION
-
-# # Bootstrap the Rails environment, frameworks, and default configuration
+# Bootstrap the Rails environment, frameworks, and default configuration
 # require File.join(File.dirname(__FILE__), 'boot')
 
+# ensure /usr/local/bin is in our path
+ENV["PATH"]="#{ENV["PATH"]}:/usr/local/bin"
 
-# # require our patched logger
-# require File.join(RAILS_ROOT, "lib", "binary_buffered_logger")
+# Load the Rails application.
+require_relative 'application'
 
-# patch for Rails 2.x with Ruby 2+
-# TODO: BB remove after upgrade
-# if Rails::VERSION::MAJOR == 2 && RUBY_VERSION >= '2.0.0'
-#   module Gem
-#     def self.source_index
-#       sources
-#     end
-#     def self.cache
-#       sources
-#     end
-#     SourceIndex = Specification
-#     class SourceList
-#       # If you want vendor gems, this is where to start writing code.
-#       def search(*args); []; end
-#       def each(&block); end
-#       include Enumerable
-#     end
-#   end
-# end
-
-# # ensure /usr/local/bin is in our path
-# ENV["PATH"]="#{ENV["PATH"]}:/usr/local/bin"
-
-# Load the rails application
-require File.expand_path('../application', __FILE__)
-
-# Initialize the rails application
+# Initialize the Rails application.
 Rails.application.initialize!
-
