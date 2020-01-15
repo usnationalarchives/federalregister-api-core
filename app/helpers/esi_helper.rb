88f9ea8089a527bd6bd761eecbbeb774881942eb
Brandon Carag <brandon@criticaljuncture.org>
2020-01-15 15:29:45 -0800

    Fix ESI rendering since we were previously relying on the FragmentFu plugin

---
+++ app/helpers/esi_helper.rb
@@ -0,0 +1,10 @@
+module EsiHelper
+
+  def render_esi(options)
+    #NOTE: This method soureed from FragmentFu::RenderWithEsi#render_esi
+    url = options[:esi]
+    query = (url.is_a?(Hash) ? controller.url_for(url.merge({:only_path => true})).gsub(/.*\?/,'') : url)
+    %Q{<esi:include src="#{query}" max-age="#{options[:ttl].to_i || 0}"/>} 
+  end
+
+end
