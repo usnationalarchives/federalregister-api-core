75c3449044a31e2d257b67f2db33d7c412f43661
Brandon Carag <brandon@criticaljuncture.org>
2020-01-17 15:32:49 -0800

    Fix ESI helper—ensure we’re calling HTML safe on its results so varnish picks up

--- app/helpers/esi_helper.rb
+++ app/helpers/esi_helper.rb
@@ -4,7 +4,7 @@ module EsiHelper
     #NOTE: This method soureed from FragmentFu::RenderWithEsi#render_esi
     url = options[:esi]
     query = (url.is_a?(Hash) ? controller.url_for(url.merge({:only_path => true})).gsub(/.*\?/,'') : url)
-    %Q{<esi:include src="#{query}" max-age="#{options[:ttl].to_i || 0}"/>} 
+    %Q{<esi:include src="#{query}" max-age="#{options[:ttl].to_i || 0}"/>}.html_safe
   end
 
 end
