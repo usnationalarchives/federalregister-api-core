907c43e2b590436636a29c1af18a4042f54bfd00
Brandon Carag <brandon@criticaljuncture.org>
2020-01-21 11:11:08 -0800

    Add jQuery library that calls #sortable for section highlights area

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require admin/jquery-1.8.3.min.js
 //= require jquery_ujs
+//= require jquery-ui-1.9.2.custom.min.js
 //= require jquery-ui-1.9.2.effects.min.js
 //= require admin/handlebars-1.0.rc.1.min.js
 
@@ -29,3 +30,4 @@
 //= require admin/fr_index
 //= require admin/issue_reprocessor
 //= require admin/presidential_document_importer
+
