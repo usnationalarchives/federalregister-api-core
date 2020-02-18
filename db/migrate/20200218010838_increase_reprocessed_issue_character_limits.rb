be6181dd691489643a7ad1f9d6079413a2eb9e1f
Peregrinator <bob.burbach@gmail.com>
2020-02-18 10:53:14 -0800

    Update: remove index add for now

--- db/migrate/20200218010838_increase_reprocessed_issue_character_limits.rb
+++ db/migrate/20200218010838_increase_reprocessed_issue_character_limits.rb
@@ -2,7 +2,5 @@ class IncreaseReprocessedIssueCharacterLimits < ActiveRecord::Migration[6.0]
   def change
     change_column :reprocessed_issues, :status, :string, limit: 1000
     change_column :reprocessed_issues, :message, :string, limit: 1000
-
-    add_index :reprocessed_issues, [:issue_id, :status], length: {issue_id: 5, status: 100}
   end
 end
