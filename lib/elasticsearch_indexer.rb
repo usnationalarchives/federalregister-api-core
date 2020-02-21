b9ef274c24366f49e504420183ef2dfc5666fa98
Brandon Carag <brandon@criticaljuncture.org>
2020-02-21 14:21:27 -0800

    Add spec/handling for elasticsearch indexer and delta handling

--- lib/elasticsearch_indexer.rb
+++ lib/elasticsearch_indexer.rb
@@ -9,4 +9,28 @@ module ElasticsearchIndexer
     EntryChange.insert_all(entry_change_collection)
   end
 
+  def self.handle_entry_changes
+    remove_deleted_entries
+    reindex_modified_entries
+  end
+
+  def self.remove_deleted_entries
+    deleted_entry_ids.each do |entry_id|
+      $entry_repository.delete(entry_id)
+    end
+  end
+
+  def self.reindex_modified_entries
+    Entry.where(id: EntryChange.where.not(entry_id: deleted_entry_ids).pluck(:entry_id)).each do |entry|
+      $entry_repository.save(entry)
+    end
+  end
+
+  def self.deleted_entry_ids
+    EntryChange.
+      joins("LEFT JOIN entries on entry_changes.entry_id = entries.id").
+      where("entries.id IS NULL").
+      pluck(:entry_id)
+  end
+
 end
---
+++ spec/lib/elasticsearch_indexer_spec.rb
@@ -0,0 +1,34 @@
+require "spec_helper"
+
+describe ElasticsearchIndexer do
+
+  before(:each) do
+    $entry_repository.create_index!(force: true)
+  end
+
+  it "removes deleted entries from elasticsearch" do
+    entry = Factory(:entry)
+    $entry_repository.save(entry)
+    $entry_repository.refresh_index!
+
+    expect($entry_repository.find([entry.id]).first).not_to be_nil
+
+    entry.destroy!
+    ElasticsearchIndexer.remove_deleted_entries
+    expect($entry_repository.find([entry.id])).to eq [nil]
+  end
+
+  it "reindexes modified entries from elasticsearch" do
+    entry = Factory(:entry, significant: 0)
+    $entry_repository.save(entry)
+    $entry_repository.refresh_index!
+    binding.pry
+    expect($entry_repository.find([entry.id]).first.fetch('significant')).to eq(false)
+
+    entry.update!(significant: 1)
+    ElasticsearchIndexer.reindex_modified_entries
+
+    expect($entry_repository.find([entry.id]).first.fetch('significant')).to eq(true)
+  end
+
+end
