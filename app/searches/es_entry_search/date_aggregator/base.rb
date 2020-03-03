31210cfc8d820e49113dbdf9551bbd86e46b0b81
Brandon Carag <brandon@criticaljuncture.org>
2020-03-03 12:43:44 -0800

    Reformat: Clarify naming conventions in ES DateAggregator

--- app/searches/es_entry_search/date_aggregator/base.rb
+++ app/searches/es_entry_search/date_aggregator/base.rb
@@ -1,8 +1,8 @@
 class EsEntrySearch::DateAggregator::Base
   extend Memoist
 
-  def initialize(sphinx_search, options)
-    @sphinx_search = sphinx_search
+  def initialize(es_search, options)
+    @es_search = es_search
     if options[:with] && options[:with][:publication_date]
       @start_date = Time.at(options[:with][:publication_date].first).utc.to_date
 
@@ -28,11 +28,9 @@ class EsEntrySearch::DateAggregator::Base
     end
   end
 
-  # The desired results
-  # return {"1994"=>33337, "1995"=>31707, "1996"=>33132, "1997"=>34218, "1998"=>34743, "1999"=>34066, "2008"=>31812, "2009"=>30662, "2001"=>31984, "2000"=>33302, "2005"=>32226, "2004"=>32367, "2002"=>33075, "2003"=>32792, "2006"=>31501, "2007"=>30830, "2010"=>32465, "2011"=>33103, "2012"=>30875, "2013"=>30771}
   def raw_results
     group_and_counts = {}
-    sphinx_search.aggregation_buckets.map do |term|
+    es_search.aggregation_buckets.map do |term|
       datetime = Date.parse(term['key_as_string']) #TODO: Do we need to handle UTC processing parallel to what's happening in the Sphinx-based base.rb class?
 
       group_and_counts[sphinx_format(datetime)] = term.doc_count
@@ -44,7 +42,7 @@ class EsEntrySearch::DateAggregator::Base
 
   private
 
-  attr_reader :sphinx_search
+  attr_reader :es_search
 
   def sphinx_format(date)
     date.strftime(sphinx_format_str)
