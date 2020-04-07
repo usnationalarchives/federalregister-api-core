72b35f22f225707e57ec96fad621019292dc5986
Brandon Carag <brandon@criticaljuncture.org>
2020-04-06 19:03:44 -0700

    Add: Port pre-existing search infrastructure specs to ES search class.

--- spec/searches/es_entry_search_spec.rb
+++ spec/searches/es_entry_search_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe "Elasticsearch Entry Search" do
+describe EsEntrySearch do
   before(:each) { allow(ElasticsearchIndexer).to receive(:es_enabled?).and_return(true) }
 
   def build_entry_double(hsh)
@@ -20,6 +20,241 @@ describe "Elasticsearch Entry Search" do
       publication_date: Date.new(2020,1,1),
       significant: 1,
     )
+  end
+
+  context "Pre-ES specs" do
+    #NOTE: These specs were used for testing search before the ES upgrade.
+
+    describe 'agency_ids' do
+      it "populates sphinx `with`" do
+        agencies = (1..2).map{ Factory.create(:agency) }
+        search = described_class.new()
+        search.agency_ids = agencies.map(&:id)
+        search.with.should == {:agency_ids => agencies.map(&:id)}
+      end
+    end
+
+    describe 'significant' do
+      it "populates sphinx `with`" do
+        search = described_class.new()
+        search.significant = 1
+        search.with.should == {:significant => true}
+      end
+    end
+
+    describe 'type' do
+      it "populates sphinx `with`, CRC32 escaping" do
+        search = described_class.new
+        search.type = ['RULE', 'PRORULE']
+        search.with.should == {:type => ['RULE', 'PRORULE']}
+      end
+
+      it "collapses sunlight and notices"
+    end
+
+    describe 'presidential_document_type' do
+      it "populates sphinx `with`" do
+        search = described_class.new
+        search.presidential_document_type = ["determination", "executive_order"]
+        search.with.should == {:presidential_document_type_id => [1,2]}
+      end
+    end
+
+    describe 'cfr' do
+      it "populates sphinx `with` using the custom citation format" do
+        pending("May not be needed")
+        search = described_class.new
+        search.cfr = {:title => '10', :part => '101'}
+        search.with.should == {:cfr_affected_parts => 1000101}
+      end
+    end
+
+    describe 'regulation_id_number' do
+      it "populates sphinx `conditions` and properly quotes" do
+        pending("May not be needed")
+        search = described_class.new()
+        search.regulation_id_number = "ABCD-1234"
+        search.sphinx_conditions.should == {:regulation_id_number => '"=ABCD =1234"'}
+      end
+    end
+
+    describe 'matching_entry_citation' do
+      before(:each) do
+        allow(Issue).to receive(:current).and_return(Issue.new(:publication_date => Date.today))
+      end
+
+      it "finds no match when no term" do
+        described_class.new().matching_entry_citation.should be_nil
+      end
+
+      it "finds no match for terms that aren't FR citations" do
+        described_class.new(:conditions => {:term => "ABCD"}).matching_entry_citation.should be_nil
+        described_class.new(:conditions => {:term => "10 CFR 120"}).matching_entry_citation.should be_nil
+      end
+
+      it "finds no match for terms that contain more than an FR citation" do
+        described_class.new(:conditions => {:term => "before 71 FR 12345"}).matching_entry_citation.should be_nil
+        described_class.new(:conditions => {:term => "71 FR 12345 after"}).matching_entry_citation.should be_nil
+        described_class.new(:conditions => {:term => "before 71 FR 12345 after"}).matching_entry_citation.should be_nil
+      end
+
+      it "finds a match for valid 'OFR-style' FR citations" do
+        citation_attributes = Citation.new(:citation_type => "FR", :part_1 => 71, :part_2 => 12345).attributes
+        described_class.new(:conditions => {:term => "71 FR 12345"}).matching_entry_citation.attributes.should == citation_attributes
+        described_class.new(:conditions => {:term => "71FR12345"}).matching_entry_citation.attributes.should == citation_attributes
+        described_class.new(:conditions => {:term => " 71 FR 12345 "}).matching_entry_citation.attributes.should == citation_attributes
+        described_class.new(:conditions => {:term => "71 F.R. 12345"}).matching_entry_citation.attributes.should == citation_attributes
+      end
+
+      it "find a match for valid 'Harvard-style' FR citations" do
+        citation_attributes = Citation.new(:citation_type => "FR", :part_1 => 71, :part_2 => 12345).attributes
+        described_class.new(:conditions => {:term => "71 Fed Reg 12345"}).matching_entry_citation.attributes.should == citation_attributes
+        described_class.new(:conditions => {:term => "71 Fed. Reg. 12345"}).matching_entry_citation.attributes.should == citation_attributes
+        described_class.new(:conditions => {:term => "71 Fed. Reg. 12,345"}).matching_entry_citation.attributes.should == citation_attributes
+        described_class.new(:conditions => {:term => " 71 fed reg 12345 "}).matching_entry_citation.attributes.should == citation_attributes
+        described_class.new(:conditions => {:term => "71 fedreg 12345"}).matching_entry_citation.attributes.should == citation_attributes
+        described_class.new(:conditions => {:term => "71fedreg12345"}).matching_entry_citation.attributes.should == citation_attributes
+      end
+    end
+
+    describe 'publication_date' do
+      [:is, :gte, :lte].each do |type|
+        describe "`#{type}`" do
+          describe 'error handling' do
+            before(:each) do
+              @date_string = "NOT A VALID DATE"
+              @search = described_class.new(:conditions => {:publication_date => {type => @date_string}})
+            end
+
+            it "adds an error when given a bad `#{type}` date" do
+              @search.validation_errors[:publication_date].should be_present
+            end
+
+            it "populates the value when given a bad `#{type}` date" do
+              @search.publication_date.send(type).should == @date_string
+            end
+          end
+        end
+      end
+    end
+
+    describe 'entry_with_document_number' do
+      before(:each) do
+        allow(Issue).to receive(:current).and_return(Issue.new(:publication_date => Date.today))
+      end
+
+      it "finds no match when no term" do
+        described_class.new().entry_with_document_number.should be_nil
+      end
+
+      it "finds no match for terms that aren't FR citations" do
+        described_class.new(:conditions => {:term => "ABCD"}).entry_with_document_number.should be_nil
+      end
+
+      it "finds a match for valid FR document numbers" do
+        entry = Entry.create!(:document_number => "2010-1")
+        described_class.new(:conditions => {:term => "2010-1"}).entry_with_document_number.should == entry
+      end
+    end
+
+    describe 'results_for_date' do
+      it "retains the same filters/conditions, but forces a particular publication_date" do
+        pending("Readdress when re-recording TS v3 VCR specs")
+        date = Date.parse("2010-10-10")
+        search = described_class.new(:conditions => {:term => "HOWDY", :significant => '1', :cfr =>{:title => '7', :part => '132'}})
+
+        Entry.should_receive(:search).with(
+          'HOWDY',
+          hash_including(
+            with: hash_including(
+              significant: '1',
+              publication_date: (date.to_time.utc.beginning_of_day.to_i .. date.to_time.utc.end_of_day.to_i)
+            ),
+            per_page: 1000
+          )
+        )
+        search.results_for_date(date)
+      end
+    end
+
+    describe "summary" do
+      it "says 'All Documents' if no term or filters" do
+        described_class.new(:conditions => {}).summary.should == "All Documents"
+      end
+
+      it "includes the term" do
+        described_class.new(:conditions => {:term => "OH HAI"}).summary.should == "Documents matching 'OH HAI'"
+      end
+
+      it "includes the effective date" do
+        search = described_class.new(:conditions => {:effective_date => {:year => 2011}})
+
+        search.summary.should == "Documents with an effective date in 2011"
+      end
+
+      it "includes the single agency" do
+        agency = Factory(:agency, :name => "Commerce Department")
+        search = described_class.new(:conditions => {:agency_ids => [agency.id]})
+
+        search.summary.should == "Documents from Commerce Department"
+      end
+
+      it "includes all agencies" do
+        agency_1 = Factory(:agency, :name => "Commerce Department")
+        agency_2 = Factory(:agency, :name => "State Department")
+        search = described_class.new(:conditions => {:agency_ids => [agency_1.id,agency_2.id]})
+
+        search.summary.should == "Documents from Commerce Department or State Department"
+      end
+
+      it "includes the document type" do
+        search = described_class.new(:conditions => {:type => ['RULE','PRORULE']})
+        search.summary.should == "Documents of type Rule or Proposed Rule"
+      end
+
+      it "includes the agency docket" do
+        search = described_class.new(:conditions => {:docket_id => 'EPA-HQ-OPPT-2005-0049'})
+        search.summary.should == "Documents filed under agency docket EPA-HQ-OPPT-2005-0049"
+      end
+
+      it "includes the significance" do
+        search = described_class.new(:conditions => {:significant => '1'})
+        search.summary.should == "Documents whose Associated Unified Agenda Deemed Significant Under EO 12866"
+      end
+
+      it "includes the affected CFR part" do
+        search = described_class.new(:conditions => {:cfr => {:title => '40', :part => '745'}})
+        search.summary.should == "Documents affecting 40 CFR 745"
+      end
+
+      #TODO: uncomment when merging new geolocation code
+      it "includes the location"# do
+      #   search = described_class.new(:conditions => {:near => {:location => " 94118", :within => 50}})
+      #   search.summary.should == "Documents located within 50 miles of  94118"
+      # end
+
+      it "includes the section" do
+        section = Factory(:section, :title => "Environment")
+        search = described_class.new(:conditions => {:section_ids => [section.id]})
+        search.summary.should == "Documents in Environment"
+      end
+
+      it "includes the topic" do
+        topic = Factory(:topic, :name => "Reporting and recordkeeping requirements")
+        search = described_class.new(:conditions => {:topic_ids => [topic.id]})
+        search.summary.should == "Documents about Reporting and recordkeeping requirements"
+      end
+
+      it "combines multiple types of filters with the appropriate conjunction" do
+        search = described_class.new(:conditions => {
+              :term => "fishing",
+              :type => ['RULE','PRORULE'],
+              :cfr => {:title => '45', :part => '745'}
+        })
+        search.summary.should == "Documents matching 'fishing', of type Rule or Proposed Rule, and affecting 45 CFR 745"
+      end
+    end
+
   end
 
   context "Elasticsearch query definition" do
