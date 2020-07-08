a595dd5998190a65ec5d6f70e148f270f14458a6
Brandon Carag <brandon@criticaljuncture.org>
2020-07-08 13:17:26 -0700

    Update: Include shell of application search class since its used for namespacing extensively.

---
+++ app/searches/application_search.rb
@@ -0,0 +1,4 @@
+#This used to have a full Sphinx-based search implementation--the full implementation was removed, but we're retaining a class here since it's used for namespacing in other search classes
+
+class ApplicationSearch
+end
