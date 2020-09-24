4ad425f3a12a763678dd40807c7cc801d0777736
Brandon Carag <brandon@criticaljuncture.org>
2020-09-23 22:06:15 -0700

    Update: Add feature flag for public inspection page views.

--- app/presenters/public_inspection_document_api_representation.rb
+++ app/presenters/public_inspection_document_api_representation.rb
@@ -31,11 +31,13 @@ class PublicInspectionDocumentApiRepresentation < ApiRepresentation
     public_inspection_raw_text_url(document)
   }
   field(:num_pages)
-  field(:page_views, :select => [:document_number]) do |entry|
-    {
-      count:         PageViewCount.count_for(entry.document_number, PageViewType::PUBLIC_INSPECTION_DOCUMENT),
-      last_updated:  PageViewCount.last_updated(PageViewType::PUBLIC_INSPECTION_DOCUMENT),
-    }
+  if SETTINGS['public_inspection_page_views']
+    field(:page_views, :select => [:document_number]) do |entry|
+      {
+        count:         PageViewCount.count_for(entry.document_number, PageViewType::PUBLIC_INSPECTION_DOCUMENT),
+        last_updated:  PageViewCount.last_updated(PageViewType::PUBLIC_INSPECTION_DOCUMENT),
+      }
+    end
   end
   field(:publication_date)
   field(:type, :select => :granule_class) {|document| document.entry_type}
