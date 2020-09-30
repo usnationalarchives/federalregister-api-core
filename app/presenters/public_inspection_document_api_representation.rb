9f65be7cb794c98c999bb336c1fb1c5bb5941d1e
Brandon Carag <brandon@criticaljuncture.org>
2020-09-30 10:21:10 -0700

    Update: Add spec/date configurability around PI page view results in API

--- app/presenters/public_inspection_document_api_representation.rb
+++ app/presenters/public_inspection_document_api_representation.rb
@@ -31,10 +31,12 @@ class PublicInspectionDocumentApiRepresentation < ApiRepresentation
     public_inspection_raw_text_url(document)
   }
   field(:num_pages)
-  if SETTINGS['public_inspection_page_views']
-    field(:page_views, :select => [:document_number]) do |entry|
+  field(:page_views, :select => [:document_number, :filed_at]) do |document|
+    start_date = SETTINGS['public_inspection_document_page_view_start_date']
+
+    if document.filed_at && start_date && (document.filed_at.to_date >= start_date)
       {
-        count:         PageViewCount.count_for(entry.document_number, PageViewType::PUBLIC_INSPECTION_DOCUMENT),
+        count:         PageViewCount.count_for(document.document_number, PageViewType::PUBLIC_INSPECTION_DOCUMENT),
         last_updated:  PageViewCount.last_updated(PageViewType::PUBLIC_INSPECTION_DOCUMENT),
       }
     end
--- spec/presenters/public_inspection_document_api_representation_spec.rb
+++ spec/presenters/public_inspection_document_api_representation_spec.rb
@@ -7,4 +7,23 @@ describe PublicInspectionDocumentApiRepresentation do
     result = representation.value(:html_url)
     expect(result).to eq("http://www.fr2.local:8081/public_inspection_documents/2020/09/22/#{public_inspection_document.document_number}/test")
   end
+
+  context "#page_views" do
+    let(:public_inspection_document) { Factory(:public_inspection_document, filed_at: Date.new(2020,9,22)) }
+
+    it "returns page views " do
+      allow(SETTINGS).to receive(:[]).with("public_inspection_document_page_view_start_date").and_return(Date.parse("2020-09-21"))
+      representation = PublicInspectionDocumentApiRepresentation.new(public_inspection_document)
+
+      result = representation.value(:page_views)
+      expect(result).to eq({count: 0, last_updated: nil})
+    end
+
+    it "returns nil when a public_inspection_document_page_view_start_date isn't set" do
+      representation = PublicInspectionDocumentApiRepresentation.new(public_inspection_document)
+
+      result = representation.value(:page_views)
+      expect(result).to eq(nil)
+    end
+  end
 end
