e4a91ee4845646a7826484c5aa5c34de086f3cdc
Brandon Carag <brandon@criticaljuncture.org>
2020-10-05 17:40:24 -0700

    Update: Add specs/modify Google Analytics page queries for new PI urls.

--- spec/lib/page_view_count_spec.rb
+++ spec/lib/page_view_count_spec.rb
@@ -6,10 +6,31 @@ describe PageViewCount do
   end
 
   it "the regex for extracting document numbers is correct" do
-    sample_url = "/public_inspection_documents/2012/03/30/2012-05896/disparate-impact-and-reasonable-factors-other-than-age-under-the-age-discrimination-in-employment"
+    valid_urls = [
+      "/public_inspection_documents/2012/03/30/2012-05896/disparate-impact-and-reasonable-factors-other-than-age-under-the-age-discrimination-in-employment",
+      "/public-inspection/2012-05896/disparate-impact-and-reasonable-factors-other-than-age-under-the-age-discrimination-in-employment",
+    ]
     regex = PageViewType.find_by_identifier('public_inspection_document').google_analytics_url_regex
-    result = sample_url =~ regex
-    expect(result).to eq(0)
+
+    valid_urls.each do |url|
+      result = url =~ regex
+      expect(result).to eq(0)
+    end
+
+
+  end
+
+  it "the regex doesn't mis-identify documents" do
+    invalid_urls = [
+      "/public-inspection/search",
+      "/public-inspection/20-05896/disparate-impact-and-reasonable-factors-other-than-age-under-the-age-discrimination-in-employment",
+    ]
+    regex = PageViewType.find_by_identifier('public_inspection_document').google_analytics_url_regex
+
+    invalid_urls.each do |url|
+      result = url =~ regex
+      expect(result).to eq(nil)
+    end
   end
 
 end
