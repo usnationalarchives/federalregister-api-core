0afd0b7cb1e1c6e53898914de49cb4e3676a18cf
Brandon Carag <brandon@criticaljuncture.org>
2020-10-23 12:10:33 -0700

    Update: Upgrade VCR from 2.6 => 6.0

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -4,7 +4,9 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr_cassettes'
   c.hook_into :webmock
   c.filter_sensitive_data('<API_KEY>') { Rails.application.secrets[:api_keys][:google_maps_geocode] }
+  c.filter_sensitive_data('<API_KEY>') { Rails.application.secrets[:regulations_dot_gov][:v4_api_key] }
 
   # comment below to re-record
   c.default_cassette_options = {:record => :new_episodes}
+  c.allow_http_connections_when_no_cassette = true
 end
