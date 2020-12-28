5343c495b10a11618db438e96473d4f91e989237
Brandon Carag <brandon@criticaljuncture.org>
2020-12-28 15:52:54 -0800

    Update: Handle ECFR.gov-sourced images via the existing processor.
    Add configurable image magick convert options to image processing pipeline.  Images sourced from ECFR.gov come out looking very grainy/poor quality without the specification of a 300 density.

--- lib/paperclip/gpo_image_converter.rb
+++ lib/paperclip/gpo_image_converter.rb
@@ -1,4 +1,5 @@
 class Paperclip::GpoImageConverter < Paperclip::Thumbnail
+  extend Memoist
   # GPO images appear to come in at least 2 densities - 300 and 660
   def source_file_options
     options = @source_file_options
@@ -10,6 +11,11 @@ class Paperclip::GpoImageConverter < Paperclip::Thumbnail
   private
 
   def density
-    @density ||= Paperclip.run("identify -format '%x' #{File.expand_path(@file.path)}")
+    if attachment.instance.sourced_via_ecfr_dot_gov && (options.fetch(:style) == :original_png)
+      300
+    else
+      Paperclip.run("identify -format '%x' #{File.expand_path(@file.path)}")
+    end
   end
+  memoize :density
 end
