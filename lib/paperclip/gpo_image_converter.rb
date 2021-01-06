92bdd6046cdfd31743720d69e230dd8efb9a68f7
Brandon Carag <brandon@criticaljuncture.org>
2021-01-06 14:36:19 -0800

    Update: Perform scaling based on linear upscaling threshold.
    We want to upscale images at a linear rate up to a certain threshold, after which we will statically scale images based on a desired max width in pixels.

--- lib/paperclip/gpo_image_converter.rb
+++ lib/paperclip/gpo_image_converter.rb
@@ -20,10 +20,21 @@ class Paperclip::GpoImageConverter < Paperclip::Thumbnail
   end
   memoize :density
 
+  MAX_DESIRED_WIDTH_IN_PIXELS          = 756
+  LINEAR_UPSCALING_THRESHOLD_IN_PIXELS = 351 #NOTE: This represents the threshold at which we will no longer upscale the image linearly.
   def resize_options
     if sourced_via_ecfr_dot_gov_options?
-      dimensions = Paperclip.run("identify -format '%wx%h' #{File.expand_path(@file.path)}")
-      " -resize #{dimensions}"
+      dimensions    = Paperclip.run("identify -format '%wx%h' #{File.expand_path(@file.path)}")
+      width, height = dimensions.split('x').map(&:to_i)
+
+      if width > LINEAR_UPSCALING_THRESHOLD_IN_PIXELS
+        scaled_dimensions = "#{MAX_DESIRED_WIDTH_IN_PIXELS}x#{(MAX_DESIRED_WIDTH_IN_PIXELS.to_f/width) * height}"
+      else
+        scaling_multiplier = MAX_DESIRED_WIDTH_IN_PIXELS.to_f / LINEAR_UPSCALING_THRESHOLD_IN_PIXELS
+        scaled_dimensions = "#{width * scaling_multiplier}x#{height * scaling_multiplier}"
+      end
+
+      " -resize #{scaled_dimensions}"
     end
   end
 
