7b25f2e0edc3ab5aca31394b74cbd651cc101662
bdnelson123 <ben.david.nelson@gmail.com>
2021-02-04 19:01:46 +0000

    #180: move include CacheUtils out of the perform method of IssueTocRegenerator

--- app/workers/issue_toc_regenerator.rb
+++ app/workers/issue_toc_regenerator.rb
@@ -1,16 +1,18 @@
 class IssueTocRegenerator
   include Sidekiq::Worker
   include Sidekiq::Throttled::Worker
+  include CacheUtils
 
   sidekiq_options :queue => :issue_reprocessor, :retry => 0
 
   def perform(date)
     ActiveRecord::Base.clear_active_connections!
 
+    date = date.is_a?(Date) ? date : Date.parse(date)
     XmlTableOfContentsTransformer.perform(date)
 
-    include CacheUtils
     purge_cache("/documents/#{date.strftime("%Y/%m/%d")}")
     purge_cache("/documents/#{Time.current.to_date.strftime('%Y')}/#{Time.current.to_date.strftime('%m')}")
+    purge_cache("/documents/json/#{date.strftime("%Y/%m/%d")}.json")
   end
 end
