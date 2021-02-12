d6b3c84618b72f00b21aa65967a70129ee43087e
Brandon Carag <brandon@criticaljuncture.org>
2021-02-11 17:36:39 -0800

    Update: Refine scaling calculations to take into account various densities.
    Images appear to come in densities of 72, 300, or 600, but it appears we should always be dividing the supplied pixel width by 72 to calculate the actual page width in inches e.g. images er16no17.186 (density of 600) and ep30no18.005 (density of 300)
    
    See #322.

--- lib/paperclip/gpo_image_converter.rb
+++ lib/paperclip/gpo_image_converter.rb
@@ -25,16 +25,20 @@ class Paperclip::GpoImageConverter < Paperclip::Thumbnail
   end
   memoize :density
 
+  ROUNDING_PRECISION = 0
+  PRINT_PAGE_DPI     = 72
   def resize_options
     if (paperclip_style == :medium) || (paperclip_style == :large)
-      dimensions    = Paperclip.run("identify -format '%wx%h' #{File.expand_path(@file.path)}")
-      width, height = dimensions.split('x').map(&:to_i)
-
-      if width > full_page_pixel_width_in_print
-        scaled_dimensions = "#{max_desired_pixel_width}x#{((max_desired_pixel_width.to_f/width) * height).round(0)}"
+      dimensions                = Paperclip.run("identify -format '%wx%h' #{File.expand_path(@file.path)}")
+      pixel_width, pixel_height = dimensions.split('x').map(&:to_i)
+      width_in_inches           = (pixel_width.to_f/PRINT_PAGE_DPI) #NOTE: It appears that even if an image has a density that's not 72, we should use 72 as the factor for calculating the width of the image in inches e.g. images er16no17.186 (density of 600) and ep30no18.005 (density of 300)
+      if width_in_inches > full_page_inches_width_in_print
+        scaled_height     =  ((max_desired_pixel_width.to_f/pixel_width) * pixel_height).round(ROUNDING_PRECISION)
+        scaled_dimensions = "#{max_desired_pixel_width}x#{scaled_height}"
       else
-        scaling_multiplier = max_desired_pixel_width.to_f / full_page_pixel_width_in_print
-        scaled_dimensions = "#{(width * scaling_multiplier).round(0)}x#{(height * scaling_multiplier).round(0)}"
+        scaled_width      = (width_in_inches/full_page_inches_width_in_print * max_desired_pixel_width).round(ROUNDING_PRECISION)
+        scaled_height     = (scaled_width.to_f/pixel_width * pixel_height).round(ROUNDING_PRECISION)
+        scaled_dimensions = "#{scaled_width}x#{scaled_height}"
       end
 
       "-resize #{scaled_dimensions}"
@@ -56,6 +60,10 @@ class Paperclip::GpoImageConverter < Paperclip::Thumbnail
     else
       raise NotImplementedError
     end
+  end
+
+  def full_page_inches_width_in_print
+    full_page_pixel_width_in_print.to_f / 72
   end
 
   def full_page_pixel_width_in_print
