f6aaa271a93e49df08f37377ed89019a2228ff8c
Brandon Carag <brandon@criticaljuncture.org>
2021-03-18 16:53:51 -0700

    Update: Add additional guard clause to ensure sft-sourced EPS images not overwritten.

--- lib/gpo_images/ecfr_dot_gov_converter.rb
+++ lib/gpo_images/ecfr_dot_gov_converter.rb
@@ -10,22 +10,27 @@ module GpoImages
     def perform(image_identifier)
       #TODO: Handle deletion of existing sourced_via_ecfr_dot_gov images.
       @image_identifier = normalize_image_identifier(image_identifier)
+
       gpo_graphic       = GpoGraphic.find_or_initialize_by(
         identifier:               image_identifier,
-        sourced_via_ecfr_dot_gov: true,
       )
 
-      temp_file = Tempfile.new([image_identifier, '.pdf'])
-      temp_file.binmode
-      open(ecfr_pdf_url) do |url_file|
-        temp_file.write(url_file.read)
+      if gpo_graphic && (gpo_graphic.sourced_via_ecfr_dot_gov == false)
+        return #Don't overwrite SFTP-sourced images
+      else
+        gpo_graphic.sourced_via_ecfr_dot_gov = true
+        temp_file = Tempfile.new([image_identifier, '.pdf'])
+        temp_file.binmode
+        open(ecfr_pdf_url) do |url_file|
+          temp_file.write(url_file.read)
+        end
+        temp_file.rewind
+
+        gpo_graphic.graphic = temp_file
+        gpo_graphic.save!
+
+        gpo_graphic.move_to_public_bucket
       end
-      temp_file.rewind
-
-      gpo_graphic.graphic = temp_file
-      gpo_graphic.save!
-
-      gpo_graphic.move_to_public_bucket
     end
 
     private
