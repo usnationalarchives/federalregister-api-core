009afffb886463aa8fb196b55f74d27d0d8b500d
Brandon Carag <brandon@criticaljuncture.org>
2021-03-24 15:47:01 -0700

    Update: Add toggleable paperclip patch for hard failing on S3 error.
    When we were doing bulk reprocessing, we discovered that sometimes Paperclip will silently create a blank style (thumbnail) if the original doesn’t exist.  We’re adding a hard raise here to short-circuit that process earlier if needed.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -28,3 +28,26 @@ Paperclip.interpolates :style_if_not_with_banner do |attachment, style|
 end
 
 Paperclip.options[:command_path] = "/usr/local/bin/"
+
+if SETTINGS['paperclip']['abort_on_s3_error']
+  module Paperclip
+    module Storage
+      module S3
+
+        def copy_to_local_file(style, local_dest_path)
+          log("copying #{path(style)} to local file #{local_dest_path}")
+          ::File.open(local_dest_path, 'wb') do |local_file|
+            s3_object(style).get do |chunk|
+              local_file.write(chunk)
+            end
+          end
+        rescue Aws::Errors::ServiceError => e
+          raise "Aborting Paperclip processing due to S3 error (This is a custom CJ error). #{e.inspect}"
+          warn("#{e} - cannot copy #{path(style)} to local file #{local_dest_path}")
+          false
+        end
+
+      end
+    end
+  end
+end
