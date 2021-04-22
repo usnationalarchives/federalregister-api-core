853505cad0f28a6af889c254990256c7820f783c
Brandon Carag <brandon@criticaljuncture.org>
2021-04-22 16:58:50 -0700

    Add: Basic wrapper around Prince calls

---
+++ lib/prince_xml_service.rb
@@ -0,0 +1,17 @@
+class PrinceXmlService
+  class PrinceXmlServiceFailure < StandardError; end
+
+  def self.html_to_pdf(string, output_path)
+    response = Faraday.post(url, html: string)
+    if response.body.blank?
+      raise PrinceXmlServiceFailure
+    else
+      File.open(output_path, 'wb') { |file| file.write(response.body) }
+    end
+  end
+
+  def self.url
+    "#{SETTINGS['prince']['url']}:#{SETTINGS['prince']['port']}"
+  end
+
+end
