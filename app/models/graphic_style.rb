8aa5a37c2172e99824781d32ba94e06d06098578
Brandon Carag <brandon@criticaljuncture.org>
2021-06-28 14:39:56 -0700

    Update: Add bulk recalculation method.

--- app/models/graphic_style.rb
+++ app/models/graphic_style.rb
@@ -1,6 +1,13 @@
 class GraphicStyle < ApplicationModel
   belongs_to :styleable, polymorphic: true, foreign_type: :graphic_type, foreign_key: :graphic_id
 
+  def self.recalculate_all!
+    GraphicStyle.delete_all
+    GpoGraphic.find_each do |gpo_graphic|
+      gpo_graphic.update_graphic_styles
+    end
+  end
+
   def url
     styleable.graphic.styles.values.first.attachment.url(style_name, timestamp: false)
   end
