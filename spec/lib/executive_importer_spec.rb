4601b9d2c34dec77ad98c105b6e730371d1d197e
Brandon Carag <brandon@criticaljuncture.org>
2021-10-05 13:46:03 -0400

    Add: Additional spec attributes
    See #201

--- spec/lib/executive_importer_spec.rb
+++ spec/lib/executive_importer_spec.rb
@@ -15,13 +15,13 @@ end
 
 describe "ExecutiveOrderImporter" do
 
-  it "updates existent executive orders (normalizes CSV headers and ignores blank lines)" do
+  it "updates existent executive orders (normalizes CSV headers, strips document number/publication date, and ignores blank lines)" do
     entry = FactoryGirl.create(:entry)
     agency_name = FactoryGirl.create(:agency_name, name: 'Executive Office of the President')
 
     csv_rows = <<-eos.strip_heredoc
       citation,document_number,end_page,executive_order_notes,ExEcUtive_order_number  ,html_url,pdf_url,publication_date,signing_date,start_page,title
-      59 FR 499,#{entry.document_number},499,,12890,https://www.federalregister.gov/documents/1994/01/05/94-290/amendment-to-executive-order-no-12864,https://www.govinfo.gov/content/pkg/FR-1994-01-05/html/94-290.htm,#{Date.current.to_s(:default)},12/30/93,499,Amendment to Executive Order No. 12864
+      59 FR 499,   #{entry.document_number} ,499,,12890,https://www.federalregister.gov/documents/1994/01/05/94-290/amendment-to-executive-order-no-12864,https://www.govinfo.gov/content/pkg/FR-1994-01-05/html/94-290.htm,#{Date.current.to_s(:default)},12/30/93,499,Amendment to Executive Order No. 12864
       ,,,,,,,,,,
     eos
     csv_file = stubbed_csv(csv_rows)
