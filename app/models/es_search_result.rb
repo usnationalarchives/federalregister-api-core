f218fce35016fe7831e46e08faf1b6f6ebbe5bdd
Brandon Carag <brandon@criticaljuncture.org>
2021-10-28 08:39:26 -0700

    Add: Page view handling
    See #203.

--- app/models/es_search_result.rb
+++ app/models/es_search_result.rb
@@ -9,4 +9,12 @@ class EsSearchResult < OpenStruct
     end
   end
 
+
+  def page_views
+    {
+      count:        PageViewCount.count_for(document_number, PageViewType::DOCUMENT),
+      last_updated: PageViewCount.last_updated(PageViewType::DOCUMENT)
+    }
+  end
+
 end
