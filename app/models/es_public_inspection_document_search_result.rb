cd4e46c0e31d9fb4b7058ee3c6377b12d9d5d3c8
Brandon Carag <brandon@criticaljuncture.org>
2021-11-04 12:26:48 -0700

    Fix: ES PI Search Result
    See Transition Away from Sphinx-style Record Retrieval #203

--- app/models/es_public_inspection_document_search_result.rb
+++ app/models/es_public_inspection_document_search_result.rb
@@ -3,9 +3,9 @@ class EsPublicInspectionDocumentSearchResult < EsSearchResult
   def page_views
     start_date = SETTINGS['public_inspection_document_page_view_start_date']
 
-    if filed_at && start_date && (document.filed_at.to_date >= start_date)
+    if filed_at && start_date && (filed_at.to_date >= start_date)
       {
-        count:         PageViewCount.count_for(document.document_number, PageViewType::PUBLIC_INSPECTION_DOCUMENT),
+        count:         PageViewCount.count_for(document_number, PageViewType::PUBLIC_INSPECTION_DOCUMENT),
         last_updated:  PageViewCount.last_updated(PageViewType::PUBLIC_INSPECTION_DOCUMENT),
       }
     end
