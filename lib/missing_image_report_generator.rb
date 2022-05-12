51b9fa201faf0eac0af43d8b3bd0c3bdd274bd1f
Brandon Carag <brandon@criticaljuncture.org>
2022-05-11 21:32:34 -0700

    Fix: Probable Active Record string association bug
    Add workaround for generating missing images report since `Image.includes(:image_usages)` fails on certain batches.
    
    See: https://github.com/rails/rails/issues/42669

--- lib/missing_image_report_generator.rb
+++ lib/missing_image_report_generator.rb
@@ -10,14 +10,21 @@ class MissingImageReportGenerator
   def self.perform(output_path) #eg /data/test.csv
     CSV.open(output_path, 'wb') do |csv|
       csv << COLUMN_HEADERS
-      Image.includes(:image_variants, image_usages: [:entry]).find_each do |image|
-        csv << [
-          image.identifier,
-          image.image_usages.map(&:document_number).join(','),
-          image.image_usages.map(&:entry).map(&:publication_date).join(','),
-          image.image_file_name.present?,
-          image.image_variants.exists?
-        ]
+      Image.includes(:image_variants).find_in_batches do |batch|
+        image_usages = ImageUsage.
+          includes(:entry).
+          where(identifier: batch.pluck(:identifier)).
+          group_by(&:identifier)
+        batch.each do |image|
+          usages = image_usages[image.identifier] || []
+          csv << [
+            image.identifier,
+            usages.map(&:document_number).join(','),
+            usages.map(&:entry).map(&:publication_date).join(','),
+            image.image_file_name.present?,
+            image.image_variants.present?
+          ]
+        end
       end
     end
   end
