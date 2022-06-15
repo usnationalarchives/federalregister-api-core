e30b688e7fdb6d8704fb6b0b2ad0ac528ce7b270
Brandon Carag <brandon@criticaljuncture.org>
2022-06-15 11:25:10 -0700

    Update: Handle additional error conditions

--- lib/image_pipeline/environment_image_downloader.rb
+++ lib/image_pipeline/environment_image_downloader.rb
@@ -9,7 +9,15 @@ class ImagePipeline::EnvironmentImageDownloader
   def perform(s3_key)
     @s3_key = s3_key
     @connection = GpoImages::FogAwsConnection.new.connection
-    if already_downloaded? #|| s3_file_already_exists?# || db_record exists?
+    if already_downloaded?
+      return
+    elsif s3_file_already_exists?
+      # This scenario is unexpected, but could occur if OFR is uploading a replacement image
+      Honeybadger.notify("#{s3_key} already exists")
+      return
+    elsif image_record_already_exists?
+      # This scenario is unexpected
+      Honeybadger.notify("#{s3_key} already_exists in the database")
       return
     end
 
@@ -22,7 +30,7 @@ class ImagePipeline::EnvironmentImageDownloader
       
       # Save image to S3
       image = Image.find_or_initialize_by(
-        identifier: normalize_image_identifier(s3_key),
+        identifier: normalized_image_identifier,
       )
       image.assign_attributes(
         image:      temp_file,
@@ -46,23 +54,34 @@ class ImagePipeline::EnvironmentImageDownloader
     )
 
     # Enqueue job to check on removing from holding tank
-    ImagePipeline::ImageHoldingTankRemover.perform_in(10.minutes, image_identifier)
+    ImagePipeline::ImageHoldingTankRemover.perform_in(10.minutes, s3_key)
   end
 
   private
 
   attr_reader :s3_key, :connection
 
+  def normalized_image_identifier
+    normalize_image_identifier(s3_key)
+  end
+  memoize :normalize_image_identifier
+
   def image_holding_tank_s3_bucket
     SETTINGS['s3_buckets']['image_holding_tank']
   end
 
   def already_downloaded?
+    #TODO: Possibly handle situation when image doesn't exist at all so we don't hard fail
     get_s3_tags[downloaded_at_tag]
   end
 
+  def image_record_already_exists?
+    Image.find_by(identifier: normalized_image_identifier)
+  end
+
   def s3_file_already_exists?
-    raise "TODO"
+    directory = connection.directories.get(SETTINGS['s3_buckets']['original_images'])
+    directory.files.get(normalized_image_identifier)
   end
 
   def get_s3_tags
