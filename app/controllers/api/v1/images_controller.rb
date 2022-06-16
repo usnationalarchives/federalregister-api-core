98678a087aad4d3fd67248f52fd9eaf807f8a936
Brandon Carag <brandon@criticaljuncture.org>
2022-06-16 14:52:45 -0700

    Update: Image API to use new schema

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::ImagesController < ApiController
 
   def show
-    graphic_styles = GraphicStyle.
-      includes(styleable: :gpo_graphic_usages).
-      where(image_identifier: params[:id].try(:downcase))
+    image = Image.
+      find_by(identifier: params[:id].try(:upcase))
+    image_variants = image.try(:image_variants)
 
-    if graphic_styles.present? && publicly_accessible?(graphic_styles)
-      render_json_or_jsonp image_json(graphic_styles)
+    if image_variants.present? && image.made_public_at?
+      render_json_or_jsonp image_json(image_variants)
     else
       render json: {}, status: 404
     end
@@ -14,23 +14,19 @@ class Api::V1::ImagesController < ApiController
 
   private
 
-  def image_json(graphic_styles)
-    gpo_graphic = graphic_styles.first.styleable
-    image_source = gpo_graphic.sourced_via_ecfr_dot_gov ? "Retired ECFR.gov" : "GPO SFTP"
-
-    graphic_styles.each_with_object(Hash.new) do |graphic_style, hsh|
-      hsh[graphic_style.style_name] = {
-        height: graphic_style.height,
-        image_format: graphic_style.image_format,
-        image_source: image_source,
-        url:    graphic_style.url,
-        width:  graphic_style.width,
+  def image_json(image_variants)
+    image = image_variants.first.original_image
+    image_variants.each_with_object(Hash.new) do |image_variant, hsh|
+      hsh[image_variant.style] = {
+        content_type: image_variant.image_content_type,
+        height:       image_variant.image_height,
+        image_source: image.image_source.try(:identifier) || 'Unknown',
+        sha:          image_variant.image_sha,
+        size:         image_variant.image_size,
+        url:          image_variant.image.url,
+        width:        image_variant.image_width,
       }
     end
-  end
-
-  def publicly_accessible?(graphic_styles)
-    graphic_styles.first.public?
   end
 
 end
--- spec/requests/images_spec.rb
+++ spec/requests/images_spec.rb
@@ -4,73 +4,39 @@ RSpec.describe "Images Endpoint", :type => :request do
   before(:each) { allow_any_instance_of(GraphicStyle).to receive(:url).and_return('http://wwww.example.com') }
 
   it "renders available styles" do
-    allow_any_instance_of(GraphicStyle).to receive(:public?).and_return('truthy')
-    GpoGraphic.create!(id: 9999, identifier: 'ep13oc15.011')
-    GraphicStyle.create!(
-      graphic_id: 9999,
-      graphic_type: 'GpoGraphic',
-      height: 126,
-      image_format: 'png',
-      image_identifier: 'ep13oc15.011',
-      style_name: 'medium',
-      width: 572,
-    )
-    GraphicStyle.create!(
-      graphic_id: 9999,
-      graphic_type: 'GpoGraphic',
-      height: 363,
-      image_format: 'png',
-      image_identifier: 'ep13oc15.011',
-      style_name: 'original_png',
-      width: 1649,
-    )
-    GraphicStyle.create!(
-      graphic_id: 9999,
-      graphic_type: 'GpoGraphic',
-      height: 363,
-      image_format: 'eps',
-      image_identifier: 'ep13oc15.011',
-      style_name: 'original',
-      width: 1649,
+    image = Image.create!(id: 9999, identifier: 'ep13oc15.011', made_public_at: Time.current, source_id: ImageSource::GPO_SFTP.id)
+    ImageVariant.create!(
+      image_height: 126,
+      image_content_type: 'png',
+      image_sha: '0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33',
+      image_size: 999,
+      identifier: 'ep13oc15.011',
+      style: 'medium',
+      image_width: 572,
     )
 
     get '/api/v1/images/EP13OC15.011'
     expect(response.status).to eq(200)
     expect(response.body).to eq({
       "medium": {
-        height: 126,
-        image_format: "png",
-        image_source: "GPO SFTP",
-        url: "http://wwww.example.com",
-        width: 572,
+        content_type: 'png',
+        height:       126,
+        image_source: 'gpo_sftp',
+        sha:          '0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33',
+        size:         999,
+        url:          nil,
+        width:        572,
       },
-      "original_png": {
-        height: 363,
-        image_format: "png",
-        image_source: "GPO SFTP",
-        url: "http://wwww.example.com",
-        width: 1649,
-      },
-      "original": {
-        height: 363,
-        image_format: "eps",
-        image_source: "GPO SFTP",
-        url: "http://wwww.example.com",
-        width: 1649,
-      }
     }.to_json)
   end
 
   it "renders a 404 if the image is not public" do
     allow_any_instance_of(GraphicStyle).to receive(:public?).and_return(false)
-    GraphicStyle.create!(
-      graphic_id: 9999,
-      graphic_type: 'GpoGraphic',
-      height: 126,
-      image_format: 'png',
-      image_identifier: 'ep13oc15.011',
-      style_name: 'medium',
-      width: 572,
+    image = Image.create!(
+      id: 9999,
+      identifier: 'ep13oc15.011',
+      made_public_at: nil,
+      source_id: ImageSource::GPO_SFTP.id
     )
 
     get '/api/v1/images/EP13OC15.011'
