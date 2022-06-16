ef044e436a1065178840136a696011d2c8a005c2
Brandon Carag <brandon@criticaljuncture.org>
2022-06-16 09:56:47 -0700

    Update: Add route for generating signed S3 urls
    The missing images page links to the original images if they are available, and we need a way of exposing private images from the interface.

--- app/controllers/admin/missing_images_controller.rb
+++ app/controllers/admin/missing_images_controller.rb
@@ -3,5 +3,18 @@ class Admin::MissingImagesController < AdminController
     @presenter = MissingImagesPresenter.new
   end
 
+  def show
+    # This route is used for generating signed S3 urls for private objects
+    image = Image.find_by_identifier(params[:id])
+    if image
+      if image.made_public_at.blank?
+        image.fog_public = false #Calling fog_public = false causes image_file.url to generate a signed URL.
+      end
+      redirect_to image.image_file.url
+    else
+      redirect_to admin_missing_images_path
+    end
+  end
+
 end
 
