e035ed87e45c8b74d6280b2694b30f5ef5c88873
Brandon Carag <brandon@criticaljuncture.org>
2022-09-02 15:13:35 -0700

    Update: Address deprecations
    eg:
    ...../home/app/spec/support/rails_3_upgrade_spec_helper_methods.rb:16: warning: Passing safe_level with the 2nd argument of ERB.new is deprecated. Do not use it, and specify other arguments as keyword arguments.
    */home/app/spec/support/rails_3_upgrade_spec_helper_methods.rb:16: warning: Passing safe_level with the 2nd argument of ERB.new is deprecated. Do not use it, and specify other arguments as keyword arguments.
    *./home/app/spec/support/rails_3_upgrade_spec_helper_methods.rb:16: warning: Passing safe_level with the 2nd argument of ERB.new is deprecated. Do not use it, and specify other arguments as keyword arguments.
    /home/app/spec/support/rails_3_upgrade_spec_helper_methods.rb:16: warning: Passing safe_level with the 2nd argument of ERB.new is deprecated. Do not use it, and specify other arguments as keyword arguments.
    ./home/app/spec/support/rails_3_upgrade_spec_helper_methods.rb:16: warning: Passing safe_level with the 2nd argument of ERB.new is deprecated. Do not use it, and specify other arguments as keyword arguments.

--- spec/support/rails_3_upgrade_spec_helper_methods.rb
+++ spec/support/rails_3_upgrade_spec_helper_methods.rb
@@ -5,13 +5,14 @@ module Rails3UpgradeSpecHelperMethods
 
   # This method was deprecated in later versions of rspec (rspec 2ish).  Bringing the code into Rails 3.2 from here: https://www.rubydoc.info/gems/rspec-rails/1.3.4/Spec%2FRails%2FExample%2FHelperExampleGroup:eval_erb
   def eval_erb(text)
-    erb_args = [text]
     if helper.respond_to?(:output_buffer)
-      erb_args += [nil, nil, '@output_buffer']
+      options = {trim_mode: nil, eoutvar: '@output_buffer'}
+    else
+      options = {}
     end
 
     helper.instance_eval do
-      ERB.new(*erb_args).result(binding)
+      ERB.new(text, **options).result(binding)
     end
   end
 
