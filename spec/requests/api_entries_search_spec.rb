fc4f7a2593619a277c30628d3d5ca6a7c9182ce3
Brandon Carag <brandon@criticaljuncture.org>
2022-10-14 10:03:03 -0700

    Fix: Add spec/handling for docs with unassigned agencies

--- spec/requests/api_entries_search_spec.rb
+++ spec/requests/api_entries_search_spec.rb
@@ -33,6 +33,26 @@ RSpec.describe "Entries API", :type => :request, :es => true do
     )
   end
 
+  it "performs an agency facet search" do
+    agency_name_1 = Factory.create(:agency_name)
+    agency_name_2 = Factory.create(:agency_name)
+    agency_name_3 = Factory.create(:agency_name, agency: nil)
+    entry_1       = Factory(:entry, agency_name_ids: [agency_name_1.id]),
+    entry_2       = Factory(:entry, agency_name_ids: [agency_name_2.id]),
+    entry_3       = Factory(:entry, agency_name_ids: [agency_name_2.id])
+    entry_4       = Factory(:entry, agency_name_ids: [agency_name_3.id]) #ie doesn't break for entries with agency names not assigned to an agency
+    ElasticsearchIndexer.reindex_entries(recreate_index: true)
+
+    get "/api/v1/documents/facets/agency"
+    
+    result = JSON.parse(response.body)
+    expect(result).to eq({
+      agency_name_2.agency.slug => {count: 2, name: agency_name_2.agency.name},
+      agency_name_1.agency.slug => {count: 1, name: agency_name_1.agency.name},
+      #NOTE: We chose to omit entries without assigned agencies from the results
+    }.as_json)
+  end
+
   context "#show" do
     it "handles single document search queries" do
       entry = Factory(
