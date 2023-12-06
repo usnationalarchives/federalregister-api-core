72f306f8c6fa895e3dc1787bb5352c685703914a
Brandon Carag <brandon@criticaljuncture.org>
2023-12-06 15:43:35 -0800

    Add: Initial CSP for FR Admin

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,17 +4,28 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https
+  policy.font_src    :self, :https, :data
+  policy.img_src     :self, :https, :data
+  policy.object_src  :none
+  policy.script_src  :self, :https
+  policy.style_src   :self, :https
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+  #   # Specify URI for violation reports
+  if ['production', 'staging'].include?(Rails.env)
+    # Increment version on each change to this file
+    csp_version = 1
+
+    policy.report_uri -> {
+      api_key = Rails.application.credentials.dig(:honeybadger, :csp_api_key)
+      context = {context: try(:honeybadger_context) || {}}
+      context[:context][:csp_version] = csp_version
+      context_args = context.to_query
+      "https://api.honeybadger.io/v1/browser/csp?api_key=#{api_key}&env=#{Rails.env}&report_only=#{Settings.app.csp.report_only}&#{context_args}"
+    }
+  end
+end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
