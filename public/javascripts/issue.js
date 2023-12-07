0c8a9b599bb440b077ddff843ed32e5cec72b2fd
Brandon Carag <brandon@criticaljuncture.org>
2023-12-07 10:26:03 -0800

    Update: Put CSP into report-only

--- public/javascripts/issue.js
+++ public/javascripts/issue.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
       // $(this)
       var form = $($(this).closest('form'));
       form.find('img').remove();
-      form.find('a').prepend('<img src="/images/ui/ui-anim_basic_16x16.gif">')
+      form.find('a').prepend('<img src="/images/ui/ui-anim_basic_16x16.gif">');
       $.ajax({
         url: form.attr('action'),
         data: form.serialize(),
@@ -12,7 +12,7 @@ $(document).ready(function(){
         success: function(){
           form.find('img').remove();
         }
-      })
+      });
     });
   }
 });
