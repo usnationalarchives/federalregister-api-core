0145903cc4121f21f8135dea7369826b0dc975a2
Brandon Carag <brandon@criticaljuncture.org>
2023-12-06 17:28:09 -0800

    Add: SSL options

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -46,6 +46,12 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  # Configure SSL options to enable HSTS with subdomains.
+  Rails.application.config.ssl_options = {
+    redirect: false,
+    hsts: { subdomains: true }
+  }
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
