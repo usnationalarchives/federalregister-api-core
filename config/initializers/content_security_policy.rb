5893f53e4251450562cb1ce454b0b70ae4c0bebe
Brandon Carag <brandon@criticaljuncture.org>
2023-12-08 08:52:44 -0800

    Add: Missing report only flag

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,6 +4,8 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
+Rails.application.config.content_security_policy_report_only = Settings.app.csp.report_only
+
 Rails.application.config.content_security_policy do |policy|
   policy.default_src :self, :https
   policy.font_src    :self, :https, :data
