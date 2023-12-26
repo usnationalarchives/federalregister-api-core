690a6c4d0bcc9afa2ac5e5bcb269739d257c31c6
Brandon Carag <brandon@criticaljuncture.org>
2023-12-26 09:53:54 -0800

    Refactor: Scraper to better support testing for edge cases

--- lib/nara_eo_scraper.rb
+++ lib/nara_eo_scraper.rb
@@ -64,78 +64,89 @@ class NaraEoScraper
   end
 
   # Use this to test individual scraping of pages: 
-  # reload!; NaraEoScraper.scrape_year_specific_page('https://www.archives.gov/federal-register/executive-orders/1962.html','clinton')
   def self.scrape_year_specific_page(url, president_identifier)
     # Load and parse the HTML file
     html_content = URI.open(url).read
 
-    doc = Nokogiri::HTML(html_content)
 
     # Prepare CSV file
     CSV.open('data/nara_executive_orders.csv', 'a', write_headers: false, headers: HEADERS) do |csv|
-      # Iterate over each executive order
-      doc.css('hr').each do |hr|
-
-        title_element = hr.next_element
-        next unless title_element && title_element.name == 'p' && (hr.next_element.children.length > 1)
-
-        # Extract title
-        title = title_element.text.strip.split("\n")
-        title.shift
-        title = title.join("\n")
-
-        # Extract presidential document number from the title
-        presidential_document_number = title_element.children.find{|x| x.name == 'a'}.try(:[], 'name') || title_element.children.first.text.gsub(/Executive Order /, '').strip
-
-      
-        # Initialize details
-        details = { 'signing_date' => '', 'citation' => '', 'publication_date' => '', 'disposition_notes' => [] }
-
-        # Iterate over details
-        title_element.xpath('following-sibling::ul[1]/li').each do |li|
-          case li.text.strip
-          when /^Signed:/
-            details['signing_date'] = li.text.gsub('Signed: ', '')
-            details['parsed_signing_date'] = Date.try(:parse, details['signing_date'])
-          when /not received for publication/
-            # mark columns as inappropriate
-            ['citation', 'publication_date', 'parsed_publication_date'].each do |column_name|
-              details[column_name] = 'not_received_for_publication'
-            end
-          when /not received in time for publication/
-            # mark columns as inappropriate
-            ['citation', 'publication_date', 'parsed_publication_date'].each do |column_name|
-              details[column_name] = 'not_received_in_time_for_publication'
-            end
-          when /Federal Register page and date:/i
-            citation_text = li.text.gsub('Federal Register page and date: ', '').strip
-
-            #Sometimes we have a citation like "Federal Register page and date: 61 FR 1209; January 18, 1996" and sometimes it's like "Federal Register page and date: 70 FR 2323, January 12, 2005"
-            if citation_text.include?(";")
-              details['citation'] = citation_text.split(';').first
-              details['publication_date'] = citation_text.split(';').last
-            else
-              details['citation'] = citation_text.split(',').first
-              details['publication_date'] = citation_text.split(',').last(2).join(',')
-            end
-            begin
-              details['parsed_publication_date'] = Date.parse(details['publication_date'])
-            rescue
-              details['parsed_publication_date'] = "date_parsing_error"
-            end
-          else
-            details['disposition_notes'] << li.text.strip
-          end
-        end
-
-        # Concatenate disposition notes
-        disposition_notes = details['disposition_notes'].join(', ')
-
-        president = president_identifier
-
-        # Write to CSV
-        csv << [title, details['citation'], presidential_document_number, details['signing_date'], details['parsed_signing_date'], details['publication_date'], details['parsed_publication_date'], president, disposition_notes, url]
+      eo_metadata(html_content, president_identifier, url).each do |eo|
+        csv << eo
       end
+    end
+  end
+
+  def self.eo_metadata(html_content, president_identifier, url)
+    # Iterate over each executive order
+    nokogiri_doc = Nokogiri::HTML(html_content)
+    nokogiri_doc.css('hr').map do |hr|
+      title_element = hr.next_element
+      next unless title_element && title_element.name == 'p' && (hr.next_element.children.length > 1)
+
+      # Extract title
+      title = title_element.text.strip.split("\n")
+      title.shift
+      title = title.join("\n")
+
+      # Extract presidential document number from the title
+      presidential_document_number = title_element.children.find{|x| x.name == 'a'}.try(:[], 'name') || title_element.children.first.text.gsub(/Executive Order /, '').strip
+
+    
+      # Initialize details
+      details = { 'signing_date' => '', 'citation' => '', 'publication_date' => '', 'disposition_notes' => [] }
+
+      # Iterate over details
+      title_element.xpath('following-sibling::ul[1]/li').each do |li|
+        case li.text.strip
+        when /^Signed:/
+          details['signing_date'] = li.text.gsub('Signed: ', '')
+          details['parsed_signing_date'] = Date.try(:parse, details['signing_date'])
+        when /not received for publication/
+          # mark columns as inappropriate
+          ['citation', 'publication_date', 'parsed_publication_date'].each do |column_name|
+            details[column_name] = 'not_received_for_publication'
+          end
+        when /not received in time for publication/
+          # mark columns as inappropriate
+          ['citation', 'publication_date', 'parsed_publication_date'].each do |column_name|
+            details[column_name] = 'not_received_in_time_for_publication'
+          end
+        when /Federal Register page and date:/i
+          citation_text = li.text.gsub('Federal Register page and date: ', '').strip
+
+          #Sometimes we have a citation like "Federal Register page and date: 61 FR 1209; January 18, 1996" and sometimes it's like "Federal Register page and date: 70 FR 2323, January 12, 2005"
+          if citation_text.include?(";")
+            details['citation'] = citation_text.split(';').first
+            details['publication_date'] = citation_text.split(';').last
+          else
+            details['citation'] = citation_text.split(',').first
+            details['publication_date'] = citation_text.split(',').last(2).join(',')
+          end
+          begin
+            details['parsed_publication_date'] = Date.parse(details['publication_date'])
+          rescue
+            details['parsed_publication_date'] = "date_parsing_error"
+          end
+        else
+          details['disposition_notes'] << li.text.strip
+        end
+      end
+
+      # Concatenate disposition notes
+      disposition_notes = details['disposition_notes'].join(', ')
+
+      [
+        title, details['citation'],
+        presidential_document_number,
+        details['signing_date'],
+        details['parsed_signing_date'],
+        details['publication_date'],
+        details['parsed_publication_date'],
+        president_identifier,
+        disposition_notes,
+        url
+      ]
     end
   end
 
