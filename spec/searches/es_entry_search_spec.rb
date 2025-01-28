ad62c684d8f35caa7c05f238123023180ebc8747
Peregrinator <bob.burbach@gmail.com>
2025-01-28 17:56:32 +0000

    Cleanup whitespace

--- spec/searches/es_entry_search_spec.rb
+++ spec/searches/es_entry_search_spec.rb
@@ -46,14 +46,14 @@ describe EsEntrySearch, es: true, type: :request do #NOTE: Only one spec in this
         raw_text_updated_at: Time.current
       )
       ElasticsearchIndexer.reindex_entries(recreate_index: true)
-  
+
       get "/api/v1/documents.json?per_page=20&conditions[term]=goat"
       json_response = JSON.parse(response.body)
-  
+
       expect(json_response).to include(
         'count' => 1
       )
-  
+
       expect(json_response.fetch('results').first).to include(
         'html_url' => "http://www.fr2.local:8081/documents/#{entry.publication_date.year}/#{sprintf('%02i',entry.publication_date.month)}/#{sprintf('%02i',entry.publication_date.day)}/#{entry.document_number}/#{entry.slug}",
         'pdf_url'  => "https://www.govinfo.gov/content/pkg/FR-#{entry.publication_date.to_s(:iso)}/pdf/#{entry.document_number}.pdf",
@@ -1325,7 +1325,7 @@ describe EsEntrySearch, es: true, type: :request do #NOTE: Only one spec in this
           :entry,
           document_number: nil,
           title: 'fish',
-        ) 
+        )
         Factory.create(:issue, publication_date: Date.current, completed_at: Time.current)
         Entry.bulk_index([entry], refresh: true)
         search = EsEntrySearch.new(conditions: {term: 'fish'}, include_pre_1994_docs: true)
@@ -1341,7 +1341,7 @@ describe EsEntrySearch, es: true, type: :request do #NOTE: Only one spec in this
           :entry,
           document_number: nil,
           title: 'fish',
-        ) 
+        )
         Factory.create(:issue, publication_date: Date.current, completed_at: Time.current)
         Entry.bulk_index([entry], refresh: true)
         search = EsEntrySearch.new(conditions: {term: 'fish'}, include_pre_1994_docs: true)
